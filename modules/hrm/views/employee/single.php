09e0985732246de591c4598a0778bd6609f34d54
Tareq Hasan <tareq1988@gmail.com>
2014-12-16 01:46:30 +0600

    single employee view update

--- modules/hrm/views/employee/single.php
+++ modules/hrm/views/employee/single.php
@@ -1,4 +1,4 @@
-<div class="wrap erp-leads-single">
+<div class="wrap erp erp-leads-single">
     <h2>Employee <a href="#" class="add-new-h2">Add New</a></h2>
 
     <div class="erp-lead-wrap">
@@ -9,39 +9,150 @@
                 </div>
 
                 <div class="leads-user-info">
-                    <h3><span class="title">Mr. John Doe</span> <span class="company">- ABC Company</span></h3>
+                    <h3><span class="title">John Doe</span></h3>
 
                     <ul class="lead-info">
                         <li>
-                            <label>Lead Owner</label> <span class="sep">:</span> <span class="value">Tareq Hasan</span>
+                            Software Engineer - Engineering
                         </li>
 
                         <li>
-                            <label>Email</label> <span class="sep">:</span> <span class="value"><a href="mailto:john@doe.com">john@doe.com</a></span>
+                            <a href="mailto:john@doe.com">john@doe.com</a>
                         </li>
 
                         <li>
-                            <label>Phone</label> <span class="sep">:</span> <span class="value"><a href="tel:+8802457821"><span class="dashicons dashicons-smartphone"></span></a> +8802457821</span>
-                        </li>
-
-                        <li>
-                            <label>Mobile</label> <span class="sep">:</span> <span class="value"><a href="tel:+8805454545"><span class="dashicons dashicons-smartphone"></span></a> +8805454545</span>
-                        </li>
-
-                        <li>
-                            <label>Lead Status</label> <span class="sep">:</span> <span class="value">Attempted to Contact</span>
+                            <a href="tel:+8802457821"><span class="dashicons dashicons-smartphone"></span></a> +8802457821
                         </li>
                     </ul>
                 </div><!-- .leads-user-info -->
             </div><!-- .leads-profile-view -->
 
-            <h2 class="nav-tab-wrapper">
-                <a href="#notes" class="nav-tab nav-tab-active">Notes</a>
-                <a href="#tasks" class="nav-tab ">Tasks</a>
-                <a href="#events" class="nav-tab ">Events</a>
-                <a href="#calls" class="nav-tab ">Calls</a>
-                <a href="#emails" class="nav-tab ">Emails</a>
+            <h2 class="nav-tab-wrapper" style="margin-bottom: 15px;">
+                <a href="#notes" class="nav-tab nav-tab-active">General</a>
+                <a href="#tasks" class="nav-tab">Leave</a>
+                <a href="#tasks" class="nav-tab">Compensation</a>
             </h2>
+
+            <div class="postbox leads-actions">
+                <div class="handlediv" title="Click to toggle"><br></div>
+                <h3 class="hndle"><span>Basic Info</span></h3>
+                <div class="inside">
+                    First Name: John        Last Name: Doe<br>
+                    Employee ID: 1          Email: john@doe.com
+                </div>
+            </div><!-- .postbox -->
+
+            <div class="postbox leads-actions">
+                <div class="handlediv" title="Click to toggle"><br></div>
+                <h3 class="hndle"><span>Work</span></h3>
+                <div class="inside">
+                    Department: Engineering         Title: Developer<br>
+                    Reporting To: Jane Doe          Joined: 08 Dec, 2013<br>
+                    Source of Hire: Direct          Work Location: Dhaka<br>
+                    Employee Type: Permanent        Employee Status: Active<br>
+                    Work Phone: +88047525145        Employee ID: 1
+                </div>
+            </div><!-- .postbox -->
+
+            <div class="postbox leads-actions">
+                <div class="handlediv" title="Click to toggle"><br></div>
+                <h3 class="hndle"><span>Personal Details</span></h3>
+                <div class="inside">
+                    Mobile: +8801475454545          Address: Mohammadpur, Dhaka<br>
+                    Other Email: fake@email.com     Birth Date: 01 Dec, 2011<br>
+                    Gender: Male                    Nationality: Bangladesh<br>
+                    Marital Status: Single          Driving License: QZ454545425<br>
+                    Hobbies: Travelling, Reading
+                </div>
+            </div><!-- .postbox -->
+
+            <div class="postbox leads-actions">
+                <div class="handlediv" title="Click to toggle"><br></div>
+                <h3 class="hndle"><span>Work Experience</span></h3>
+                <div class="inside">
+
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Previous Company</th>
+                                <th>Job Title</th>
+                                <th>From</th>
+                                <th>To</th>
+                                <th>Job Description</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>ABC Company</td>
+                                <td>Software Engineer</td>
+                                <td>15 Jan, 2013</td>
+                                <td>30 Feb, 2015</td>
+                                <td>-</td>
+                            </tr>
+                            <tr>
+                                <td>ABC Company</td>
+                                <td>Software Engineer</td>
+                                <td>15 Jan, 2013</td>
+                                <td>30 Feb, 2015</td>
+                                <td>-</td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                </div>
+            </div><!-- .postbox -->
+
+            <div class="postbox leads-actions">
+                <div class="handlediv" title="Click to toggle"><br></div>
+                <h3 class="hndle"><span>Education</span></h3>
+                <div class="inside">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>School Name</th>
+                                <th>Degree</th>
+                                <th>Field(s) of Study</th>
+                                <th>Date of Completion</th>
+                                <th>Additional Notes</th>
+                                <th>Interests</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>ABC School</td>
+                                <td>SSC</td>
+                                <td>Science</td>
+                                <td>2004</td>
+                                <td>-</td>
+                                <td>-</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div><!-- .postbox -->
+
+            <div class="postbox leads-actions">
+                <div class="handlediv" title="Click to toggle"><br></div>
+                <h3 class="hndle"><span>Dependents</span></h3>
+                <div class="inside">
+                    <table>
+                        <tfoot>
+                            <tr>
+                                <th>Name</th>
+                                <th>Relationship</th>
+                                <th>Date of Birth</th>
+                            </tr>
+                        </tfoot>
+                        <tbody>
+                            <tr>
+                                <td>John Doe</td>
+                                <td>Father</td>
+                                <td>1 Dec, 1960</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div><!-- .postbox -->
 
         </div><!-- .leads-left -->
 
@@ -50,21 +161,7 @@
                 <h3 class="hndle"><span>Actions</span></h3>
                 <div class="inside">
                     <a class="button button-primary" href="#">Edit</a>
-                    <a class="button" href="#">Delete</a>
-                    <a class="button" href="#">Clone</a>
-                    <a class="button" href="#">Convert</a>
-                </div>
-            </div><!-- .postbox -->
-
-            <div class="postbox leads-quick-actions">
-                <h3 class="hndle"><span>Quick Actions</span></h3>
-                <div class="inside">
-                    <ul>
-                        <li><a href="#">Add a new task</a></li>
-                        <li><a href="#">Add a new event</a></li>
-                        <li><a href="#">Add a new call</a></li>
-                        <li><a href="#">Send Mail</a></li>
-                    </ul>
+                    <a class="button" href="#">Terminate</a>
                 </div>
             </div><!-- .postbox -->
         </div><!-- .leads-right -->
