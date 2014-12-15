1d80929c7b850910a24292b7e056a53b965fc1b0
Tareq Hasan <tareq1988@gmail.com>
2014-12-16 05:45:54 +0600

    beautify employee info

--- modules/hrm/views/employee/single.php
+++ modules/hrm/views/employee/single.php
@@ -37,8 +37,12 @@
                 <div class="handlediv" title="Click to toggle"><br></div>
                 <h3 class="hndle"><span>Basic Info</span></h3>
                 <div class="inside">
-                    First Name: John        Last Name: Doe<br>
-                    Employee ID: 1          Email: john@doe.com
+                    <ul class="erp-list two-col separated">
+                        <li><label>First Name</label> <span class="sep"> : </span> <span class="value">John</span></li>
+                        <li><label>Last Name</label> <span class="sep"> : </span> <span class="value">Doe</span></li>
+                        <li><label>Employee ID</label> <span class="sep"> : </span> <span class="value">1</span></li>
+                        <li><label>Email</label> <span class="sep"> : </span> <span class="value"><a href="mailto:john@doe.com">john@doe.com</a></span></li>
+                    </ul>
                 </div>
             </div><!-- .postbox -->
 
@@ -46,11 +50,15 @@
                 <div class="handlediv" title="Click to toggle"><br></div>
                 <h3 class="hndle"><span>Work</span></h3>
                 <div class="inside">
-                    Department: Engineering         Title: Developer<br>
-                    Reporting To: Jane Doe          Joined: 08 Dec, 2013<br>
-                    Source of Hire: Direct          Work Location: Dhaka<br>
-                    Employee Type: Permanent        Employee Status: Active<br>
-                    Work Phone: +88047525145        Employee ID: 1
+                    <ul class="erp-list two-col separated">
+                        <li><label>Department</label> <span class="sep"> : </span> <span class="value">Engineering</span></li>
+                        <li><label>Title</label> <span class="sep"> : </span> <span class="value">Developer</span></li>
+                        <li><label>Reporting To</label> <span class="sep"> : </span> <span class="value">Jane Doe</span></li>
+                        <li><label>Joined</label> <span class="sep"> : </span> <span class="value">08 Dec, 2013</span></li>
+                        <li><label>Source of Hire</label> <span class="sep"> : </span> <span class="value">Direct</span></li>
+                        <li><label>Employee Status</label> <span class="sep"> : </span> <span class="value">Permanent</span></li>
+                        <li><label>Work Phone</label> <span class="sep"> : </span> <span class="value"><a href="tel:+8801827273838">+8801827273838</a></span></li>
+                    </ul>
                 </div>
             </div><!-- .postbox -->
 
@@ -58,11 +66,17 @@
                 <div class="handlediv" title="Click to toggle"><br></div>
                 <h3 class="hndle"><span>Personal Details</span></h3>
                 <div class="inside">
-                    Mobile: +8801475454545          Address: Mohammadpur, Dhaka<br>
-                    Other Email: fake@email.com     Birth Date: 01 Dec, 2011<br>
-                    Gender: Male                    Nationality: Bangladesh<br>
-                    Marital Status: Single          Driving License: QZ454545425<br>
-                    Hobbies: Travelling, Reading
+                    <ul class="erp-list two-col separated">
+                        <li><label>Mobile</label> <span class="sep"> : </span> <span class="value"><a href="tel:+8801475454545">+8801475454545</a></span></li>
+                        <li><label>Address</label> <span class="sep"> : </span> <span class="value">Mohammadpur, Dhaka</span></li>
+                        <li><label>Other Email</label> <span class="sep"> : </span> <span class="value"><a href="mailto:fake@email.com">fake@email.com</a></span></li>
+                        <li><label>Date of Birth</label> <span class="sep"> : </span> <span class="value">01 Dec, 2010</span></li>
+                        <li><label>Gender</label> <span class="sep"> : </span> <span class="value">Male</span></li>
+                        <li><label>Nationality</label> <span class="sep"> : </span> <span class="value">Bangladeshi</span></li>
+                        <li><label>Marital Status</label> <span class="sep"> : </span> <span class="value">Single</span></li>
+                        <li><label>Driving License</label> <span class="sep"> : </span> <span class="value">QZ454545425</span></li>
+                        <li><label>Hobbies</label> <span class="sep"> : </span> <span class="value">Travelling, Reading</span></li>
+                    </ul>
                 </div>
             </div><!-- .postbox -->
 
@@ -71,7 +85,7 @@
                 <h3 class="hndle"><span>Work Experience</span></h3>
                 <div class="inside">
 
-                    <table>
+                    <table class="widefat">
                         <thead>
                             <tr>
                                 <th>Previous Company</th>
@@ -82,7 +96,7 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
+                            <tr class="alternate">
                                 <td>ABC Company</td>
                                 <td>Software Engineer</td>
                                 <td>15 Jan, 2013</td>
@@ -106,7 +120,7 @@
                 <div class="handlediv" title="Click to toggle"><br></div>
                 <h3 class="hndle"><span>Education</span></h3>
                 <div class="inside">
-                    <table>
+                    <table class="widefat">
                         <thead>
                             <tr>
                                 <th>School Name</th>
@@ -135,14 +149,14 @@
                 <div class="handlediv" title="Click to toggle"><br></div>
                 <h3 class="hndle"><span>Dependents</span></h3>
                 <div class="inside">
-                    <table>
-                        <tfoot>
+                    <table class="widefat">
+                        <thead>
                             <tr>
                                 <th>Name</th>
                                 <th>Relationship</th>
                                 <th>Date of Birth</th>
                             </tr>
-                        </tfoot>
+                        </thead>
                         <tbody>
                             <tr>
                                 <td>John Doe</td>
@@ -162,6 +176,7 @@
                 <div class="inside">
                     <a class="button button-primary" href="#">Edit</a>
                     <a class="button" href="#">Terminate</a>
+                    <a class="button" href="#">Print</a>
                 </div>
             </div><!-- .postbox -->
         </div><!-- .leads-right -->
