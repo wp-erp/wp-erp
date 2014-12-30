5c721a9a85a9c06920e26cf375b3166ed151fe3b
Tareq Hasan <tareq1988@gmail.com>
2014-12-31 00:58:16 +0600

    fix our erpPopup jQuery plugin

--- assets/js/jquery-popup.js
+++ assets/js/jquery-popup.js
@@ -1,16 +1,26 @@
+/* jshint devel:true */
+
+/**
+ * A simple jQuery modal plugin for our own needs
+ *
+ * @author Tareq Hasan
+ */
 ;(function($, window, document, undefined) {
 
     // Create the defaults once
     var pluginName = 'erpPopup',
         defaults = {
+            title: '',
             content: '',
-            buttons: {}
+            button: 'Submit',
+            extraClass: '',
+            onSubmit: function() {}
         };
 
     // The actual plugin constructor
-    function Plugin(element, options) {
-        this.element = element;
+    function Plugin(options) {
         this.settings = $.extend({}, defaults, options);
+        this.element = $(this.settings.content);
         this._defaults = defaults;
         this._name = pluginName;
         this.init();
@@ -18,28 +28,101 @@
 
     // Avoid Plugin.prototype conflicts
     $.extend(Plugin.prototype, {
+
+        /**
+         * initialize the plugin
+         *
+         * @return {void}
+         */
         init: function() {
-            // Place initialization logic here
-            // You already have access to the DOM element and
-            // the options via the instance, e.g. this.element
-            // and this.settings
-            // you can add more functions like the one below and
-            // call them like so: this.yourOtherFunction(this.element, this.settings).
-            console.log("xD");
+            this.show_modal();
+            this.bindEvents();
         },
-        yourOtherFunction: function() {
-            // some logic
+
+        /**
+         * Bind the DOM events
+         *
+         * @return {void}
+         */
+        bindEvents: function() {
+            // close the modal window
+            $('.erp-modal-backdrop, .erp-modal .close').on('click', $.proxy(this.closeModal, this) );
+            $('.erp-modal form.erp-modal-form').on('submit', $.proxy(this.formSubmit, this) );
+            $( window ).on( 'keydown', $.proxy(this.onEscapeKey, this) );
+        },
+
+        /**
+         * form submit callback
+         *
+         * @param  {event}
+         *
+         * @return {void}
+         */
+        formSubmit: function(e) {
+            e.preventDefault();
+
+            this.settings.onSubmit.call( $(e.currentTarget) );
+        },
+
+        /**
+         * Show modal form
+         *
+         * @return {void}
+         */
+        show_modal: function() {
+            var $modal = $( '.erp-modal' );
+
+            if ( this.settings.extraClass !== '' ) {
+                $modal.addClass( this.settings.extraClass );
+            }
+
+            $( '.erp-modal-backdrop' ).show();
+            $modal.find('h2').text( this.settings.title );
+            $modal.find('.button-primary').text( this.settings.button );
+            $modal.find( '.content').empty().html(this.element);
+            $modal.show();
+        },
+
+        /**
+         * If pressing ESC close the modal
+         *
+         * @param  {event}
+         *
+         * @return {void}
+         */
+        onEscapeKey: function(e) {
+            if ( 27 === e.keyCode ) {
+                this.closeModal();
+            }
+        },
+
+        /**
+         * Close the modal dialog
+         *
+         * @param  {event}
+         *
+         * @return {void}
+         */
+        closeModal: function(e) {
+            if ( typeof e !== 'undefined' ) {
+                e.preventDefault();
+            }
+
+            // empty and hide the modal
+            $('.erp-modal .content').empty();
+            $('.erp-modal-backdrop, .erp-modal').hide();
+
+            // remove the event handler
+            $('.erp-modal form.erp-modal-form').off('submit', this.formSubmit);
+
+            if ( this.settings.extraClass !== '' ) {
+                $('.erp-modal').removeClass( this.settings.extraClass );
+            }
         }
     });
 
-    $.fn[pluginName] = function(options) {
-        this.each(function() {
-            if (!$.data(this, "plugin_" + pluginName)) {
-                $.data(this, "plugin_" + pluginName, new Plugin(this, options));
-            }
-        });
-
-        return this;
+    $.erpPopup = function(options) {
+        new Plugin(options);
     };
 
 })(jQuery, window, document);
