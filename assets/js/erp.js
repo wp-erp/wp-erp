b578315f28fba9d6ba07c83e05cf0002d0272c92
Tareq Hasan <tareq1988@gmail.com>
2014-12-31 19:38:27 +0600

    HR js codes moved to its module directory

---
+++ assets/js/erp.js
@@ -0,0 +1,121 @@
+/* jshint devel:true */
+/* global wpErpCountries */
+/* global wp */
+/* global wpErp */
+
+;(function($) {
+    'use strict';
+
+    var WeDevs_ERP = {
+
+        /**
+         * Initialize the events
+         *
+         * @return {void}
+         */
+        initialize: function() {
+
+            $( 'select#erp-country').on( 'change', this.populateState );
+
+            $( '#postimagediv').on( 'click', '#set-company-thumbnail', this.setCompanyLogo );
+            $( '#postimagediv').on( 'click', 'a.remove-logo', this.removeCompanyLogo );
+
+            // fire change events
+            $( 'select#erp-country').change();
+        },
+
+        /**
+         * Upload and set company logo
+         *
+         * @param {event}
+         */
+        setCompanyLogo: function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+
+            var frame;
+
+            if ( frame ) {
+                frame.open();
+                return;
+            }
+
+            frame = wp.media({
+                title: wpErp.upload_logo,
+                button: { text: wpErp.set_logo }
+            });
+
+            frame.on('select', function() {
+                var selection = frame.state().get('selection');
+
+                selection.map( function( attachment ) {
+                    attachment = attachment.toJSON();
+
+                    var html = '<img src="' + attachment.url + '" alt="" />';
+                        html += '<input type="hidden" name="company_logo_id" value="' + attachment.id + '" />';
+                        html += '<a href="#" class="remove-logo">' + wpErp.remove_logo + '</a>';
+
+                    $( '.inside', '#postimagediv' ).html( html );
+                });
+            });
+
+            frame.open();
+        },
+
+        /**
+         * Remove the company logo
+         *
+         * @param  {event}
+         *
+         * @return {void}
+         */
+        removeCompanyLogo: function(e) {
+            e.preventDefault();
+
+            var html = '<a href="#" id="set-company-thumbnail" class="thickbox">' + wpErp.upload_logo + '</a>';
+
+            $( '.inside', '#postimagediv' ).html( html );
+        },
+
+        /**
+         * Populate the state dropdown based on selected country
+         *
+         * @return {void}
+         */
+        populateState: function() {
+            if ( typeof wpErpCountries === 'undefined' ) {
+                return false;
+            }
+
+            var country = $(this).val(),
+                empty = '<option val="-1">-------------</option>';
+
+            console.log( country);
+            console.log( wpErpCountries[ country ] );
+
+            if ( wpErpCountries[ country ] ) {
+                var options = '',
+                    state = wpErpCountries[ country ];
+
+                for ( var index in state ) {
+                    options = options + '<option value="' + index + '">' + state[ index ] + '</option>';
+                }
+
+                if ( $.isArray( wpErpCountries[ country ] ) ) {
+                    $('#erp-state').html( empty );
+                } else {
+                    $('#erp-state').html( options );
+                }
+
+                // console.log(options);
+            } else {
+                $('#erp-state').html( empty );
+            }
+        }
+    };
+
+    $(function() {
+        WeDevs_ERP.initialize();
+    });
+
+})(jQuery, this);
---
+++ modules/hrm/assets/js/hrm.js
@@ -0,0 +1,67 @@
+/* jshint devel:true */
+/* global wpErp */
+/* global _ */
+
+;(function($) {
+    'use strict';
+
+    var WeDevs_ERP_HR = {
+
+        /**
+         * Initialize the events
+         *
+         * @return {void}
+         */
+        initialize: function() {
+            // new department
+            $( '.erp-hr-depts').on( 'click', 'a#erp-new-dept', this.modalNewDepartment ); // new department
+        },
+
+        /**
+         * Create new department
+         *
+         * @param  {event}
+         *
+         * @return {void}
+         */
+        modalNewDepartment: function(e) {
+            e.preventDefault();
+
+            $.erpPopup({
+                title: wpErp.popup.dept_title,
+                button: wpErp.popup.dept_submit,
+                content: $('#erp-tmpl-new-dept').html(),
+                extraClass: 'smaller',
+                onSubmit: function(modal) {
+                    var input = this.serialize();
+
+                    $.post(wpErp.ajaxurl, input, function(resp) {
+                        // console.log(resp.data);
+
+                        if ( resp.success === true ) {
+                            var row   = $('#erp-tmpl-dept-row').html(),
+                                table = $('table.department-list-table');
+
+                            if ( table ) {
+                                var cls = $('tr:last', table).attr('class'),
+                                    cls = ( cls === 'odd' ) ? 'alternate' : 'odd';
+
+                                resp.data.cls = cls;
+                                row = _.template( row, resp.data );
+                                table.append(row);
+
+                                modal.closeModal();
+                            }
+                        } else {
+                            alert( resp.data );
+                        }
+                    }); // $.post
+                } // onSubmit
+            }); //popup
+        },
+    };
+
+    $(function() {
+        WeDevs_ERP_HR.initialize();
+    });
+})(jQuery);
