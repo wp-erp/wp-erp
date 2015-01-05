a0fcd29350adb16b6fe12c8c95518b35df14424f
Tareq Hasan <tareq1988@gmail.com>
2015-01-06 02:42:51 +0600

    no employee found message

--- modules/hrm/views/employee.php
+++ modules/hrm/views/employee.php
@@ -56,26 +56,37 @@ $vendors = array(
             <?php
             $employees = erp_hr_get_employees( erp_get_current_company_id() );
 
-            foreach( $employees as $num => $employee ) { ?>
-            <tr class="<?php echo $num % 2 == 0 ? 'alternate' : 'odd'; ?>">
-                <th scope="row" class="check-column">
-                    <input id="cb-select-1" type="checkbox" name="post[]" value="1">
-                </th>
-                <td class="username col- column-username">
-                    <?php echo get_avatar( '', 32 ); ?>
+            if ( $employees ) {
 
-                    <strong><a href="<?php echo erp_hr_url_single_employee( $employee->id ); ?>"><?php echo $employee->get_full_name(); ?></a></strong>
+                foreach( $employees as $num => $employee ) { ?>
+                    <tr class="<?php echo $num % 2 == 0 ? 'alternate' : 'odd'; ?>">
+                        <th scope="row" class="check-column">
+                            <input id="cb-select-1" type="checkbox" name="post[]" value="1">
+                        </th>
+                        <td class="username col- column-username">
+                            <?php echo get_avatar( '', 32 ); ?>
 
-                    <div class="row-actions">
-                        <span class="edit"><a href="#" data-id="<?php echo $employee->id; ?>" title="<?php echo esc_attr( 'Edit this item', 'wp-erp' ); ?>"><?php _e( 'Edit', 'wp-erp' ); ?></a> | </span>
-                        <span class="trash"><a class="submitdelete" data-id="<?php echo $employee->id; ?>" title="<?php echo esc_attr( 'Delete this item', 'wp-erp' ); ?>" href="#"><?php _e( 'Delete', 'wp-erp' ); ?></a></span>
-                    </div>
-                </td>
-                <td class="col-"><?php echo $employee->get_job_title(); ?></td>
-                <td class="col-"><?php echo $employee->get_department_title(); ?></td>
-                <td class="col-"><?php echo $employee->get_type(); ?></td>
-                <td class="col-"><?php echo $employee->get_joined_date(); ?></td>
-            </tr>
+                            <strong><a href="<?php echo erp_hr_url_single_employee( $employee->id ); ?>"><?php echo $employee->get_full_name(); ?></a></strong>
+
+                            <div class="row-actions">
+                                <span class="edit"><a href="#" data-id="<?php echo $employee->id; ?>" title="<?php echo esc_attr( 'Edit this item', 'wp-erp' ); ?>"><?php _e( 'Edit', 'wp-erp' ); ?></a> | </span>
+                                <span class="trash"><a class="submitdelete" data-id="<?php echo $employee->id; ?>" title="<?php echo esc_attr( 'Delete this item', 'wp-erp' ); ?>" href="#"><?php _e( 'Delete', 'wp-erp' ); ?></a></span>
+                            </div>
+                        </td>
+                        <td class="col-"><?php echo $employee->get_job_title(); ?></td>
+                        <td class="col-"><?php echo $employee->get_department_title(); ?></td>
+                        <td class="col-"><?php echo $employee->get_type(); ?></td>
+                        <td class="col-"><?php echo $employee->get_joined_date(); ?></td>
+                    </tr>
+                <?php } ?>
+            <?php } else { ?>
+
+                <tr>
+                    <td colspan="6">
+                        <?php _e( 'No employees found!', 'wp-erp' ); ?>
+                    </td>
+                </tr>
+
             <?php } ?>
         </tbody>
     </table>
