f63f638a6b8d5815d5bf294cb270034613761c06
Asaquzzaman Mishu <mishu@Asaquzzamans-MacBook-Pro.local>
2015-11-02 12:52:58 +0600

    style leave request form

--- modules/hrm/views/leave/new-request.php
+++ modules/hrm/views/leave/new-request.php
@@ -1,82 +1,87 @@
 <div class="wrap erp erp-hr-leave-request-new">
-    <h2><?php _e( 'New Leave Request', 'wp-erp' ); ?></h2>
+    <div class="postbox">
+        <h3 class="hndle"><?php _e( 'New Leave Request', 'wp-erp');?></h3>
 
-    <?php if ( isset( $_GET['msg'] ) ) {
+        <div class="inside">
 
-        if ( $_GET['msg'] == 'submitted' ) {
-            erp_html_show_notice( __( 'Leave request has been submitted successfully.', 'wp-erp' ) );
-        } elseif ( $_GET['msg'] == 'error' ) {
-            erp_html_show_notice( __( 'Something went wrong.', 'wp-erp' ), 'error' );
-        }
+            <?php if ( isset( $_GET['msg'] ) ) {
 
-    } ?>
+                if ( $_GET['msg'] == 'submitted' ) {
+                    erp_html_show_notice( __( 'Leave request has been submitted successfully.', 'wp-erp' ) );
+                } elseif ( $_GET['msg'] == 'error' ) {
+                    erp_html_show_notice( __( 'Something went wrong.', 'wp-erp' ), 'error' );
+                }
 
-    <form action="" method="post">
+            } ?>
 
-        <ul class="erp-list blur blocked" style="width: 400px;">
+            <form action="" method="post">
 
-            <?php if ( current_user_can( 'manage_options' ) ) { ?>
-                <li>
-                    <?php erp_html_form_input( array(
-                        'label'    => __( 'Employee', 'wp-erp' ),
-                        'name'     => 'employee_id',
-                        'value'    => '',
-                        'required' => true,
-                        'type'     => 'select',
-                        'options'  => erp_hr_get_employees_dropdown_raw()
-                    ) ); ?>
-                </li>
-            <?php } ?>
+                <ul class="erp-list blur blocked" style="width: 400px;">
 
-            <li>
-                <?php erp_html_form_input( array(
-                    'label'    => __( 'Leave Type', 'wp-erp' ),
-                    'name'     => 'leave_policy',
-                    'value'    => '',
-                    'required' => true,
-                    'type'     => 'select',
-                    'options'  => array( '' => __( '- Select -', 'wp-erp' ) ) + erp_hr_leave_get_policies_dropdown_raw()
-                ) ); ?>
-            </li>
+                    <?php if ( current_user_can( 'manage_options' ) ) { ?>
+                        <li>
+                            <?php erp_html_form_input( array(
+                                'label'    => __( 'Employee', 'wp-erp' ),
+                                'name'     => 'employee_id',
+                                'value'    => '',
+                                'required' => true,
+                                'type'     => 'select',
+                                'options'  => erp_hr_get_employees_dropdown_raw()
+                            ) ); ?>
+                        </li>
+                    <?php } ?>
 
-            <li class="two-col">
-                <div class="cols">
-                    <?php erp_html_form_input( array(
-                        'label'    => __( 'From', 'wp-erp' ),
-                        'name'     => 'leave_from',
-                        'value'    => '',
-                        'required' => true,
-                        'class'    => 'erp-date-field',
-                    ) ); ?>
-                </div>
+                    <li>
+                        <?php erp_html_form_input( array(
+                            'label'    => __( 'Leave Type', 'wp-erp' ),
+                            'name'     => 'leave_policy',
+                            'value'    => '',
+                            'required' => true,
+                            'type'     => 'select',
+                            'options'  => array( '' => __( '- Select -', 'wp-erp' ) ) + erp_hr_leave_get_policies_dropdown_raw()
+                        ) ); ?>
+                    </li>
 
-                <div class="cols last">
-                    <?php erp_html_form_input( array(
-                        'label'    => __( 'To', 'wp-erp' ),
-                        'name'     => 'leave_to',
-                        'value'    => '',
-                        'required' => true,
-                        'class'    => 'erp-date-field',
-                    ) ); ?>
-                </div>
-            </li>
+                    <li class="two-col">
+                        <div class="cols">
+                            <?php erp_html_form_input( array(
+                                'label'    => __( 'From', 'wp-erp' ),
+                                'name'     => 'leave_from',
+                                'value'    => '',
+                                'required' => true,
+                                'class'    => 'erp-date-field',
+                            ) ); ?>
+                        </div>
 
-            <li class="show-days"></li>
+                        <div class="cols last">
+                            <?php erp_html_form_input( array(
+                                'label'    => __( 'To', 'wp-erp' ),
+                                'name'     => 'leave_to',
+                                'value'    => '',
+                                'required' => true,
+                                'class'    => 'erp-date-field',
+                            ) ); ?>
+                        </div>
+                    </li>
 
-            <li>
-                <?php erp_html_form_input( array(
-                    'label'       => __( 'Reason', 'wp-erp' ),
-                    'name'        => 'leave_reason',
-                    'type'        => 'textarea',
-                    'custom_attr' => array( 'cols' => 30, 'rows' => 3 )
-                ) ); ?>
-            </li>
-        </ul>
+                    <li class="show-days"></li>
 
-        <input type="hidden" name="erp-action" value="hr-leave-req-new">
-        <?php wp_nonce_field( 'erp-leave-req-new' ); ?>
-        <?php submit_button( __( 'Submit Request', 'wp-erp' ), 'primary', 'submit', true, array( 'disabled' => 'disabled' )  ); ?>
-    </form>
+                    <li>
+                        <?php erp_html_form_input( array(
+                            'label'       => __( 'Reason', 'wp-erp' ),
+                            'name'        => 'leave_reason',
+                            'type'        => 'textarea',
+                            'custom_attr' => array( 'cols' => 30, 'rows' => 3 )
+                        ) ); ?>
+                    </li>
+                </ul>
+
+                <input type="hidden" name="erp-action" value="hr-leave-req-new">
+                <?php wp_nonce_field( 'erp-leave-req-new' ); ?>
+                <?php submit_button( __( 'Submit Request', 'wp-erp' ), 'primary', 'submit', true, array( 'disabled' => 'disabled' )  ); ?>
+            </form>
+        </div>
+    </div>
 </div><!-- .wrap -->
 
 <?php erp_get_js_template( WPERP_HRM_JS_TMPL . '/leave-days.php', 'erp-leave-days' ); ?>
