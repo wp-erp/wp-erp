9bfd1f3892b97447ba2cc33044aeb22bff81fdb9
Tareq Hasan <tareq1988@gmail.com>
2015-11-05 14:40:42 +0600

    People class

--- includes/framework/models/peoples.php
+++ includes/framework/models/peoples.php
@@ -7,4 +7,16 @@ class People extends Model {
     protected $primaryKey = 'id';
     protected $table      = 'erp_peoples';
     public $timestamps    = false;
+    
+    /**
+     * Fetch people with types
+     * 
+     * @param object $query
+     * @param string $type
+     * 
+     * @return object
+     */
+    public function scopeType( $query, $type = 'customer' ) {
+        return $query->where( 'type', '=', $type );
+    }
 }
