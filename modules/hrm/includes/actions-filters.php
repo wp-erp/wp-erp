8dd1f7273c0009405f78b605670636cd7c65cfd6
Tareq Hasan <tareq1988@gmail.com>
2016-01-07 16:55:49 +0600

    remove employee_on_initialize as duplicate, hiring date added on new employee creation logs

--- modules/hrm/includes/actions-filters.php
+++ modules/hrm/includes/actions-filters.php
@@ -2,7 +2,6 @@
 
 // Actions *****************************************************************/
 
-add_action( 'user_register', 'erp_hr_employee_on_initialize' );
 add_action( 'user_register', 'erp_hr_new_admin_as_manager' );
 add_action( 'delete_user', 'erp_hr_employee_on_delete' );
 add_action( 'set_user_role', 'erp_hr_existing_role_to_employee', 10, 2 );
