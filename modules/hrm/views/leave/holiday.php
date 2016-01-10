1cbd11ace89e6225aabb9dd9aba5bb4d50b07ef2
Tareq Hasan <tareq1988@gmail.com>
2016-01-11 03:05:25 +0600

    Typo fix on iCal button

--- modules/hrm/views/leave/holiday.php
+++ modules/hrm/views/leave/holiday.php
@@ -257,7 +257,11 @@ class Leave_Holiday_List_Table extends WP_List_Table {
 ?>
 
 <div class="wrap erp-hr-holiday-wrap">
-    <h2><?php _e( 'Holiday', 'wp-erp' ); ?> <a href="#" id="erp-hr-new-holiday" class="add-new-h2"><?php _e( 'Add New', 'wp-erp' ); ?></a><a href="#import-ical" id="erp-hr-import-ical" class="add-new-h2"><?php _e( 'Import ICal', 'wp-erp' ); ?></a></h2>
+    <h2>
+        <?php _e( 'Holiday', 'wp-erp' ); ?>
+        <a href="#" id="erp-hr-new-holiday" class="add-new-h2"><?php _e( 'Add New', 'wp-erp' ); ?></a>
+        <a href="#import-ical" id="erp-hr-import-ical" class="add-new-h2"><?php _e( 'Import iCal', 'wp-erp' ); ?></a>
+    </h2>
 
     <div class="list-table-wrap">
         <div class="list-wrap-inner">
