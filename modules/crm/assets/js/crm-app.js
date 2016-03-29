5e8e000ba4bfe83003cb50038c3d1bc81d1e11a0
Sabbir Ahmed <sabbir.081070@gmail.com>
2016-03-29 16:09:49 +0600

    Added feed delete progress feedback for UX

--- modules/crm/assets/js/crm-app.js
+++ modules/crm/assets/js/crm-app.js
@@ -413,10 +413,15 @@ Vue.component( 'timeline-item', {
                 _wpnonce : wpCRMvue.nonce
             };
 
+
             if ( confirm( wpCRMvue.confirm ) ) {
+                vm.progressStart('#timeline-item-'+feed.id );
                 jQuery.post( wpCRMvue.ajaxurl, data, function( resp ) {
                     if ( resp.success ) {
-                        vm.feeds.$remove( feed )
+                        vm.progreassDone(true);
+                        setTimeout( function() {
+                            vm.feeds.$remove( feed );
+                        }, 500);
                     } else {
                         alert( resp.data );
                     };
@@ -998,7 +1003,7 @@ var vm = new Vue({
     methods: {
 
         loadMoreContent: function( feeds ) {
-            vm.progreassStart('.feed-load-more');
+            vm.progressStart('.feed-load-more');
             this.loading = true;
             this.offset = this.offset + this.limit;
 
@@ -1095,9 +1100,9 @@ var vm = new Vue({
 
             if ( feed_id ) {
                 this.feedData.id = feed_id;
-                vm.progreassStart( '#timeline-item-'+feed_id );
+                vm.progressStart( '#timeline-item-'+feed_id );
             } else {
-                vm.progreassStart('#erp-crm-feed-nav-content');
+                vm.progressStart('#erp-crm-feed-nav-content');
             }
 
             this.feedData._wpnonce = wpCRMvue.nonce;
@@ -1227,7 +1232,7 @@ var vm = new Vue({
 
 
             if ( filter ) {
-                vm.progreassStart('#erp-crm-activities-filter');
+                vm.progressStart('#erp-crm-activities-filter');
 
                 data.customer_id = filter.customer_id;
                 data.created_by = filter.created_by;
@@ -1253,7 +1258,7 @@ var vm = new Vue({
          *
          * @param  {[string]} id
          */
-        progreassStart: function( id ) {
+        progressStart: function( id ) {
             NProgress.configure({ parent: id });
             NProgress.start();
         },
