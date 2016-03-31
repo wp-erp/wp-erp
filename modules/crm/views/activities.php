296462f4559b280343060eeb004c000226daa569
Sabbir Ahmed <sabbir.081070@gmail.com>
2016-03-31 15:29:15 +0600

    Change actvity feed js components structures

--- modules/crm/views/activities.php
+++ modules/crm/views/activities.php
@@ -56,10 +56,13 @@ $contacts = erp_crm_get_contact_dropdown();
 
                     <li v-for="feed in feed_obj">
 
-                        <i v-if="feed.type == 'email'" class="fa fa-envelope-o" @click.prevent="toggleFooter"></i>
-                        <i v-if="feed.type == 'new_note'" class="fa fa-file-text-o" @click.prevent="toggleFooter"></i>
-                        <i v-if="feed.type == 'log_activity'" class="fa fa-list" @click.prevent="toggleFooter"></i>
-                        <i v-if="( feed.type == 'log_activity' && isSchedule( feed.start_date )  )" class="fa fa-calendar-check-o" @click.prevent="toggleFooter"></i>
+                        <i v-if="feed.type == 'email'" class="fa fa-envelope-o"></i>
+                        <i v-if="feed.type == 'new_note'" class="fa fa-file-text-o"></i>
+                        <i v-if="feed.type == 'log_activity'" class="fa fa-list"></i>
+                        <i v-if="( feed.type == 'log_activity' && isSchedule( feed.start_date )  )" class="fa fa-calendar-check-o"></i>
+                        <i v-if="feed.type == 'tasks'" class="fa fa-check-square-o"></i>
+
+                        <?php do_action( 'erp_crm_customer_feed_icon' ); ?>
 
                         <timeline-item :feed="feed" disbale-footer="true"></timeline-item>
 
