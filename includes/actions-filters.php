9023c37298359be066ffbf1e61fabb1727b602bd
Sohel Amin <sohelamincse@gmail.com>
2016-04-18 16:10:02 +0000

    CSV import/export tool added

--- includes/actions-filters.php
+++ includes/actions-filters.php
@@ -7,6 +7,8 @@ add_action( 'admin_init', 'erp_process_actions' );
 add_action( 'admin_notices', 'erp_activation_notice' );
 add_action( 'admin_footer', 'erp_activation_notice_javascript' );
 add_action( 'wp_ajax_nopriv_erp_api_mode_change', 'erp_api_mode_change' );
+add_action( 'admin_footer', 'erp_import_export_javascript' );
+add_action( 'admin_init', 'erp_process_import_export' );
 
 /** Filters *******************************************************************/
 
