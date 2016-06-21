e48047da9fd0735f52ba483d96ddf19b0a88f4c9
Sohel Amin <sohelamincse@gmail.com>
2016-06-21 04:35:16 +0000

    Employee todays birthday check through cron

--- modules/hrm/includes/actions-filters.php
+++ modules/hrm/includes/actions-filters.php
@@ -10,6 +10,7 @@ add_action( 'set_user_role', 'erp_hr_existing_role_to_employee', 10, 2 );
 // After create employee apply leave policy
 add_action( 'erp_hr_employee_new', 'erp_hr_apply_new_employee_policy', 10, 1 );
 add_action( 'erp_daily_scheduled_events', 'erp_hr_apply_policy_schedule' );
+add_action( 'erp_daily_scheduled_events', 'erp_hr_schedule_check_todays_birthday' );
 
 add_action( 'admin_init', 'erp_hr_apply_entitlement_yearly' );
 
