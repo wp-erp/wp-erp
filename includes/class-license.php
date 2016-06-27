f8346b8e88b45673dbf3b0ccf99b25ca921c21ad
Sohel Amin <sohelamincse@gmail.com>
2016-06-27 04:14:59 +0000

    include .dev as local server while checking license

--- includes/class-license.php
+++ includes/class-license.php
@@ -50,7 +50,7 @@ class License {
      * @return boolean
      */
     private function is_local_server() {
-        return in_array( $_SERVER['REMOTE_ADDR'], array( '127.0.0.1', '::1' ) );
+        return ( in_array( $_SERVER['REMOTE_ADDR'], array( '127.0.0.1', '::1' ) ) || substr( $_SERVER['HTTP_HOST'], -4 ) == '.dev' );
     }
 
     /**
