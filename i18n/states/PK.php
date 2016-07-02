65a671f7dee9a4515c1948bc846bb43fa177cfb6
saqibj <saqib.jawaid@circles.consulting>
2016-07-02 16:28:08 +0500

    Add files via upload

---
+++ i18n/states/PK.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Pakistan states
+ *
+ * @category    i18n
+ * @package     WPERP/i18n
+ */
+global $states;
+
+$states['PK'] = array(
+	'AJK'  => __( 'Azad Jammu & Kashmir', 'erp' ),
+	'BN'  => __( 'Balochistan', 'erp' ),
+	'FATA'  => __( 'Federally Administered Tribal Areas ', 'erp' ),
+	'GB' => __( 'Gilgit-Baltistan', 'erp' ),
+	'ICT'  => __( 'Islamabad Capital Territory', 'erp' ),
+	'KP'  => __( 'Khyber Pakhtunkhwa', 'erp' ),
+	'PB'  => __( 'Punjab', 'erp' ),
+	'SD'  => __( 'Sindh', 'erp' ),
+);
