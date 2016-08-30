498d343901d8bb02e1ff117cb9e963dc0b93f631
Asaquzzaman Mishu <joy.mishu@gmail.com>
2016-08-30 13:07:02 +0600

    hrm holiday end date increase by +1day

---
+++ includes/updates/update-1.1.5.php
@@ -0,0 +1,12 @@
+<?php
+$results = \WeDevs\ERP\HRM\Models\Leave_Holiday::select('id', 'end')->get();
+        
+if ( $results ) {
+    foreach ( $results as $key => $result ) {
+        $date = new \DateTime($result->end);
+        $date->modify('+1 day');
+        $new_date = $date->format('Y-m-d H:i:s') ;
+        \WeDevs\ERP\HRM\Models\Leave_Holiday::where( 'id', '=', $result->id )->update(['end' => $new_date]);
+    }
+}
+
