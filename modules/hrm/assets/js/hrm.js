f7099a871866ad9cba23fd682830f0eb2c7925eb
Sabbir Ahmed <sabbir.081070@gmail.com>
2016-11-14 11:18:43 +0600

    List table checkbox toggling issue fixed in hodilay and Department list table

--- modules/hrm/assets/js/hrm.js
+++ modules/hrm/assets/js/hrm.js
@@ -80,8 +80,80 @@
             $('body').on( 'erp-hr-after-new-desig', this.designation.afterNew );
 
             this.initTipTip();
+        },
 
-            // this.employee.addWorkExperience();
+        initToggleCheckbox: function() {
+            var lastClicked = false;
+
+            // check all checkboxes
+            $('tbody').children().children('.check-column').find(':checkbox').click( function(e) {
+                if ( 'undefined' == e.shiftKey ) { return true; }
+                if ( e.shiftKey ) {
+                    if ( ! lastClicked ) {
+                        return true;
+                    }
+
+                    checks  = $( lastClicked ).closest( 'form' ).find( ':checkbox' ).filter( ':visible:enabled' );
+                    first   = checks.index( lastClicked );
+                    last    = checks.index( this );
+                    checked = $(this).prop('checked');
+
+                    if ( 0 < first && 0 < last && first != last ) {
+                        sliced = ( last > first ) ? checks.slice( first, last ) : checks.slice( last, first );
+                        sliced.prop( 'checked', function() {
+                            if ( $(this).closest('tr').is(':visible') )
+                                return checked;
+
+                            return false;
+                        });
+                    }
+                }
+
+                lastClicked = this;
+
+                // toggle "check all" checkboxes
+                var unchecked = $(this).closest('tbody').find(':checkbox').filter(':visible:enabled').not(':checked');
+                $(this).closest('table').children('thead, tfoot').find(':checkbox').prop('checked', function() {
+                    return ( 0 === unchecked.length );
+                });
+
+                return true;
+            });
+
+            $('thead, tfoot').find('.check-column :checkbox').on( 'click.wp-toggle-checkboxes', function( event ) {
+                var $this          = $(this),
+                    $table         = $this.closest( 'table' ),
+                    controlChecked = $this.prop('checked'),
+                    toggle         = event.shiftKey || $this.data('wp-toggle');
+
+                $table.children( 'tbody' ).filter(':visible')
+                    .children().children('.check-column').find(':checkbox')
+                    .prop('checked', function() {
+                        if ( $(this).is(':hidden,:disabled') ) {
+                            return false;
+                        }
+
+                        if ( toggle ) {
+                            return ! $(this).prop( 'checked' );
+                        } else if ( controlChecked ) {
+                            return true;
+                        }
+
+                        return false;
+                    });
+
+                $table.children('thead,  tfoot').filter(':visible')
+                    .children().children('.check-column').find(':checkbox')
+                    .prop('checked', function() {
+                        if ( toggle ) {
+                            return false;
+                        } else if ( controlChecked ) {
+                            return true;
+                        }
+
+                        return false;
+                    });
+            });
         },
 
         initTipTip: function() {
@@ -203,7 +275,9 @@
              * @return {void}
              */
             reload: function() {
-                $( '#erp-dept-table-wrap' ).load( window.location.href + ' #erp-dept-table-wrap' );
+                $( '#erp-dept-table-wrap' ).load( window.location.href + ' #erp-dept-table-wrap', function() {
+                    WeDevs_ERP_HR.initToggleCheckbox();
+                } );
             },
 
             /**
--- modules/hrm/assets/js/leave.js
+++ modules/hrm/assets/js/leave.js
@@ -35,6 +35,79 @@
             this.initDateField();
         },
 
+        initToggleCheckbox: function() {
+            var lastClicked = false;
+            // check all checkboxes
+            $('tbody').children().children('.check-column').find(':checkbox').click( function(e) {
+                if ( 'undefined' == e.shiftKey ) { return true; }
+                if ( e.shiftKey ) {
+                    if ( ! lastClicked ) {
+                        return true;
+                    }
+
+                    checks  = $( lastClicked ).closest( 'form' ).find( ':checkbox' ).filter( ':visible:enabled' );
+                    first   = checks.index( lastClicked );
+                    last    = checks.index( this );
+                    checked = $(this).prop('checked');
+
+                    if ( 0 < first && 0 < last && first != last ) {
+                        sliced = ( last > first ) ? checks.slice( first, last ) : checks.slice( last, first );
+                        sliced.prop( 'checked', function() {
+                            if ( $(this).closest('tr').is(':visible') )
+                                return checked;
+
+                            return false;
+                        });
+                    }
+                }
+
+                lastClicked = this;
+
+                // toggle "check all" checkboxes
+                var unchecked = $(this).closest('tbody').find(':checkbox').filter(':visible:enabled').not(':checked');
+                $(this).closest('table').children('thead, tfoot').find(':checkbox').prop('checked', function() {
+                    return ( 0 === unchecked.length );
+                });
+
+                return true;
+            });
+
+            $('thead, tfoot').find('.check-column :checkbox').on( 'click.wp-toggle-checkboxes', function( event ) {
+                var $this          = $(this),
+                    $table         = $this.closest( 'table' ),
+                    controlChecked = $this.prop('checked'),
+                    toggle         = event.shiftKey || $this.data('wp-toggle');
+
+                $table.children( 'tbody' ).filter(':visible')
+                    .children().children('.check-column').find(':checkbox')
+                    .prop('checked', function() {
+                        if ( $(this).is(':hidden,:disabled') ) {
+                            return false;
+                        }
+
+                        if ( toggle ) {
+                            return ! $(this).prop( 'checked' );
+                        } else if ( controlChecked ) {
+                            return true;
+                        }
+
+                        return false;
+                    });
+
+                $table.children('thead,  tfoot').filter(':visible')
+                    .children().children('.check-column').find(':checkbox')
+                    .prop('checked', function() {
+                        if ( toggle ) {
+                            return false;
+                        } else if ( controlChecked ) {
+                            return true;
+                        }
+
+                        return false;
+                    });
+            });
+        },
+
         initDateField: function() {
             $( '.erp-leave-date-field' ).datepicker({
                 dateFormat: 'yy-mm-dd',
@@ -86,6 +159,7 @@
                     onReady: function() {
                         Leave.initDateField();
                         Leave.holiday.checkRange();
+                        Leave.initToggleCheckbox();
                     },
                     onSubmit: function(modal) {
                         e.data.holiday.submit.call(this, modal);
@@ -173,6 +247,7 @@
 
                         $( '.list-table-wrap' ).load( window.location.href + ' .list-wrap-inner', function() {
                             Leave.initDateField();
+                            Leave.initToggleCheckbox();
                         } );
                     },
                     error: function(error) {
