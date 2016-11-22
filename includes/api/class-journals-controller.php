f0ac16a50ac2c8b119cd1f4cd692378d483047a4
Sohel Amin <sohelamincse@gmail.com>
2016-11-22 14:42:35 +0600

    Journal API changes

--- includes/api/class-journals-controller.php
+++ includes/api/class-journals-controller.php
@@ -46,6 +46,15 @@ class Journals_Controller extends REST_Controller {
                     'context' => $this->get_context_param( [ 'default' => 'view' ] ),
                 ],
             ],
+            [
+                'methods'  => WP_REST_Server::EDITABLE,
+                'callback' => [ $this, 'update_journal' ],
+                'args'     => $this->get_endpoint_args_for_item_schema( WP_REST_Server::EDITABLE ),
+            ],
+            [
+                'methods'  => WP_REST_Server::DELETABLE,
+                'callback' => [ $this, 'delete_journal' ],
+            ],
             'schema' => [ $this, 'get_public_item_schema' ],
         ] );
     }
@@ -64,11 +73,8 @@ class Journals_Controller extends REST_Controller {
             'type'   => 'journal',
         ];
 
-        $items          = erp_ac_get_ledger_transactions( $args );
-        $args['number'] = -1;
-        $total_items    = count( erp_ac_get_ledger_transactions( $args ) );
-
-        // dd( $items );
+        $items       = erp_ac_get_all_transaction( ['type' => 'journal', 'join' => ['journals'], 'with_ledger' => true] );
+        $total_items = erp_ac_get_transaction_count( ['type' => 'journal'] );
 
         $formated_items = [];
         foreach ( $items as $item ) {
@@ -93,25 +99,13 @@ class Journals_Controller extends REST_Controller {
      */
     public function get_journal( $request ) {
         $id   = (int) $request['id'];
-        $item = \WeDevs\ERP\Accounting\Model\Transaction::find( $id );
+        $item = erp_ac_get_transaction( $id, ['type' => 'journal', 'join' => ['journals'], 'with_ledger' => true, 'output_by' => 'object'] );
 
         if ( empty( $id ) || empty( $item->id ) || $item->type != 'journal' ) {
             return new WP_Error( 'rest_journal_invalid_id', __( 'Invalid resource id.' ), [ 'status' => 404 ] );
         }
 
-        $transaction_items = $item->items->toArray();
-        $items             = [];
-        foreach ( $transaction_items as $transaction_item ) {
-            $items[] = [
-                'journal_id' => (int) $transaction_item['journal_id'],
-                'unit_price' => (float) $transaction_item['unit_price'],
-                'total'      => (float) $transaction_item['line_total'],
-            ];
-        }
-
-        dd($transaction_items);
-
-        $additional_fields = ['items' => $items];
+        $additional_fields = [];
 
         $item     = $this->prepare_item_for_response( $item, $request, $additional_fields );
         $response = rest_ensure_response( $item );
@@ -128,78 +122,15 @@ class Journals_Controller extends REST_Controller {
      */
     public function create_journal( $request ) {
         $trans_data = $this->prepare_item_for_database( $request );
+        $items      = $request['items'];
 
-        if ( empty( $request['form_type'] ) || ! in_array( $request['form_type'], ['payment', 'invoice'] ) ) {
-            return new WP_Error( 'rest_sale_invalid_form_type', __( 'Invalid form type.' ), [ 'status' => 404 ] );
-        }
-
-        if ( empty( $request['customer'] ) ) {
-            return new WP_Error( 'rest_sale_invalid_customer', __( 'Required customer.' ), [ 'status' => 404 ] );
-        }
-
-        if ( $trans_data['form_type'] == 'payment' ) {
-            $due_transactions = erp_ac_get_all_transaction([
-                'status'      => ['in' => ['awaiting_payment', 'partial']],
-                'user_id'     => $request['customer'],
-                'parent'      => 0,
-                'type'        => 'sales',
-                'join'        => ['journals'],
-                'with_ledger' => true,
-                'output_by'   => 'array',
-            ]);
-
-            if ( ! empty( $due_transactions ) && empty( $request['partial_id'] ) ) {
-                $due_items = [];
-
-                foreach ( $due_transactions as $due_transaction ) {
-                    $due_items[] = [
-                        'transaction_id' => (int) $due_transaction['id'],
-                        'account_id'     => 1,
-                        'due'            => (float) $due_transaction['due'],
-                    ];
-                }
-
-                $error_response = rest_ensure_response( [
-                    'code'    => 'rest_sale_due_invoices',
-                    'message' => __( 'You\'ve some due invoices.', 'erp' ),
-                    'data'    => $due_items
-                ] );
-                $error_response->set_status( 404 );
-
-                return $error_response;
-            }
-
-            if ( ! empty( $request['partial_id'] ) ) {
-                $trans_data['status'] = 'closed';
-            }
-        }
-
-        $items = $this->prepare_trans_items_for_database( $request );
-
-        $tax_total = array_reduce( $items, function( $total, $value ) {
-            return $total + $value['tax_rate'];
-        } );
-
-        $trans_data['sub_total'] = array_reduce( $items, function( $total, $value ) {
-            return $total + $value['line_total'];
-        } );
-
-        $trans_data['trans_total'] = $trans_data['sub_total'] + $tax_total;
-        $trans_data['total']       = $trans_data['trans_total'];
-        $trans_data['line_total']  = array_pluck( $items, 'line_total' );
-
-        if ( $trans_data['form_type'] == 'invoice' ) {
-            $trans_data['due'] = $trans_data['total'];
-        }
-
-        $id = erp_ac_insert_transaction( $trans_data, $items );
+        $id = erp_ac_new_journal( $trans_data, $items );
 
         if ( is_wp_error( $id ) ) {
             return $id;
         }
 
-        $transaction = \WeDevs\ERP\Accounting\Model\Transaction::find( $id );
-        $transaction->items = $transaction->items->toArray();
+        $transaction = erp_ac_get_transaction( $id, ['type' => 'journal', 'join' => ['journals'], 'with_ledger' => true, 'output_by' => 'object'] );
 
         $request->set_param( 'context', 'edit' );
         $response = $this->prepare_item_for_response( $transaction, $request );
@@ -220,27 +151,10 @@ class Journals_Controller extends REST_Controller {
     protected function prepare_item_for_database( $request ) {
         $prepared_item = [];
 
-        $prepared_item['id']              = isset( $request['id'] ) ? intval( $request['id'] ) : 0;
-        $prepared_item['type']            = 'sales';
-        $prepared_item['form_type']       = isset( $request['form_type'] ) ? sanitize_text_field( $request['form_type'] ) : '';
-        $prepared_item['account_id']      = isset( $request['account_id'] ) ? intval( $request['account_id'] ) : 0;
-
-        if ( $request['form_type'] == 'payment' ) {
-            $prepared_item['partial_id']  = ! empty( $request['partial_id'] ) ? explode( ",", str_replace( " ", "", $request['partial_id'] ) ) : [];
-        }
-
-        if ( $request['form_type'] == 'invoice' ) {
-            $prepared_item['account_id'] = 1;
-        }
-
-        $prepared_item['status']          = ( $prepared_item['form_type'] == 'payment' ) ? 'closed' : 'draft';
-        $prepared_item['user_id']         = isset( $request['customer'] ) ? intval( $request['customer'] ) : 0;
-        $prepared_item['billing_address'] = isset( $request['billing_address'] ) ? wp_kses_post( $request['billing_address'] ) : '';
-        $prepared_item['ref']             = isset( $request['reference'] ) ? sanitize_text_field( $request['reference'] ) : '';
-        $prepared_item['issue_date']      = isset( $request['issue_date'] ) ? sanitize_text_field( $request['issue_date'] ) : '';
-        $prepared_item['due_date']        = isset( $request['due_date'] ) ? sanitize_text_field( $request['due_date'] ) : '';
-        $prepared_item['summary']         = isset( $request['summary'] ) ? wp_kses_post( $request['summary'] ) : '';
-        $prepared_item['currency']        = isset( $request['currency'] ) ? sanitize_text_field( $request['currency'] ) : 'USD';
+        $prepared_item['type']       = 'journal';
+        $prepared_item['ref']        = isset( $request['reference'] ) ? sanitize_text_field( $request['reference'] ) : '';
+        $prepared_item['issue_date'] = isset( $request['issue_date'] ) ? sanitize_text_field( $request['issue_date'] ) : '';
+        $prepared_item['summary']    = isset( $request['summary'] ) ? wp_kses_post( $request['summary'] ) : '';
 
         return $prepared_item;
     }
@@ -256,14 +170,13 @@ class Journals_Controller extends REST_Controller {
      */
     public function prepare_item_for_response( $item, $request, $additional_fields = [] ) {
         $data = [
-            'id'         => (int) $item->id,
-            'reference'  => $item->ref,
-            'issue_date' => $item->issue_date,
-            'summary'    => $item->summary,
-            'debit'      => (float) $item->debit,
-            'credit'     => (float) $item->credit,
-            'total'      => (float) $item->total,
-            'created_at' => $item->created_at,
+            'id'          => (int) $item->id,
+            'reference'   => $item->ref,
+            'summary'     => $item->summary,
+            'issue_date'  => $item->issue_date,
+            'items'       => $this->format_transaction_items( $item->journals ),
+            'total'       => (float) $item->total,
+            'trans_total' => (float) $item->trans_total,
         ];
 
         if ( isset( $request['include'] ) ) {
@@ -285,6 +198,25 @@ class Journals_Controller extends REST_Controller {
     }
 
     /**
+     * Format the transaction's items
+     *
+     * @param  array $items
+     *
+     * @return array
+     */
+    protected function format_transaction_items( $items ) {
+        return array_map( function( $item ) {
+            return [
+                'id'          => (int) $item['id'],
+                'debit'       => (float) $item['debit'],
+                'credit'      => (float) $item['credit'],
+                'ledger_id'   => (int) $item['ledger']['id'],
+                'ledger_name' => isset( $item['ledger']['name'] ) ? $item['ledger']['name'] : '',
+            ];
+        }, $items );
+    }
+
+    /**
      * Get the User's schema, conforming to JSON Schema
      *
      * @return array
@@ -292,7 +224,7 @@ class Journals_Controller extends REST_Controller {
     public function get_item_schema() {
         $schema = [
             '$schema'    => 'http://json-schema.org/draft-04/schema#',
-            'title'      => 'sale',
+            'title'      => 'journal',
             'type'       => 'object',
             'properties' => [
                 'id'          => [
