b5b550f3bbb91807c6ebe8cd38572f476430871c
Asaquzzaman Mishu <joy.mishu@gmail.com>
2016-12-06 11:47:21 +0600

    Set minimum discount value at transaction time

--- modules/accounting/includes/views/common/transaction-table-tr-render.php
+++ modules/accounting/includes/views/common/transaction-table-tr-render.php
@@ -38,7 +38,7 @@
                             //'placeholder' => 1,
                             'value'       => isset( $item['qty'] ) && $item['qty'] ? $item['qty'] : 1,
                             'class'       => 'line_qty',
-                            'custom_attr' => [ 'min' => 1 ]
+                            'custom_attr' => [ 'min' => 1, 'step' => 'any' ]
                         ) );
                         ?>
                     </td>
@@ -78,7 +78,7 @@
                             'addon_pos'   => 'after',
                             'class'       => 'line_dis',
                             'value'       => isset( $item['discount'] ) ? $item['discount'] : '0',
-                            'custom_attr' => array( 'max' => 100, 'step' => 'any' )
+                            'custom_attr' => array( 'max' => 100, 'min' => '0', 'step' => 'any' )
                         ) );
                         ?>
                     </td>
