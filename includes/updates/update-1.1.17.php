e3fc2f6aa61ac1fd8ad379cd0629499e400ab7f1
Edi Amin <to.ediamin@gmail.com>
2017-03-29 17:54:08 +0600

    Change employee pay_rate column int to decimal in db
    Fixes #431

--- includes/updates/update-1.1.17.php
+++ includes/updates/update-1.1.17.php
@@ -16,6 +16,14 @@ function erp_crm_update_table_1_1_17() {
     if ( ! in_array( 'hash', $cols ) ) {
         $wpdb->query( "ALTER TABLE $table ADD `hash` VARCHAR(40) NULL DEFAULT NULL AFTER `unsubscribe_at`;" );
     }
+
+    // Add hash column in `erp_crm_contact_subscriber` table
+    $table = $wpdb->prefix . 'erp_hr_employees';
+    $cols  = $wpdb->get_col( "DESC $table");
+
+    if ( in_array( 'pay_rate', $cols ) ) {
+        $wpdb->query( "ALTER TABLE $table CHANGE `pay_rate` `pay_rate` DECIMAL(11,2) UNSIGNED NOT NULL DEFAULT '0.00';" );
+    }
 }
 
 erp_crm_update_table_1_1_17();
