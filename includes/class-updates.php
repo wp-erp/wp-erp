8b75f0adaf3378c934caa5d5888c6922df6ea632
Edi Amin <to.ediamin@gmail.com>
2017-07-13 09:50:06 +0600

    Prepare release v1.2.2

--- includes/class-updates.php
+++ includes/class-updates.php
@@ -36,7 +36,7 @@ class Updates {
         '1.1.9'  => 'updates/update-1.1.9.php',
         '1.1.17' => 'updates/update-1.1.17.php',
         '1.2.1'  => 'updates/update-1.2.1.php',
-        '1.2.2-rc.1'  => 'updates/update-1.2.2-rc.1.php',
+        '1.2.2'  => 'updates/update-1.2.2.php',
     ];
 
     /**
