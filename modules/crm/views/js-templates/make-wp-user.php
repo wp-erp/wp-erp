4fbd95467c2d9c7a735d832455178cc64031abb9
Ahmed Kandil <a.kandel@iwassa.com>
2017-08-22 14:47:30 +0200

    Create make-wp-user.php

---
+++ modules/crm/views/js-templates/make-wp-user.php
@@ -0,0 +1,27 @@
+<div class="erp-crm-customer-make-wpuser" id="erp-crm-customer-make-wpuser">
+    <# if ( '' == data.email ) { #>
+        <div class="row">
+            <label for="make-wp-user-customer-email"><?php _e( 'Enter your email', 'erp' ); ?></label>
+            <input type="email" name="customer_email" id="make-wp-user-customer-email" placeholder="<?php echo 'Enter your email' ?>" required>
+        </div>
+    <# } #>
+    <div class="row">
+        <label for="wp-user-role"><?php _e( 'Role', 'erp' ) ?></label>
+        <select name="customer_role" id="wp-user-role">
+            <?php erp_dropdown_roles( get_option('default_role') ); ?>
+        </select>
+    </div>
+
+    <div class="row">
+        <label for="send-password-to-email"><?php _e( 'Send password' ) ?></label>
+        <span class="checkbox">
+            <label for="send-password-to-email">
+                <input type="checkbox" id="send-password-to-email" name="send_password_notification"> <?php _e( 'Send password to this user email', 'erp' ) ?>
+            </label>
+        </span>
+    </div>
+    <?php wp_nonce_field( 'erp-crm-make-wp-user' ); ?>
+    <input type="hidden" name="action" value="erp-crm-make-wp-user">
+    <input type="hidden" name="id" value="{{ data.id }}">
+    <input type="hidden" name="type" value="{{ data.type }}">
+</div>
