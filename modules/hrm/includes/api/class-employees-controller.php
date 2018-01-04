d29fec1947e29c5cb5f09d51c5b801ffb41a2441
sultann <manikdrmc@gmail.com>
2018-01-04 11:48:55 +0600

    HRM API create employee refactor

--- modules/hrm/includes/api/class-employees-controller.php
+++ modules/hrm/includes/api/class-employees-controller.php
@@ -155,6 +155,53 @@ class Employees_Controller extends REST_Controller {
     }
 
     /**
+     * Create employees
+     *
+     * @param $request
+     *
+     * @return $this|int|\WP_Error|\WP_REST_Response
+     */
+    public function create_employees( \WP_REST_Request $request ) {
+        $employees = json_decode( $request->get_body(), true );
+
+        foreach ( $employees as $employee ) {
+            $item_data = $this->prepare_item_for_database( $employee );
+            $item      = new Employee( null );
+            $created   = $item->create_employee( $item_data );
+            if ( is_wp_error( $created ) ) {
+                return $created;
+            }
+        }
+
+        return new WP_REST_Response( true, 201 );
+    }
+
+    /**
+     * Create an employee
+     *
+     * @param \WP_REST_Request $request
+     *
+     * @return WP_Error|\WP_REST_Request
+     */
+    public function create_employee( $request ) {
+        $item_data = $this->prepare_item_for_database( $request );
+
+        $employee = new Employee( null );
+        $created  = $employee->create_employee( $item_data );
+        if ( is_wp_error( $created ) ) {
+            return $created;
+        }
+        $request->set_param( 'context', 'edit' );
+        $response = $this->prepare_item_for_response( $employee, $request );
+        $response = rest_ensure_response( $response );
+        $response->set_status( 201 );
+        $response->header( 'Location', rest_url( sprintf( '/%s/%s/%d', $this->namespace, $this->rest_base, $employee->get_user_id() ) ) );
+
+        return $response;
+    }
+
+
+    /**
      * Prepare a single user output for response
      *
      * @param \WeDevs\ERP\HRM\Employee $item
@@ -200,7 +247,7 @@ class Employees_Controller extends REST_Controller {
             'postal_code'     => '',
         ];
 
-        $data = wp_parse_args($item->get_data(array(), true ), $default);
+        $data = wp_parse_args( $item->get_data( array(), true ), $default );
 
         if ( isset( $request['include'] ) ) {
             $include_params = explode( ',', str_replace( ' ', '', $request['include'] ) );
@@ -237,6 +284,164 @@ class Employees_Controller extends REST_Controller {
         $response = $this->add_links( $response, $item );
 
         return $response;
+    }
+
+    /**
+     * Prepare a single item for create or update
+     *
+     * @param \WP_REST_Request $request Request object.
+     *
+     * @return array $prepared_item
+     */
+    protected function prepare_item_for_database( $request ) {
+        $prepared_item = [];
+
+        // required arguments.
+        if ( isset( $request['first_name'] ) ) {
+            $prepared_item['personal']['first_name'] = $request['first_name'];
+        }
+
+        if ( isset( $request['last_name'] ) ) {
+            $prepared_item['personal']['last_name'] = $request['last_name'];
+        }
+
+        if ( isset( $request['employee_id'] ) ) {
+            $prepared_item['work']['employee_id'] = $request['employee_id'];
+        }
+
+        if ( isset( $request['email'] ) ) {
+            $prepared_item['user_email'] = $request['email'];
+        }
+
+        // optional arguments.
+        if ( isset( $request['user_id'] ) ) {
+            $prepared_item['user_id'] = absint( $request['user_id'] );
+        }
+
+        if ( isset( $request['middle_name'] ) ) {
+            $prepared_item['personal']['middle_name'] = $request['middle_name'];
+        }
+
+        if ( isset( $request['designation'] ) ) {
+            $prepared_item['work']['designation'] = $request['designation'];
+        }
+
+        if ( isset( $request['department'] ) ) {
+            $prepared_item['work']['department'] = $request['department'];
+        }
+
+        if ( isset( $request['reporting_to'] ) ) {
+            $prepared_item['work']['reporting_to'] = $request['reporting_to'];
+        }
+
+        if ( isset( $request['location'] ) ) {
+            $prepared_item['work']['location'] = $request['location'];
+        }
+
+        if ( isset( $request['hiring_source'] ) ) {
+            $prepared_item['work']['hiring_source'] = $request['hiring_source'];
+        }
+
+        if ( isset( $request['hiring_date'] ) ) {
+            $prepared_item['work']['hiring_date'] = $request['hiring_date'];
+        }
+
+        if ( isset( $request['date_of_birth'] ) ) {
+            $prepared_item['work']['date_of_birth'] = $request['date_of_birth'];
+        }
+
+        if ( isset( $request['pay_rate'] ) ) {
+            $prepared_item['work']['pay_rate'] = $request['pay_rate'];
+        }
+
+        if ( isset( $request['pay_type'] ) ) {
+            $prepared_item['work']['pay_type'] = $request['pay_type'];
+        }
+
+        if ( isset( $request['type'] ) ) {
+            $prepared_item['work']['type'] = $request['type'];
+        }
+
+        if ( isset( $request['status'] ) ) {
+            $prepared_item['work']['status'] = $request['status'];
+        }
+
+        if ( isset( $request['other_email'] ) ) {
+            $prepared_item['personal']['other_email'] = $request['other_email'];
+        }
+
+        if ( isset( $request['phone'] ) ) {
+            $prepared_item['personal']['phone'] = $request['phone'];
+        }
+
+        if ( isset( $request['work_phone'] ) ) {
+            $prepared_item['personal']['work_phone'] = $request['work_phone'];
+        }
+
+        if ( isset( $request['mobile'] ) ) {
+            $prepared_item['personal']['mobile'] = $request['mobile'];
+        }
+
+        if ( isset( $request['address'] ) ) {
+            $prepared_item['personal']['address'] = $request['address'];
+        }
+
+        if ( isset( $request['gender'] ) ) {
+            $prepared_item['personal']['gender'] = $request['gender'];
+        }
+
+        if ( isset( $request['marital_status'] ) ) {
+            $prepared_item['personal']['marital_status'] = $request['marital_status'];
+        }
+
+        if ( isset( $request['nationality'] ) ) {
+            $prepared_item['personal']['nationality'] = $request['nationality'];
+        }
+
+        if ( isset( $request['driving_license'] ) ) {
+            $prepared_item['personal']['driving_license'] = $request['driving_license'];
+        }
+
+        if ( isset( $request['hobbies'] ) ) {
+            $prepared_item['personal']['hobbies'] = $request['hobbies'];
+        }
+
+        if ( isset( $request['user_url'] ) ) {
+            $prepared_item['personal']['user_url'] = $request['user_url'];
+        }
+
+        if ( isset( $request['description'] ) ) {
+            $prepared_item['personal']['description'] = $request['description'];
+        }
+
+        if ( isset( $request['street_1'] ) ) {
+            $prepared_item['personal']['street_1'] = $request['street_1'];
+        }
+
+        if ( isset( $request['street_2'] ) ) {
+            $prepared_item['personal']['street_2'] = $request['street_2'];
+        }
+
+        if ( isset( $request['city'] ) ) {
+            $prepared_item['personal']['city'] = $request['city'];
+        }
+
+        if ( isset( $request['country'] ) ) {
+            $prepared_item['personal']['country'] = $request['country'];
+        }
+
+        if ( isset( $request['state'] ) ) {
+            $prepared_item['personal']['state'] = $request['state'];
+        }
+
+        if ( isset( $request['postal_code'] ) ) {
+            $prepared_item['personal']['postal_code'] = $request['postal_code'];
+        }
+        if ( isset( $request['photo_id'] ) ) {
+            $prepared_item['personal']['photo_id'] = $request['photo_id'];
+        }
+
+        return $prepared_item;
     }
 
 
