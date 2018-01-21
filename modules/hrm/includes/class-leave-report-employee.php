fde138ecbb700a88625cb23b26aa89c75819764b
MD Sultan Nasir Uddin <manikdrmc@gmail.com>
2018-01-21 19:30:55 +0600

    leave report filter add

--- modules/hrm/includes/class-leave-report-employee.php
+++ modules/hrm/includes/class-leave-report-employee.php
@@ -24,6 +24,66 @@ class Leave_Report_Employee_Based extends \WP_List_Table {
         $this->policies = \WeDevs\ERP\HRM\Models\Leave_Policies::select( 'name', 'id' )->get();
     }
 
+    /**
+     * Render extra filtering option in
+     * top of the table
+     *
+     * @since 0.1
+     *
+     * @param  string $which
+     *
+     * @return void
+     */
+    function extra_tablenav( $which ) {
+        if ( $which != 'top' ) {
+            return;
+        }
+        $selected_desingnation = ( isset( $_GET['filter_designation'] ) ) ? $_GET['filter_designation'] : 0;
+        $selected_department   = ( isset( $_GET['filter_department'] ) ) ? $_GET['filter_department'] : 0;
+        $selected_type         = ( isset( $_GET['filter_employment_type'] ) ) ? $_GET['filter_employment_type'] : '';
+        $selected_time         = ( isset( $_GET['filter_time'] ) ) ? $_GET['filter_time'] : date( 'Y' );
+        $current_year          = date( 'Y' );
+        ?>
+        <div class="actions alignleft">
+            <label class="screen-reader-text" for="new_role"><?php _e( 'Filter by Designation', 'erp' ) ?></label>
+            <select name="filter_year" id="filter_year">
+                <?php
+                for ( $i = 0; $i <= 5; $i ++ ) {
+                    $year = $current_year - $i;
+                    echo sprintf( "<option value='%s'%s>%s</option>\n", $year, selected( $selected_time, $year, false ), $year );
+
+                }
+                ?>
+            </select>
+
+            <select name="filter_designation" id="filter_designation">
+                <?php echo erp_hr_get_designation_dropdown( $selected_desingnation ); ?>
+            </select>
+
+            <label class="screen-reader-text" for="new_role"><?php _e( 'Filter by Designation', 'erp' ) ?></label>
+            <select name="filter_department" id="filter_department">
+                <?php echo erp_hr_get_departments_dropdown( $selected_department ); ?>
+            </select>
+
+            <label class="screen-reader-text" for="new_role"><?php _e( 'Filter by Employment Type', 'erp' ) ?></label>
+            <select name="filter_employment_type" id="filter_employment_type">
+                <option value="-1"><?php _e( '- Select Employment Type -', 'erp' ) ?></option>
+                <?php
+                $types = erp_hr_get_employee_types();
+
+                foreach ( $types as $key => $title ) {
+                    echo sprintf( "<option value='%s'%s>%s</option>\n", $key, selected( $selected_type, $key, false ), $title );
+                }
+                ?>
+            </select>
+
+            <?php wp_nonce_field( 'epr-rep-leaves' ); ?>
+            <?php submit_button( __( 'Filter' ), 'apply', 'filter_leave_report', false ); ?>
+        </div>
+        <?php
+    }
+
+
 
     function get_table_classes() {
         return array( 'widefat', 'fixed', 'striped', $this->_args['plural'] );
@@ -114,11 +174,28 @@ class Leave_Report_Employee_Based extends \WP_List_Table {
         $current_page = $this->get_pagenum();
         $offset       = ( $current_page - 1 ) * $per_page;
 
+        $selected_desingnation = ( isset( $_GET['filter_designation'] ) ) ? $_GET['filter_designation'] : 0;
+        $selected_department   = ( isset( $_GET['filter_department'] ) ) ? $_GET['filter_department'] : 0;
+        $selected_type         = ( isset( $_GET['filter_employment_type'] ) ) ? $_GET['filter_employment_type'] : '';
+        $selected_time         = ( isset( $_GET['filter_time'] ) ) ? $_GET['filter_time'] : date( 'Y' );
+
         $query         = \WeDevs\ERP\HRM\Models\Employee::where( 'status', 'active' )->select( 'user_id' );
+        if ( $selected_department && '-1' != $selected_department ) {
+            $query->where( 'department', intval( $selected_department ) );
+        }
+
+        if ( $selected_desingnation && '-1' != $selected_desingnation ) {
+            $query->where( 'designation', intval( $selected_desingnation ) );
+        }
+
+        if ( $selected_type && '-1' != $selected_type ) {
+            $query->where( 'type', $selected_type );
+        }
+
         $employees_obj = $query->skip( $offset )->take( $per_page )->get()->toArray();
 
         $employees     = wp_list_pluck( $employees_obj, 'user_id' );
-        $reports       = erp_get_leave_report( $employees, '2017-01-01', '2017-12-31' );
+        $reports       = erp_get_leave_report( $employees, $selected_time.'-01-01', $selected_time.'-12-31' );
         $this->reports = $reports;
         $this->items   = $employees;
         $this->set_pagination_args( array(
