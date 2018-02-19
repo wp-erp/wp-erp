fe324fc5ee31ad44a8e033b2f29e80abb97cf3ad
Nadim <nadim0988@gmail.com>
2018-02-19 14:23:46 +0600

    Fixes #554

--- modules/accounting/includes/views/js-templates/vendor.php
+++ modules/accounting/includes/views/js-templates/vendor.php
@@ -1,4 +1,5 @@
 <input type="hidden" name="type" value="vendor">
+<input type="hidden" name="field_id" value="0">
 <?php 
 $item_type = 'vendor';
 include dirname( dirname( __FILE__ ) ) . '/user-form-rows.php'; 
