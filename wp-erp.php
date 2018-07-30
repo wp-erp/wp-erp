2623b13eb6340ac9a76cf1b3edd4e8dfe402568c
MD Sultan Nasir Uddin <manikdrmc@gmail.com>
2018-07-30 13:02:36 +0600

    Prepare release v1.3.13

--- wp-erp.php
+++ wp-erp.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://wperp.com
  * Author: weDevs
  * Author URI: https://wedevs.com
- * Version: 1.3.12
+ * Version: 1.3.13
  * License: GPL2
  * Text Domain: erp
  * Domain Path: /i18n/languages/
@@ -52,7 +52,7 @@ final class WeDevs_ERP {
      *
      * @var string
      */
-    public $version = '1.3.12';
+    public $version = '1.3.13';
 
     /**
      * Minimum PHP version required
