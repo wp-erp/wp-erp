8bb727bf1879d858bfa2f1354ba881c44f63fb64
Towhidul Islam <itowhid06@gmail.com>
2018-12-10 20:18:43 +0600

    update bills api

--- modules/accounting/api/class-rest-api-bills.php
+++ modules/accounting/api/class-rest-api-bills.php
@@ -32,7 +32,7 @@ class Bills_Controller extends \WeDevs\ERP\API\REST_Controller {
             [
                 'methods'             => WP_REST_Server::READABLE,
                 'callback'            => [ $this, 'get_bills' ],
-                'args'                => $this->get_collection_params(),
+                'args'                => [],
                 'permission_callback' => function ( $request ) {
                     return current_user_can( 'erp_ac_view_expense' );
                 },
@@ -40,7 +40,7 @@ class Bills_Controller extends \WeDevs\ERP\API\REST_Controller {
             [
                 'methods'             => WP_REST_Server::CREATABLE,
                 'callback'            => [ $this, 'create_bill' ],
-                'args'                => $this->get_endpoint_args_for_item_schema( WP_REST_Server::CREATABLE ),
+                'args'                => [],
                 'permission_callback' => function ( $request ) {
                     return current_user_can( 'erp_ac_create_expenses_voucher' );
                 },
@@ -52,9 +52,7 @@ class Bills_Controller extends \WeDevs\ERP\API\REST_Controller {
             [
                 'methods'             => WP_REST_Server::READABLE,
                 'callback'            => [ $this, 'get_bill' ],
-                'args'                => [
-                    'context' => $this->get_context_param( [ 'default' => 'view' ] ),
-                ],
+                'args'                => [],
                 'permission_callback' => function ( $request ) {
                     return current_user_can( 'erp_ac_view_expense' );
                 },
@@ -62,7 +60,7 @@ class Bills_Controller extends \WeDevs\ERP\API\REST_Controller {
             [
                 'methods'             => WP_REST_Server::EDITABLE,
                 'callback'            => [ $this, 'update_bill' ],
-                'args'                => $this->get_endpoint_args_for_item_schema( WP_REST_Server::EDITABLE ),
+                'args'                => [],
                 'permission_callback' => function ( $request ) {
                     return current_user_can( 'erp_ac_create_expenses_voucher' );
                 },
@@ -81,7 +79,7 @@ class Bills_Controller extends \WeDevs\ERP\API\REST_Controller {
             [
                 'methods'             => WP_REST_Server::READABLE,
                 'callback'            => [ $this, 'void_bill' ],
-                'args'                => $this->get_collection_params(),
+                'args'                => [],
                 'permission_callback' => function ( $request ) {
                     return current_user_can( 'erp_ac_publish_expenses_voucher' );
                 },
@@ -97,12 +95,17 @@ class Bills_Controller extends \WeDevs\ERP\API\REST_Controller {
      * @return WP_Error|WP_REST_Response
      */
     public function get_bills( $request ) {
-        global $wpdb;
+        $additional_fields = [];
+        $bill_data  = erp_acct_get_bills();
+        $bill_count = erp_acct_get_bill_count();
 
-        $invoice_data = erp_acct_get_bills();
+        $additional_fields['namespace'] = $this->namespace;
+        $additional_fields['rest_base'] = $this->rest_base;
 
-        $response = rest_ensure_response( $invoice_data );
-        $response = $this->format_collection_response( $response, $request, count( $invoice_data ) );
+        $response = rest_ensure_response( $bill_data );
+        $response = $this->format_collection_response( $response, $request, $bill_count );
+
+        $response->set_status( 200 );
 
         return $response;
     }
@@ -115,18 +118,22 @@ class Bills_Controller extends \WeDevs\ERP\API\REST_Controller {
      * @return WP_Error|WP_REST_Response
      */
     public function get_bill( $request ) {
-        global $wpdb;
-
         $id = (int) $request['id'];
 
         if ( empty( $id ) ) {
             return new WP_Error( 'rest_bill_invalid_id', __( 'Invalid resource id.' ), [ 'status' => 404 ] );
         }
 
-        $invoice_data =  erp_acct_get_bill( $id );
+        $bill_data =  erp_acct_get_bill( $id );
+        $bill_data['id'] = $id;
 
-        $response = rest_ensure_response( $invoice_data );
-        $response = $this->format_collection_response( $response, $request, 1 );
+        $additional_fields['namespace'] = $this->namespace;
+        $additional_fields['rest_base'] = $this->rest_base;
+
+        $item  = $this->prepare_item_for_response( $bill_data, $request, $additional_fields );
+        $response = rest_ensure_response( $item );
+
+        $response->set_status( 200 );
 
         return $response;
     }
@@ -141,10 +148,31 @@ class Bills_Controller extends \WeDevs\ERP\API\REST_Controller {
     public function create_bill( $request ) {
         $bill_data = $this->prepare_item_for_database( $request );
 
+        $item_amount = []; $item_tax = []; $item_total = []; $additional_fields = [];
+
+        $items = $request['bill_details'];
+
+        foreach ( $items as $key => $item ) {
+            $item_amount[$key] = $item['amount'];
+            $item_tax[$key] = $item['tax'];
+            $item_total[$key] = $item['amount'] + $item['tax'];
+        }
+        $bill_data['billing_address'] = maybe_serialize( $request['billing_address'] );
+        $bill_data['attachments'] = maybe_serialize( $request['attachments'] );
+        $bill_data['subtotal'] = array_sum( $item_amount );
+        $bill_data['tax'] = array_sum( $item_tax );
+        $bill_data['total'] = array_sum( $item_total );
+
         $bill_id = erp_acct_insert_bill( $bill_data );
 
-        $response = rest_ensure_response( $bill_id );
-        $response = $this->format_collection_response( $response, $request, 1 );
+        $bill_data['id'] = $bill_id;
+        $additional_fields['namespace'] = $this->namespace;
+        $additional_fields['rest_base'] = $this->rest_base;
+
+        $bill_data = $this->prepare_item_for_response( $bill_data, $request, $additional_fields );
+
+        $response = rest_ensure_response( $bill_data );
+        $response->set_status( 201 );
 
         return $response;
     }
@@ -165,10 +193,32 @@ class Bills_Controller extends \WeDevs\ERP\API\REST_Controller {
 
         $bill_data = $this->prepare_item_for_database( $request );
 
-        erp_acct_update_bill( $bill_data, $id );
+        $item_amount = []; $item_tax = []; $item_total = []; $additional_fields = [];
+
+        $items = $request['bill_details'];
+
+        foreach ( $items as $key => $item ) {
+            $item_amount[$key] = $item['amount'];
+            $item_tax[$key] = $item['tax'];
+            $item_total[$key] = $item['amount'] + $item['tax'];
+
+        }
+        $bill_data['billing_address'] = maybe_serialize( $request['billing_address'] );
+        $bill_data['attachments'] = maybe_serialize( $request['attachments'] );
+        $bill_data['subtotal'] = array_sum( $item_amount );
+        $bill_data['tax'] = array_sum( $item_tax );
+        $bill_data['total'] = array_sum( $item_total );
+
+        $bill_id = erp_acct_update_bill( $bill_data, $id );
+
+        $$bill_data['id'] = $bill_id;
+        $additional_fields['namespace'] = $this->namespace;
+        $additional_fields['rest_base'] = $this->rest_base;
+
+        $bill_data = $this->prepare_item_for_response( $bill_data, $request, $additional_fields );
 
         $response = rest_ensure_response( $bill_data );
-        $response = $this->format_collection_response( $response, $request, count( $items ) );
+        $response->set_status( 200 );
 
         return $response;
     }
@@ -219,7 +269,6 @@ class Bills_Controller extends \WeDevs\ERP\API\REST_Controller {
      * @return array $prepared_item
      */
     protected function prepare_item_for_database( $request ) {
-
         $prepared_item = [];
 
         if ( isset( $request['vendor_id'] ) ) {
@@ -234,11 +283,11 @@ class Bills_Controller extends \WeDevs\ERP\API\REST_Controller {
         if ( isset( $request['due_date'] ) ) {
             $prepared_item['due_date'] = absint( $request['due_date'] );
         }
-        if ( isset( $request['amount'] ) ) {
-            $prepared_item['amount'] = $request['amount'];
+        if ( isset( $request['remarks'] ) ) {
+            $prepared_item['remarks'] = $request['remarks'];
         }
-        if ( isset( $request['transaction_no'] ) ) {
-            $prepared_item['transaction_no'] = $request['transaction_no'];
+        if ( isset( $request['trn_no'] ) ) {
+            $prepared_item['trn_no'] = $request['trn_no'];
         }
         if ( isset( $request['attachments'] ) ) {
             $prepared_item['attachments'] = $request['attachments'];
@@ -256,13 +305,15 @@ class Bills_Controller extends \WeDevs\ERP\API\REST_Controller {
     /**
      * Prepare a single user output for response
      *
-     * @param object $item
+     * @param array|object $item
      * @param WP_REST_Request $request Request object.
      * @param array $additional_fields (optional)
      *
      * @return WP_REST_Response $response Response data.
      */
     public function prepare_item_for_response( $item, $request, $additional_fields = [] ) {
+        $item = (object) $item;
+
         $data = [
             'id'              => (int) $item->id,
             'voucher_no'      => (int) $item->voucher_no,
@@ -273,7 +324,6 @@ class Bills_Controller extends \WeDevs\ERP\API\REST_Controller {
             'bill_details'    => $item->bill_details,
             'subtotal'        => (int) $item->subtotal,
             'total'           => (int) $item->total,
-            'discount'        => (int) $item->discount,
             'tax'             => (int) $item->tax,
             'ref'             => $item->ref,
             'remarks'         => $item->remarks,
@@ -284,7 +334,7 @@ class Bills_Controller extends \WeDevs\ERP\API\REST_Controller {
         // Wrap the data in a response object
         $response = rest_ensure_response( $data );
 
-        $response = $this->add_links( $response, $item );
+        $response = $this->add_links( $response, $item, $additional_fields );
 
         return $response;
     }
@@ -315,7 +365,7 @@ class Bills_Controller extends \WeDevs\ERP\API\REST_Controller {
                     ],
                 ],
                 'vendor_id'   => [
-                    'description' => __( 'Customer id for the resource.' ),
+                    'description' => __( 'People id for the resource.' ),
                     'type'        => 'integer',
                     'context'     => [ 'edit' ],
                     'arg_options' => [
