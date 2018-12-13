2f573bef6df57c9b38631fbc38969b93798da4e8
Jahidur Rahman <nadim0988@gmail.com>
2018-12-13 17:56:14 +0600

    Add vue-click-outside directives

--- modules/accounting/assets/src/admin/components/Invoice/InvoiceCreate.vue
+++ modules/accounting/assets/src/admin/components/Invoice/InvoiceCreate.vue
@@ -6,6 +6,10 @@
             <div class="wperp-row wperp-between-xs">
                 <div class="wperp-col">
                     <h2 class="content-header__title">New Invoice</h2>
+                    <!-- just for showing modal -->
+                    <a href="#" class="wperp-btn btn--primary" @click="showInvoiceModal">
+                        <span>Show Invoice Modal</span>
+                    </a>
                 </div>
             </div>
         </div>
@@ -108,180 +112,7 @@
             </div>
         </div>
 
-        <div id="wperp-invoice-modal" class="wperp-modal wperp-invoice-modal wperp-custom-scroll" role="dialog">
-                <div class="wperp-modal-dialog">
-                    <div class="wperp-modal-content">
-                        <div class="wperp-modal-header">
-                            <h4>
-                                Invoice
-                            </h4>
-                            <div class="d-print-none">
-                                <a href="#" class="wperp-btn btn--default print-btn">
-                                    <i class="flaticon-printer-1"></i>
-                                    &nbsp; Print
-                                </a>
-                                <!-- todo: more action has some dropdown and will implement later please consider as planning -->
-                                <a href="#" class="wperp-btn btn--default">
-                                    <i class="flaticon-settings-work-tool"></i>
-                                    &nbsp; More Action
-                                </a>
-                            </div>
-                        </div>
-                        <div class="wperp-modal-body">
-                            <div class="wperp-invoice-panel">
-                                <div class="invoice-header">
-                                    <div class="invoice-logo">
-                                        <img :src="acct_var.acc_aaset_url + '/images/dummy-logo.png'" alt="logo name">
-                                    </div>
-                                    <div class="invoice-address">
-                                        <address>
-                                            <strong>Amazon Limited</strong><br>
-                                            983 Aiden Roads Suite 062<br>
-                                            Address Line 2<br>
-                                            1483 Theresafort Afyon<br>
-                                            Turkey
-                                        </address>
-                                    </div>
-                                </div>
-
-                                <div class="invoice-body">
-                                    <h4>Invoice</h4>
-                                    <div class="wperp-row">
-                                        <div class="wperp-col-sm-6">
-                                            <h5>Bill to:</h5>
-                                            <div class="persons-info">
-                                                <strong>Md Ashraf Hossain</strong><br>
-                                                983 Aiden Roads Suite 062<br>
-                                                Address Line 2<br>
-                                                1483 Theresafort Afyon<br>
-                                                Turkey
-                                            </div>
-                                        </div>
-                                        <div class="wperp-col-sm-6">
-                                            <table class="invoice-info">
-                                                <tr>
-                                                    <th>Invoice No.</th>
-                                                    <td>INV-0001</td>
-                                                </tr>
-                                                <tr>
-                                                    <th>Invoice Date:</th>
-                                                    <td>17-10-2018</td>
-                                                </tr>
-                                                <tr>
-                                                    <th>Due Date:</th>
-                                                    <td>17-10-2018</td>
-                                                </tr>
-                                                <tr>
-                                                    <th>Amount Due:</th>
-                                                    <td>17-10-2018</td>
-                                                </tr>
-                                            </table>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="wperp-invoice-table">
-                                    <table class="wperp-table wperp-form-table invoice-table">
-                                        <thead>
-                                            <tr>
-                                                <th>Description</th>
-                                                <th>City</th>
-                                                <th>Unit Price</th>
-                                                <th>Discount</th>
-                                                <th>Tax</th>
-                                                <th>Tax Amount</th>
-                                                <th>Amount</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <th>Buy Fabrics</th>
-                                                <td>10</td>
-                                                <td>$1500.00</td>
-                                                <td>10%</td>
-                                                <td>0%</td>
-                                                <td>$0.00</td>
-                                                <td>$15,000.00</td>
-                                            </tr>
-                                            <tr>
-                                                <th>Buy Fabrics</th>
-                                                <td>10</td>
-                                                <td>$1500.00</td>
-                                                <td>10%</td>
-                                                <td>0%</td>
-                                                <td>$0.00</td>
-                                                <td>$15,000.00</td>
-                                            </tr>
-                                            <tr>
-                                                <th>Buy Fabrics</th>
-                                                <td>10</td>
-                                                <td>$1500.00</td>
-                                                <td>10%</td>
-                                                <td>0%</td>
-                                                <td>$0.00</td>
-                                                <td>$15,000.00</td>
-                                            </tr><tr>
-                                                <th>Buy Fabrics</th>
-                                                <td>10</td>
-                                                <td>$1500.00</td>
-                                                <td>10%</td>
-                                                <td>0%</td>
-                                                <td>$0.00</td>
-                                                <td>$15,000.00</td>
-                                            </tr><tr>
-                                                <th>Buy Fabrics</th>
-                                                <td>10</td>
-                                                <td>$1500.00</td>
-                                                <td>10%</td>
-                                                <td>0%</td>
-                                                <td>$0.00</td>
-                                                <td>$15,000.00</td>
-                                            </tr>
-                                        </tbody>
-                                        <tfoot>
-                                            <tr>
-                                                <td colspan="7">
-                                                    <ul>
-                                                        <li><span>Subtotal:</span> $15,000.00</li>
-                                                        <li><span>Total:</span> $15,000.00</li>
-                                                        <li><span>Total Related Payments:</span> $15,000.00</li>
-                                                    </ul>
-                                                </td>
-                                            </tr>
-                                        </tfoot>
-                                    </table>
-                                </div>
-
-                            </div>
-                            <div class="invoice-attachments d-print-none">
-                                <h4>Attachments</h4>
-                                <a class="attachment-item" href="#">
-                                    <img :src="acct_var.acc_aaset_url + '/images/img-thumb.png'" alt="image name">
-                                    <div class="attachment-meta">
-                                        <span>File name with extension</span><br>
-                                        <span class="text-muted">file size</span>
-                                    </div>
-                                </a>
-                                <a class="attachment-item" href="#">
-                                    <img :src="acct_var.acc_aaset_url + '/images/doc-thumb.png'" alt="image name">
-                                    <div class="attachment-meta">
-                                        <span>File name with extension</span><br>
-                                        <span class="text-muted">file size</span>
-                                    </div>
-                                </a>
-                                <a class="attachment-item" href="#">
-                                    <img :src="acct_var.acc_aaset_url + '/images/pdf-thumb.png'" alt="image name">
-                                    <div class="attachment-meta">
-                                        <span>File name with extension</span><br>
-                                        <span class="text-muted">file size</span>
-                                    </div>
-                                </a>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <invoice-modal v-if="invoiceModal" />
 
         <!-- End .wperp-crm-table -->
     </div>
@@ -292,6 +123,7 @@ import HTTP from 'admin/http'
 import Datepicker from 'admin/components/base/Datepicker.vue'
 import FileUpload from 'admin/components/base/FileUpload.vue'
 import SubmitButton from 'admin/components/base/SubmitButton.vue'
+import InvoiceModal from 'admin/components/invoice/InvoiceModal.vue'
 import TransactionRow from 'admin/components/invoice/TransactionRow.vue'
 import InvoiceCustomers from 'admin/components/invoice/InvoiceCustomers.vue'
 
@@ -303,6 +135,7 @@ export default {
         Datepicker,
         FileUpload,
         SubmitButton,
+        InvoiceModal,
         TransactionRow,
         InvoiceCustomers
     },
@@ -321,9 +154,8 @@ export default {
             transactionLines: [{}],
             finalTotalAmount: 0,
 
+            invoiceModal: false,
             isWorking: false,
-
-            acct_var: erp_acct_var
         }
     },
 
@@ -343,6 +175,10 @@ export default {
 
         this.$root.$on('total-updated', amount => {            
             this.updateFinalAmount();
+        });
+
+        this.$root.$on('invoice-modal-close', () => {
+            this.invoiceModal = false;
         });
     },
 
@@ -424,6 +260,10 @@ export default {
             });
 
             this.finalTotalAmount = parseFloat(finalAmount).toFixed(2);
+        },
+
+        showInvoiceModal() {
+            this.invoiceModal = true;
         }
     }
 
--- modules/accounting/assets/src/admin/main.js
+++ modules/accounting/assets/src/admin/main.js
@@ -8,6 +8,26 @@ Vue.config.productionTip = false
 
 Vue.use(VeeValidate);
 
+// Vue click outside directive
+Vue.directive('click-outside', {
+    bind(el, binding, vnode) {
+        const bubble = binding.modifiers.bubble
+        const handler = e => {
+            if ( bubble || ( ! el.contains( e.target ) && el !== e.target ) ) {
+                binding.value(e)
+            }
+        }
+
+        el.__vueClickOutside__ = handler
+        document.addEventListener('click', handler)
+    },
+
+    unbind(el, binding) {
+        document.removeEventListener('click', el.__vueClickOutside__)
+        el.__vueClickOutside__ = null
+    }
+})
+
 /* eslint-disable no-new */
 new Vue({
     el: '#erp-accounting',
