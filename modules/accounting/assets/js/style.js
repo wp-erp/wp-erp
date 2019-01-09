4161270819ae7924bf20b99736e34611d4de3d55
Towhidul Islam <itowhid06@gmail.com>
2019-01-09 22:14:11 +0600

    added skeleton journal modal

--- modules/accounting/assets/js/style.js
+++ modules/accounting/assets/js/style.js
@@ -1,18 +1,10 @@
 pluginWebpack([2],{
 
-<<<<<<< HEAD
-/***/ 306:
-=======
-/***/ 298:
->>>>>>> 49dbbe47836ee992f51f1e718a1e7d762cba7e89
+/***/ 319:
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
 
 /***/ })
 
-<<<<<<< HEAD
-},[306]);
-=======
-},[298]);
->>>>>>> 49dbbe47836ee992f51f1e718a1e7d762cba7e89
+},[319]);
