441ac8fb68b205eb8d5b8f2e4b7fecdcf6b27237
rafsuntaskin <rafsun.aiub@gmail.com>
2019-01-22 13:16:27 +0600

    updated ledger map query

--- modules/accounting/includes/class-ledger-map.php
+++ modules/accounting/includes/class-ledger-map.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace WeDevs\ERP\Accounting\Includes;
 
 class Ledger_Map {
@@ -9,14 +10,14 @@ class Ledger_Map {
     private function __construct() {
         global $wpdb;
 
-        $sql = "SELECT id, chart_id, category_id, name, slug, code 
+        $sql = "SELECT slug, id, chart_id, category_id, name, code 
             FROM {$wpdb->prefix}erp_acct_ledgers WHERE system = 1";
 
-        $this->ledgers = $wpdb->get_results($sql);
+        $this->ledgers = $wpdb->get_results( $sql, OBJECT_K );
     }
 
     public static function getInstance() {
-        if (static::$instance == null) {
+        if ( static::$instance == null ) {
             static::$instance = new Ledger_Map;
         }
 
@@ -24,12 +25,16 @@ class Ledger_Map {
     }
 
     public function get_ledger_id_by_slug( $slug ) {
-        foreach ( $this->ledgers as $key => $value ) {
-            if ( $value->slug === $slug ) {
-                return (int) $value->id;
-            }
+        if ( $this->ledgers[$slug] ) {
+            return $this->ledgers[$slug]->id;
         }
+        return false;
+    }
 
+    public function get_ledger_details_by_slug( $slug ) {
+        if ( $this->ledgers[$slug] ) {
+            return $this->ledgers[$slug];
+        }
         return false;
     }
 }
