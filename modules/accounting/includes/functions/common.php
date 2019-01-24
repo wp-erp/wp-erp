5824ac7280b32d55dd95a7f54a044ec3ac9f1ff8
Towhidul Islam <itowhid06@gmail.com>
2019-01-24 20:50:17 +0600

    fixed status checking in check

--- modules/accounting/includes/functions/common.php
+++ modules/accounting/includes/functions/common.php
@@ -232,14 +232,18 @@ function erp_acct_get_tax_category_by_id( $cat_id ) {
  *
  * @param $trn_id
  *
- * @return array
+ * @return string
  */
 function erp_acct_get_trn_status_by_id( $trn_id ) {
     global $wpdb;
 
-    $row = $wpdb->get_row( "SELECT type_name FROM {$wpdb->prefix}erp_acct_trn_status_types WHERE id = {$trn_id} LIMIT 1" );
+    if ( !$trn_id ) {
+        return 'awaiting_approval';
+    }
 
-    return $row;
+    $row = $wpdb->get_row( "SELECT type_name FROM {$wpdb->prefix}erp_acct_trn_status_types WHERE id = {$trn_id}" );
+
+    return ucfirst( str_replace( '_', ' ', $row->type_name ) );
 }
 
 /**
