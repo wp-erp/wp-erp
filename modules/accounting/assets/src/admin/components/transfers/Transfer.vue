5ccb7a279db62c13770354355bb5bd2465ccadbb
Towhidul Islam <itowhid06@gmail.com>
2019-01-29 21:22:38 +0600

    refactor transfer modal to transfer page

--- modules/accounting/assets/src/admin/components/transfers/Transfer.vue
+++ modules/accounting/assets/src/admin/components/transfers/Transfer.vue
@@ -1,66 +1,67 @@
 <template>
     <div class="wperp-container">
-        <div id="wperp-transfer-money-modal" class="wperp-modal has-form wperp-modal-open" role="dialog">
-            <div class="wperp-modal-dialog" v-click-outside="outside" @click="inside">
-                <div class="wperp-modal-content">
-                    <!-- modal body title -->
-                    <div class="wperp-modal-header">
-                        <h3>Transfer Money</h3>
-                        <span class="modal-close"><i class="flaticon-close"></i></span>
-                    </div>
-                    <!-- end modal body title -->
-                    <form action="" method="post" class="modal-form edit-customer-modal" @submit.prevent="submitTransfer">
-                        <div class="wperp-modal-body">
-                            <!-- add new product form -->
-                            <div class="wperp-row wperp-gutter-20">
-                                <div class="wperp-form-group wperp-col-sm-6 wperp-col-xs-12">
-                                    <label for="transfer_funds_from">Transfer Funds From</label>
-                                    <div class="wperp-custom-select with-multiselect">
-                                        <multi-select id="transfer_funds_from" name="from" v-model="transferFrom" :multiple="false" :options="fa" placeholder="Select Account"></multi-select>
-                                    </div>
-                                    <span class="balance mt-10 display-inline-block">Balance: {{transformBalance(transferFrom.balance)}}</span>
+        <!-- Start .header-section -->
+        <div class="content-header-section separator">
+            <div class="wperp-row wperp-between-xs">
+                <div class="wperp-col">
+                    <h2 class="content-header__title">Transfer Money</h2>
+                </div>
+            </div>
+        </div>
+        <!-- End .header-section -->
+        <div class="wperp-panel wperp-panel-default pb-0">
+            <div class="wperp-panel-body">
+                <form action="" method="post" class="modal-form edit-customer-modal" @submit.prevent="submitTransfer">
+                    <div class="wperp-modal-body">
+                        <!-- add new product form -->
+                        <div class="wperp-row wperp-gutter-20">
+                            <div class="wperp-form-group wperp-col-sm-6 wperp-col-xs-12">
+                                <label for="transfer_funds_from">Transfer Funds From</label>
+                                <div class="wperp-custom-select with-multiselect">
+                                    <multi-select id="transfer_funds_from" name="from" v-model="transferFrom" :multiple="false" :options="fa" placeholder="Select Account"></multi-select>
                                 </div>
-                                <div class="wperp-form-group wperp-col-sm-6 wperp-col-xs-12">
-                                    <label for="transfer_funds_to">Transfer Funds To</label>
+                                <span class="balance mt-10 display-inline-block">Balance: {{transformBalance(transferFrom.balance)}}</span>
+                            </div>
+                            <div class="wperp-form-group wperp-col-sm-6 wperp-col-xs-12">
+                                <label for="transfer_funds_to">Transfer Funds To</label>
 
-                                    <div class="wperp-custom-select with-multiselect">
-                                        <multi-select id="transfer_funds_to" name="to" v-model="transferTo" :multiple="false" :options="ta" placeholder="Select Account"></multi-select>
-                                    </div>
-                                    <span class="balance mt-10 display-inline-block">Balance: {{transformBalance(transferTo.balance)}}</span>
+                                <div class="wperp-custom-select with-multiselect">
+                                    <multi-select id="transfer_funds_to" name="to" v-model="transferTo" :multiple="false" :options="ta" placeholder="Select Account"></multi-select>
                                 </div>
-                                <div class="wperp-form-group wperp-col-sm-6 wperp-col-xs-12">
-                                    <label for="transfer_amount">Transfer Amount</label>
-                                    <input required min="0" type="number" name="transfer_amount" id="transfer_amount" class="wperp-form-field" placeholder="$100.00" v-model="amount">
-                                </div>
-                                <div class="wperp-form-group wperp-col-sm-6 wperp-col-xs-12">
-                                    <label for="transfer_date">Transfer Date</label>
-                                    <datepicker id="transfer_date" name="transfer_date" v-model="transferdate" :defaultDate="transferdate"></datepicker>
-                                </div>
-                                <div class="wperp-col-xs-12 wperp-form-group">
-                                    <label for="transfer_memo">Memo</label>
-                                    <textarea name="transfer_memo" id="transfer_memo" rows="3" class="wperp-form-field" placeholder="Type Here" v-model="remarks"></textarea>
-                                </div>
-                                <!--<div class="wperp-col-xs-12">-->
-                                    <!--<div class="attachment-container">-->
-                                        <!--<label class="col&#45;&#45;attachement">Attachment</label>-->
-                                        <!--<div class="attachment-preview">-->
-                                            <!--<img src="assets/images/img-thumb.png" alt="attachment image">-->
-                                            <!--<i class="flaticon-close remove-attachment"></i>-->
-                                        <!--</div>-->
-                                        <!--<div class="attachment-placeholder">-->
-                                            <!--To attach <input type="file" id="attachment" name="attachment" class="display-none"> <label class="mt-0" for="attachment">Select files</label> from your computer-->
-                                        <!--</div>-->
+                                <span class="balance mt-10 display-inline-block">Balance: {{transformBalance(transferTo.balance)}}</span>
+                            </div>
+                            <div class="wperp-form-group wperp-col-sm-6 wperp-col-xs-12">
+                                <label for="transfer_amount">Transfer Amount</label>
+                                <input required min="0" type="number" name="transfer_amount" id="transfer_amount" class="wperp-form-field" placeholder="$100.00" v-model="amount">
+                            </div>
+                            <div class="wperp-form-group wperp-col-sm-6 wperp-col-xs-12">
+                                <label for="transfer_date">Transfer Date</label>
+                                <datepicker id="transfer_date" name="transfer_date" v-model="transferdate" :defaultDate="transferdate"></datepicker>
+                            </div>
+                            <div class="wperp-col-xs-12 wperp-form-group">
+                                <label for="transfer_memo">Memo</label>
+                                <textarea name="transfer_memo" id="transfer_memo" rows="3" class="wperp-form-field" placeholder="Type Here" v-model="remarks"></textarea>
+                            </div>
+                            <!--<div class="wperp-col-xs-12">-->
+                                <!--<div class="attachment-container">-->
+                                    <!--<label class="col&#45;&#45;attachement">Attachment</label>-->
+                                    <!--<div class="attachment-preview">-->
+                                        <!--<img src="assets/images/img-thumb.png" alt="attachment image">-->
+                                        <!--<i class="flaticon-close remove-attachment"></i>-->
+                                    <!--</div>-->
+                                    <!--<div class="attachment-placeholder">-->
+                                        <!--To attach <input type="file" id="attachment" name="attachment" class="display-none"> <label class="mt-0" for="attachment">Select files</label> from your computer-->
                                     <!--</div>-->
                                 <!--</div>-->
-                            </div>
-
+                            <!--</div>-->
                         </div>
 
-                        <div class="wperp-modal-footer pt-0">
-                            <button class="wperp-btn btn--primary" type="submit">Submit</button>
-                        </div>
-                    </form>
-                </div>
+                    </div>
+
+                    <div class="wperp-modal-footer pt-0">
+                        <button class="wperp-btn btn--primary" type="submit">Transfer Money</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
@@ -68,12 +69,18 @@
 </template>
 
 <script>
-    import MultiSelect from "admin/components/select/MultiSelect.vue";
-    import HTTP from 'admin/http';
-    import Datepicker from 'admin/components/base/Datepicker.vue';
+    import MultiSelect from "admin/components/select/MultiSelect.vue"
+    import HTTP from 'admin/http'
+    import Datepicker from 'admin/components/base/Datepicker.vue'
+
     export default {
         name: "Transfer",
-        components: { MultiSelect, HTTP, Datepicker },
+
+        components: {
+            MultiSelect,
+            HTTP,
+            Datepicker
+        },
 
         data() {
             return {
@@ -149,12 +156,6 @@
                 this.remarks = '';
                 this.amount = '';
             },
-
-            inside() {},
-
-            outside() {
-
-            }
         },
 
         watch: {
