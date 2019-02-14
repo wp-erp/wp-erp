1f1077aa2456e0a2461bb4b6629969602ed465ae
Jahidur Rahman <nadim0988@gmail.com>
2019-02-14 12:45:18 +0600

    Version bumped and changelog updated to 1.4.2

--- wp-erp.php
+++ wp-erp.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://wperp.com
  * Author: weDevs
  * Author URI: https://wedevs.com
- * Version: 1.4.1
+ * Version: 1.4.2
  * License: GPL2
  * Text Domain: erp
  * Domain Path: /i18n/languages/
@@ -52,7 +52,7 @@ final class WeDevs_ERP {
      *
      * @var string
      */
-    public $version = '1.4.1';
+    public $version = '1.4.2';
 
     /**
      * Minimum PHP version required
