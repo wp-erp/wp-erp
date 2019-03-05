d152c2bade83970ca8b061249be217a406838914
Towhidul Islam <itowhid06@gmail.com>
2019-03-05 16:28:33 +0600

    add income statement report

--- modules/accounting/assets/src/admin/components/check/CheckCreate.vue
+++ modules/accounting/assets/src/admin/components/check/CheckCreate.vue
@@ -282,7 +282,9 @@
             },
 
             getLedgers() {
-                HTTP.get('ledgers').then((response) => {
+                let expense_chart_id = 5;
+
+                HTTP.get(`/ledgers/${expense_chart_id}/accounts`).then(response => {
                     this.ledgers = response.data;
                 });
             },
