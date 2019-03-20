27039c1031865b6e3f1485f5a4c06b70e11f154a
Hasinur Rahman <hasinurrahman3@gmail.com>
2019-03-20 22:27:14 +0600

    Fix redirect

--- modules/accounting/assets/src/admin/components/transfers/NewTransfer.vue
+++ modules/accounting/assets/src/admin/components/transfers/NewTransfer.vue
@@ -122,7 +122,7 @@
                     this.showAlert( 'success', 'Transfer Successful!' );
                     this.fetchAccounts();
                     this.resetData();
-                    this.$router.push('/transfer/list');
+                    this.$router.push('/transfer');
                 } ).catch( err => {
                     let msg = err.response.data.message;
                     this.$store.dispatch( 'spinner/setSpinner', false );
