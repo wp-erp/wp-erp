95ca07c0f4262aa42a75cb019d557d9e787ec32c
Towhidul Islam <itowhid06@gmail.com>
2019-03-22 11:43:53 +0600

    redesign dashboard bank

--- modules/accounting/assets/src/admin/components/dashboard/Accounts.vue
+++ modules/accounting/assets/src/admin/components/dashboard/Accounts.vue
@@ -1,36 +1,29 @@
 <template>
     <div :class="['bank-accounts-section', 'wperp-panel', 'wperp-panel-default', ( isEditSettingsEnabled ? 'open-edit':'' )]">
         <div class="wperp-panel-heading wperp-bg-white">
-            <h4>Bank Accounts</h4>
-            <a href="#" @click.prevent="editSettings" id="bank-account-edit" class="panel-badge">
-                <span v-if="isEditSettingsEnabled" @click.prevent="saveDashboardBanks">Save</span>
-                <i v-else class="flaticon-quick-edit"></i>
-            </a>
+            <h4>Accounts</h4>
+            <!--<a href="#" @click.prevent="editSettings" id="bank-account-edit" class="panel-badge">-->
+                <!--<span v-if="isEditSettingsEnabled" @click.prevent="saveDashboardBanks">Save</span>-->
+                <!--<i v-else class="flaticon-quick-edit"></i>-->
+            <!--</a>-->
         </div>
         <div class="wperp-panel-body pb-0">
-            <ul v-if="!isEditSettingsEnabled" class="wperp-list-unstyled list-table-content list-table-content--border">
-                <li v-if="accounts.length" :key="key" v-for="(item,key) in accounts">
+            <ul class="wperp-list-unstyled list-table-content list-table-content--border">
+                <li v-if="accounts.length" :key="key" v-for="(data,key) in accounts">
                     <div class="left">
                         <i class="flaticon-menu-1"></i>
-                        <a href="#" class="title">{{item.name}}</a>
+                        <details v-if="data.additional" open>
+                            <summary>{{ data.name }}</summary>
+                            <p :key="additional.id" v-for="additional in data.additional">
+                                <strong>{{ additional.name }}</strong>
+                                <em>{{ getCurrencySign() + Math.abs(additional.balance) }}</em>
+                            </p>
+                        </details>
+                        <span v-else>{{ data.name }}</span>
                     </div>
                     <div class="right">
-                        <span v-if="undefined === item.balance" class="price">{{formatDBAmount(0)}}</span>
-                        <span v-else class="price">{{formatDBAmount(item.balance)}}</span>
-                        <i class="flaticon-trash" @click.prevent="removeAccount(key)"></i>
-                    </div>
-                </li>
-            </ul>
-            <ul v-else class="wperp-list-unstyled list-table-content list-table-content--border">
-                <li :key="key" v-for="(item,key) in edit_accounts">
-                    <div class="left">
-                        <i class="flaticon-menu-1"></i>
-                        <a href="#" class="title">{{item.name}}</a>
-                    </div>
-                    <div class="right">
-                        <span v-if="undefined === item.balance" class="price">{{formatDBAmount(0)}}</span>
-                        <span v-else class="price">{{formatDBAmount(item.balance)}}</span>
-                        <i class="flaticon-trash" @click.prevent="removeEditAccount(key)"></i>
+                        <span v-if="undefined === data.balance" class="price">{{formatDBAmount(0)}}</span>
+                        <span v-else class="price">{{formatDBAmount(data.balance)}}</span>
                     </div>
                 </li>
             </ul>
@@ -66,48 +59,32 @@
 
         methods: {
             fetchAccounts() {
-                if ( !this.isEditSettingsEnabled ) {
-                    HTTP.get('/accounts/cash-at-bank').then( (response) => {
-                        this.accounts = response.data;
-                    });
-
-                    if ( !this.accounts.length ) {
-                        this.edit_accounts = [];
-                        HTTP.get('/accounts/bank-accounts').then( (response) => {
-                            this.accounts = response.data;
-                            this.edit_accounts = response.data;
-                        });
-                    }
-                } else {
-                    HTTP.get('/accounts/bank-accounts').then( (response) => {
-                        this.accounts = response.data;
-                        this.edit_accounts = response.data;
-                    });
-                }
-
-            },
-
-            saveDashboardBanks() {
-                HTTP.post('/accounts/cash-at-bank', {
-                    accounts: this.edit_accounts
-                }).then(() => {
-                    this.fetchAccounts();
-                    this.isEditSettingsEnabled = false;
+                HTTP.get('/accounts/cash-at-bank').then( (response) => {
+                    this.accounts = response.data;
                 });
             },
 
-            removeAccount(key) {
-                this.$delete(this.accounts, key);
-            },
-
-            removeEditAccount(key) {
-                this.$delete(this.edit_accounts, key);
-            },
-
-            editSettings() {
-                this.fetchAccounts();
-                this.isEditSettingsEnabled = !this.isEditSettingsEnabled;
-            }
+            // saveDashboardBanks() {
+            //     HTTP.post('/accounts/cash-at-bank', {
+            //         accounts: this.edit_accounts
+            //     }).then(() => {
+            //         this.fetchAccounts();
+            //         this.isEditSettingsEnabled = false;
+            //     });
+            // },
+            //
+            // removeAccount(key) {
+            //     this.$delete(this.accounts, key);
+            // },
+            //
+            // removeEditAccount(key) {
+            //     this.$delete(this.edit_accounts, key);
+            // },
+            //
+            // editSettings() {
+            //     this.fetchAccounts();
+            //     this.isEditSettingsEnabled = !this.isEditSettingsEnabled;
+            // }
         },
 
         mounted() {
@@ -116,18 +93,22 @@
 
         computed: {
             totalAmount() {
-                if ( ( typeof this.accounts === "object" && null === this.accounts ) || !this.accounts.length ) {
+                if ( ( typeof this.accounts === "object" && null === this.accounts ) ) {
                     return;
                 }
-                let total = this.accounts.reduce((amount, item) => {
-                    return amount + parseFloat(item.balance);
-                }, 0);
+                let amount = 0;
+                this.accounts.forEach( element => {
+                    if ( null === element.balance ) {
+                        element.balance = 0;
+                    }
+                    amount += parseFloat(element.balance);
+                });
 
-                if ( isNaN( parseFloat(total) )) {
-                    total = 0;
+                if ( isNaN( parseFloat(amount) )) {
+                    amount = 0;
                 }
 
-                return this.formatAmount(total);
+                return this.formatAmount(amount);
             },
         },
 
