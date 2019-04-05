33f4c55b64b7594511cf0199cb6cc18e8207c656
Towhidul Islam <itowhid06@gmail.com>
2019-04-05 11:36:57 +0600

    added ac_receivable and ac_payable in opening blnc

--- modules/accounting/api/class-rest-api-opening-balances.php
+++ modules/accounting/api/class-rest-api-opening-balances.php
@@ -47,6 +47,18 @@ class Opening_Balances_Controller extends \WeDevs\ERP\API\REST_Controller {
             'schema' => [ $this, 'get_item_schema' ],
         ] );
 
+        register_rest_route( $this->namespace, '/' . $this->rest_base . '/virtual-accts', [
+            [
+                'methods'             => WP_REST_Server::READABLE,
+                'callback'            => [ $this, 'get_virtual_accts' ],
+                'args'                => [],
+                'permission_callback' => function ( $request ) {
+                    return current_user_can( 'erp_ac_view_journal' );
+                },
+            ],
+            'schema' => [ $this, 'get_item_schema' ],
+        ] );
+
         register_rest_route( $this->namespace, '/' . $this->rest_base . '/names', [
             [
                 'methods'             => WP_REST_Server::READABLE,
@@ -157,6 +169,26 @@ class Opening_Balances_Controller extends \WeDevs\ERP\API\REST_Controller {
         $additional_fields['rest_base'] = $this->rest_base;
 
         $item     = erp_acct_get_opening_balance_names();
+
+        $response = rest_ensure_response( $item );
+
+        $response->set_status( 200 );
+
+        return $response;
+    }
+
+    /**
+     * Get a collection of opening_balance_names
+     *
+     * @param WP_REST_Request $request
+     *
+     * @return WP_Error|WP_REST_Response
+     */
+    public function get_virtual_accts( $request ) {
+
+        $opening_balance_data = $this->prepare_item_for_database( $request );
+
+        $item     = erp_acct_get_ob_virtual_accts( $opening_balance_data );
 
         $response = rest_ensure_response( $item );
 
