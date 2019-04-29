9c54b987a8e08a6d77c6d0f1e086ba4f83b46521
nadim1992 <nadim0988@gmail.com>
2019-04-29 15:51:49 +0600

    Update ledger query date

--- modules/accounting/includes/functions/ledger_accounts.php
+++ modules/accounting/includes/functions/ledger_accounts.php
@@ -261,9 +261,9 @@ function erp_acct_update_ledger( $item, $id ) {
     $sql2 = "SELECT ledger.id, ledger.name, SUM(ld.debit - ld.credit) as balance
         FROM {$wpdb->prefix}erp_acct_ledgers AS ledger
         LEFT JOIN {$wpdb->prefix}erp_acct_ledger_details as ld ON ledger.id = ld.ledger_id
-        AND ld.trn_date BETWEEN {$closest_fy_date['start_date']} AND {$today} GROUP BY ld.ledger_id";
+        AND ld.trn_date BETWEEN '%s' AND '%s' GROUP BY ld.ledger_id";
 
-    $data = $wpdb->get_results($sql2, ARRAY_A);
+    $data = $wpdb->get_results( $wpdb->prepare($sql2, $closest_fy_date['start_date'], $today), ARRAY_A);
 
     return erp_acct_ledger_balance_with_opening_balance($ledgers, $data, $opening_balance);
 }
