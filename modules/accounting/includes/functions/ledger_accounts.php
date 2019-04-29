a6f43a6c25e237c3bade5025bbc3dea6bddabd4f
nadim1992 <nadim0988@gmail.com>
2019-04-29 15:27:40 +0600

    Update ledger with opening balance

--- modules/accounting/includes/functions/ledger_accounts.php
+++ modules/accounting/includes/functions/ledger_accounts.php
@@ -209,13 +209,124 @@ function erp_acct_update_ledger( $item, $id ) {
     global $wpdb;
 
     $wpdb->update( "{$wpdb->prefix}erp_acct_ledgers", [
-        'chart_id'      => $item['chart_id'],
-        'category_id'   => $item['category_id'],
-        'name'          => $item['name'],
+        'chart_id'    => $item['chart_id'],
+        'category_id' => $item['category_id'],
+        'name'        => $item['name'],
         'slug'        => slugify($item['name']),
-        'code'          => $item['code']
+        'code'        => $item['code']
         ], [ 'id' => $id ]
     );
 
     return erp_acct_get_ledger( $id );
+}
+
+
+
+
+/**
+ * =====================
+ * =========================
+ * =============================
+ */
+/**
+ * Get ledger with balances
+ */
+
+ function erp_acct_get_ledgers_with_balances() {
+    global $wpdb;
+
+    $today  = date('Y-m-d');
+
+    $sql = "SELECT
+        ledger.id,
+        ledger.chart_id,
+        ledger.category_id,
+        ledger.name,
+        ledger.slug,
+        ledger.code,
+        ledger.system,
+        chart_of_account.name as account_name
+
+        FROM {$wpdb->prefix}erp_acct_ledgers AS ledger
+        LEFT JOIN {$wpdb->prefix}erp_acct_chart_of_accounts AS chart_of_account ON ledger.chart_id = chart_of_account.id";
+
+    $ledgers = $wpdb->get_results( $sql, ARRAY_A );
+
+    // get closest financial year id and start date
+    $closest_fy_date = erp_acct_get_closest_fn_year_date( $today );
+
+    // get opening balance data within that(^) financial year
+    $opening_balance = erp_acct_opening_balance_by_fn_year_id( $closest_fy_date['id'] );
+
+    $sql2 = "SELECT ledger.id, ledger.name, SUM(ld.debit - ld.credit) as balance
+        FROM {$wpdb->prefix}erp_acct_ledgers AS ledger
+        LEFT JOIN {$wpdb->prefix}erp_acct_ledger_details as ld ON ledger.id = ld.ledger_id
+        AND ld.trn_date BETWEEN {$closest_fy_date['start_date']} AND {$today} GROUP BY ld.ledger_id";
+
+    $data = $wpdb->get_results($sql2, ARRAY_A);
+
+    return erp_acct_ledger_balance_with_opening_balance($ledgers, $data, $opening_balance);
+}
+
+/**
+ * Ledgers opening balance
+ *
+ * @param int $id
+ *
+ * @return void
+ */
+function erp_acct_ledgers_opening_balance_by_fn_year_id( $id ) {
+    global $wpdb;
+
+    $sql = "SELECT ledger.id, ledger.name, SUM(opb.debit - opb.credit) AS balance
+        FROM {$wpdb->prefix}erp_acct_ledgers AS ledger
+        LEFT JOIN {$wpdb->prefix}erp_acct_opening_balances AS opb ON ledger.id = opb.ledger_id
+        WHERE opb.financial_year_id = %d opb.type = 'ledger' GROUP BY opb.ledger_id";
+
+    return $wpdb->get_results( $wpdb->prepare($sql, $id), ARRAY_A );
+}
+
+/**
+ * Get ledger balance with opening balance for chart of accounts
+ *
+ * @param array $ledgers
+ * @param array $data
+ * @param array $opening_balance
+ *
+ * @return array
+ */
+function erp_acct_ledger_balance_with_opening_balance($ledgers, $data, $opening_balance) {
+    $temp_data = [];
+
+    /**
+     * Start writing a very `inefficient :(` foreach loop
+     */
+    foreach ( $ledgers as $ledger ) {
+        $balance = 0;
+
+        foreach ( $data as $row ) {
+            if ( $row['id'] == $ledger['id'] ) {
+                $balance += (float) $row['balance'];
+            }
+        }
+
+        foreach ( $opening_balance as $op_balance ) {
+            if ( $op_balance['id'] == $ledger['id'] ) {
+                $balance += (float) $op_balance['balance'];
+            }
+        }
+
+        $temp_data[] = [
+            'id'          => $ledger['id'],
+            'chart_id'    => $ledger['chart_id'],
+            'category_id' => $ledger['category_id'],
+            'name'        => $ledger['name'],
+            'slug'        => $ledger['slug'],
+            'code'        => $ledger['code'],
+            'system'      => $ledger['system'],
+            'balance'     => $balance
+        ];
+    }
+
+    return $temp_data;
 }
