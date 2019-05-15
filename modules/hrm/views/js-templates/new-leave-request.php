85836b30accb565f62c6cd98ef476400bd32fa88
Rockey Opu <opu.rockey@gmail.com>
2019-05-15 18:59:06 +0600

    Leave reason field is made required now (#824)
    * Leave reason field is made required now

--- modules/hrm/views/js-templates/new-leave-request.php
+++ modules/hrm/views/js-templates/new-leave-request.php
@@ -45,6 +45,7 @@ $types = $employee_types ? $employee_types : [];
             'label'       => __( 'Reason', 'erp' ),
             'name'        => 'leave_reason',
             'type'        => 'textarea',
+            'required'    => true,
             'custom_attr' => array( 'cols' => 25, 'rows' => 3 )
         ) ); ?>
     </div>
