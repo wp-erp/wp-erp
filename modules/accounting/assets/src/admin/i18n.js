545369ebc249c82dd839d1cbf12ed0156315e869
nadim1992 <nadim0988@gmail.com>
2019-07-31 09:11:29 +0600

    Add js hook for add-on translation

--- modules/accounting/assets/src/admin/i18n.js
+++ modules/accounting/assets/src/admin/i18n.js
@@ -2,5 +2,10 @@ import { setLocaleData, __, sprintf } from '@wordpress/i18n';
 
 setLocaleData(erpAcct.locale_data, 'erp');
 
+// hook other add-on locale
+window.acct_add_locale = function( name, localeData ) {
+    setLocaleData(localeData, name);
+}
+
 window.__ = __;
 window.sprintf = sprintf;
