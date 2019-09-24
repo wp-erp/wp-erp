90d31cf2594845890dac43ed711fe864e3da068e
nadim1992 <nadim0988@gmail.com>
2019-09-24 11:15:30 +0600

    Release v1.5.4

--- wp-erp.php
+++ wp-erp.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://wperp.com
  * Author: weDevs
  * Author URI: https://wedevs.com
- * Version: 1.5.3
+ * Version: 1.5.4
  * License: GPL2
  * Text Domain: erp
  * Domain Path: /i18n/languages/
@@ -53,7 +53,7 @@ final class WeDevs_ERP {
      * @var string
      */
 
-    public $version = '1.5.3';
+    public $version = '1.5.4';
 
     /**
      * Minimum PHP version required
