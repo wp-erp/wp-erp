92c4feee7ff462dc6483df4885fca2901dd96032
nadim1992 <nadim0988@gmail.com>
2019-10-16 10:06:39 +0600

    Show photo placeholder on people update

--- modules/accounting/assets/src/admin/components/people/PeopleModal.vue
+++ modules/accounting/assets/src/admin/components/people/PeopleModal.vue
@@ -345,7 +345,6 @@ export default {
                 this.peopleFields.last_name   = people.last_name;
                 this.peopleFields.email       = people.email;
                 this.peopleFields.mobile      = people.mobile;
-                this.peopleFields.photo       = people.photo;
                 this.peopleFields.company     = people.company;
                 this.peopleFields.phone       = people.phone;
                 this.peopleFields.website     = people.website;
@@ -357,6 +356,10 @@ export default {
                 this.peopleFields.country     = this.selectedCountry(people.billing.country);
                 this.peopleFields.state       = this.selectedState(people.billing.state);
                 this.peopleFields.postal_code = people.billing.postal_code;
+
+                if (people.photo) {
+                    this.peopleFields.photo = people.photo;
+                }
             }
         },
 
