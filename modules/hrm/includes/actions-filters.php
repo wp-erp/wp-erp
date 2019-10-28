f54f137549fe38ab5fcbb03162da1fb02dfd2b32
Rockey Opu <opu.rockey@gmail.com>
2019-10-28 10:02:18 +0600

    Fixes SP-1904 (#908)
    * About to contract is set only for HR manager
    
    * Add enable/disable section at CRM settings for sending birthday greetings to contact
    
    * Hiring date anniversary reminder and wishing email to the Employee
    
    * Added total year to hiring anniversary template
    
    * Added default hiring anniversary text during installation
    
    * Hyperlink text causing problem with the email template in CRM

--- modules/hrm/includes/actions-filters.php
+++ modules/hrm/includes/actions-filters.php
@@ -10,6 +10,7 @@ add_action( 'set_user_role', 'erp_hr_existing_role_to_employee', 10, 2 );
 add_action( 'erp_hr_employee_new', 'erp_hr_apply_policy_on_new_employee', 10, 1 );
 add_action( 'erp_daily_scheduled_events', 'erp_hr_apply_scheduled_policies' );
 add_action( 'erp_daily_scheduled_events', 'erp_hr_schedule_check_todays_birthday' );
+add_action( 'erp_daily_scheduled_events', 'erp_hr_schedule_check_todays_work_anniversary' );
 add_action( 'erp_daily_scheduled_events', 'erp_hr_apply_entitlement_yearly' );
 add_action( 'erp_hr_leave_policy_new', 'erp_hr_apply_policy_existing_employee', 10, 2 );
 add_action( 'erp_hr_schedule_announcement_email', 'erp_hr_send_announcement_email', 10, 2 );
