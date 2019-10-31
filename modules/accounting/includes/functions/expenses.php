d2ff31be9395d0740e38f1d8c24de59367d1e245
nadim1992 <nadim0988@gmail.com>
2019-10-31 14:20:55 +0600

    Fix single check draft view

--- modules/accounting/includes/functions/expenses.php
+++ modules/accounting/includes/functions/expenses.php
@@ -110,27 +110,21 @@ function erp_acct_get_check( $expense_no ) {
     expense.status,
     expense.trn_by_ledger_id,
     expense.trn_by,
-    cheque.check_no,
+    expense.check_no,
     expense.attachments,
     expense.created_at,
     expense.created_by,
     expense.updated_at,
     expense.updated_by,
 
-    cheque.bank,
-    cheque.name,
-    cheque.pay_to,
-    cheque.amount,
-
     ledg_detail.debit,
     ledg_detail.credit
 
     FROM {$wpdb->prefix}erp_acct_expenses AS expense
 
-    LEFT JOIN {$wpdb->prefix}erp_acct_expense_checks AS cheque ON expense.voucher_no = cheque.trn_no
     LEFT JOIN {$wpdb->prefix}erp_acct_ledger_details AS ledg_detail ON expense.voucher_no = ledg_detail.trn_no
 
-    WHERE expense.voucher_no = {$expense_no} AND cheque.voucher_type = 'check'";
+    WHERE expense.voucher_no = {$expense_no} AND expense.trn_by = 3";
 
     $row = $wpdb->get_row( $sql, ARRAY_A );
 
@@ -157,21 +151,17 @@ function erp_acct_format_check_line_items( $voucher_no ) {
         expense_detail.particulars,
         expense_detail.amount,
 
-        ledger.name AS ledger_name,
-
-        cheque.bank,
-        cheque.name,
-        cheque.check_no,
-        cheque.pay_to
+        ledger.name AS ledger_name
 
         FROM {$wpdb->prefix}erp_acct_expenses AS expense
-        LEFT JOIN {$wpdb->prefix}erp_acct_expense_checks AS cheque ON expense.voucher_no = cheque.trn_no
-        LEFT JOIN {$wpdb->prefix}erp_acct_expense_details AS expense_detail ON expense_detail.trn_no = cheque.trn_no
+        LEFT JOIN {$wpdb->prefix}erp_acct_expense_details AS expense_detail ON expense_detail.trn_no = expense.voucher_no
         LEFT JOIN {$wpdb->prefix}erp_acct_ledgers AS ledger ON expense_detail.ledger_id = ledger.id
 
-        WHERE expense.voucher_no = %d AND cheque.voucher_type = 'check'",
+        WHERE expense.voucher_no = 3",
         $voucher_no
     );
+
+    error_log(print_r($sql, true));
 
     return $wpdb->get_results( $sql, ARRAY_A );
 }
