c85ba0eb65bf81bfe057fbacd6c746873e719ee2
nadim1992 <nadim0988@gmail.com>
2019-11-14 12:32:42 +0600

    Update promo banner style

--- includes/admin/class-admin.php
+++ includes/admin/class-admin.php
@@ -21,7 +21,6 @@ class Admin_Page {
         $this->action( 'admin_init', 'admin_redirects' );
         add_action( 'admin_footer', 'erp_include_popup_markup' );
         $this->action( 'admin_notices', 'promotional_offer' );
-        // $this->action( 'admin_notices', 'accounting_survey_notice' );
     }
 
     /**
@@ -89,17 +88,9 @@ class Admin_Page {
             return;
         }
 
-        if ( ! isset( $_GET['page'] ) ) {
-            return;
-        }
-
-        if ( $_GET['page'] !== 'erp' ) {
-            return;
-        }
-
         // check if it has already been dismissed
         $offer_key        = 'erp_wedevs_19_blackfriday';
-        $offer_start_date = strtotime( '2019-11-26 00:00:01' );
+        $offer_start_date = strtotime( '2019-11-20 00:00:01' );
         $offer_end_date   = strtotime( '2019-12-04 23:59:00' );
         $hide_notice      = get_option( $offer_key, 'show' );
 
@@ -114,16 +105,19 @@ class Admin_Page {
                         <img src="<?php echo WPERP_ASSETS . '/images/promo-logo.png' ?>" alt="ERP">
                     </div>
                     <div class="content">
-                        <h3><span class="highlight-red">Black Friday</span> &amp; <span class="highlight-blue">Cyber Monday</span></h3>
-                        <p>Don't miss out on the biggest sale of the year on <span class="highlight-red">WP ERP</span></p>
-                        <div class="coupon-box">
-                            <div class="highlight-red">Use this coupon</div>
-                            <div class="highlight-code">BFCM2019</div>
-                        </div>
+                        <p>Biggest Sale of the year on this</p>
+
+                        <h3>Black Friday &amp; <span class="highlight-green">Cyber Monday</span></h3>
+                        <p>Claim your discount on <span class="highlight-blue">WP ERP</span> till 4th December</p>
                     </div>
                     <div class="call-to-action">
-                        <a href="https://wperp.com/pricing?utm_campaign=black_friday_&_cyber_monday&utm_medium=banner&utm_source=plugin_dashboard">Save 33%</a>
-                        <p>Valid till 4th December.</p>
+                        <a target="_blank" href="https://wperp.com/pricing?utm_campaign=black_friday_&_cyber_monday&utm_medium=banner&utm_source=plugin_dashboard">
+                            <img src="<?php echo WPERP_ASSETS . '/images/promo-btn.png' ?>" alt="Btn">
+                        </a>
+                        <p>
+                            <span class="highlight-green">COUPON: </span>
+                            <span class="coupon-code">BFCM2019</span>
+                        </p>
                     </div>
                 </div>
 
@@ -131,26 +125,35 @@ class Admin_Page {
                     #erp-christmas-notice {
                         font-size: 14px;
                         border-left: none;
-                        background: #000;
+                        background: #000DFF;
                         color: #fff;
                         display: flex
+                    }
+
+                    #erp-christmas-notice .notice-dismiss:before {
+                        color: #76E5FF;
+                    }
+
+                    #erp-christmas-notice .notice-dismiss:hover:before {
+                        color: #b71c1c;
                     }
 
                     #erp-christmas-notice .logo {
                         text-align: center;
                         text-align: center;
-                        margin: 13px 30px 5px 15px;
+                        margin: auto 50px;
                     }
 
                     #erp-christmas-notice .logo img {
                         width: 80%;
                     }
 
-                    #erp-christmas-notice .highlight-red {
-                        color: #FF0000;
+                    #erp-christmas-notice .highlight-green {
+                        color: #4FFF67;
                     }
+
                     #erp-christmas-notice .highlight-blue {
-                        color: #48ABFF;
+                        color: #76E5FF;
                     }
 
                     #erp-christmas-notice .content {
@@ -159,56 +162,39 @@ class Admin_Page {
 
                     #erp-christmas-notice .content h3 {
                         color: #FFF;
-                        margin: 12px 0px 5px;
+                        margin: 12px 0 5px;
                         font-weight: normal;
-                        font-size: 20px;
+                        font-size: 30px;
                     }
 
                     #erp-christmas-notice .content p {
-                        margin: 0px 0px;
-                        padding: 0px;
-                        letter-spacing: 0.4px;
-                    }
-
-                    #erp-christmas-notice .coupon-box {
-                        margin-top: 10px;
-                        display: flex;
-                        align-items: center;
-                        font-size: 17px;
-                    }
-
-                    #erp-christmas-notice .coupon-box .highlight-code {
-                        margin-left: 15px;
-                        border: 1px dashed;
-                        padding: 4px 10px;
-                        border-radius: 15px;
-                        letter-spacing: 1px;
-                        background: #1E1B1B;
-
-                        -webkit-user-select: all;
-                        -moz-user-select: all;
-                        -ms-user-select: all;
-                        user-select: all;
+                        margin-top: 12px;
+                        padding: 0;
+                        letter-spacing: .4px;
+                        color: #ffffff;
+                        font-size: 15px;
                     }
 
                     #erp-christmas-notice .call-to-action {
-                        margin-left: 8%;
-                        margin-top: 25px;
+                        margin-left: 10%;
+                        margin-top: 20px;
                     }
-                    #erp-christmas-notice .call-to-action a {
-                        border: none;
-                        background: #FF0000;
-                        padding: 8px 15px;
-                        font-size: 15px;
+
+                    #erp-christmas-notice .call-to-action a:focus {
+                        box-shadow: none;
+                    }
+
+                    #erp-christmas-notice .call-to-action p {
+                        font-size: 16px;
                         color: #fff;
-                        border-radius: 20px;
-                        text-decoration: none;
-                        display: block;
+                        margin-top: 1px;
                         text-align: center;
                     }
-                    #erp-christmas-notice .call-to-action p {
-                        font-size: 12px;
-                        margin-top: 1px;
+
+                    #erp-christmas-notice .coupon-code {
+                        -moz-user-select: all;
+                        -webkit-user-select: all;
+                        user-select: all;
                     }
                 </style>
 
@@ -226,47 +212,6 @@ class Admin_Page {
         }
     }
 
-    /**
-     * Accounting survey notice
-     *
-     * @since 1.4.6
-     *
-     * @return void
-     */
-    public function accounting_survey_notice() {
-        // Show only to Admins
-        if ( ! current_user_can( 'manage_options' ) ) {
-            return;
-        }
-
-        // check if it has already been dismissed
-        $hide_notice = get_option( 'erp_accounting_survey_notice', 'no' );
-
-        if ( 'hide' == $hide_notice ) {
-            return;
-        } ?>
-
-        <div class="notice notice-info is-dismissible" id="erp-accounting-survey-notice">
-            <p>We have completely revamped the Accounting module. Why not check the <a href="<?php echo admin_url('/admin.php?page=erp-accounting#/erp-ac-help') ?>" target="_blank">help</a> page for detailed instructions.</p>
-        </div><!-- #erp-accounting-survey-notice -->
-
-        <style>
-            #erp-accounting-survey-notice p {
-                font-size: 15px;
-            }
-        </style>
-
-        <script type='text/javascript'>
-            jQuery('body').on('click', '#erp-accounting-survey-notice .notice-dismiss', function(e) {
-                e.preventDefault();
-
-                wp.ajax.post('erp-dismiss-accounting-survey-notice', {
-                    dismissed: true
-                });
-            });
-        </script>
-    <?php
-    }
 }
 
 new Admin_Page();
