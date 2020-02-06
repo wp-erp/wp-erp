20b2e28b9eb44edf9aea48c4d766b093b749d080
Nadim <nadim0988@gmail.com>
2020-02-06 16:09:11 +0600

    Model Update WIP 3

--- modules/hrm/includes/models/leave-approval-status.php
+++ modules/hrm/includes/models/leave-approval-status.php
@@ -15,4 +15,48 @@ class Leave_Approval_Status extends Model {
         'leave_request_id', 'approval_status_id', 'approved_by',
         'approved_date', 'forward_to', 'message'
     ];
+
+    /**
+     * Relation to Leave_Entitlement model
+     *
+     * @since 1.6.0
+     *
+     * @return object
+     */
+    public function entitlements() {
+        return $this->hasMany( 'WeDevs\ERP\HRM\Models\Leave_Entitlement', 'trn_id' );
+    }
+
+    /**
+     * Relation to Leave_Request_Detail model
+     *
+     * @since 1.6.0
+     *
+     * @return object
+     */
+    public function details() {
+        return $this->hasMany( 'WeDevs\ERP\HRM\Models\Leave_Request_Detail', 'leave_request_id', 'leave_request_id' );
+    }
+
+    /**
+     * Relation to Leave_Request model
+     *
+     * @since 1.6.0
+     *
+     * @return object
+     */
+    public function leave_request() {
+        return $this->belongsTo( 'WeDevs\ERP\HRM\Models\Leave_Request' );
+    }
+
+    /**
+     * Relation to Leaves_Unpaid model
+     *
+     * @since 1.6.0
+     *
+     * @return object
+     */
+    public function unpaids() {
+        return $this->hasMany( 'WeDevs\ERP\HRM\Models\Leaves_Unpaid', 'leave_request_id', 'leave_request_id' );
+    }
 }
--- modules/hrm/includes/models/leave-unpaid.php
+++ modules/hrm/includes/models/leave-unpaid.php
@@ -15,4 +15,48 @@ class Leaves_Unpaid extends Model {
         'leave_id', 'leave_request_id', 'leave_approval_status_id',
         'user_id', 'days', 'amount', 'total', 'status'
     ];
+
+    /**
+     * Relation to Leave model
+     *
+     * @since 1.6.0
+     *
+     * @return object
+     */
+    public function leave() {
+        return $this->belongsTo( 'WeDevs\ERP\HRM\Models\Leave' );
+    }
+
+    /**
+     * Relation to Leave_Entitlement model
+     *
+     * @since 1.6.0
+     *
+     * @return object
+     */
+    public function entitlements() {
+        return $this->hasMany( 'WeDevs\ERP\HRM\Models\Leave_Entitlement', 'trn_id' );
+    }
+
+    /**
+     * Relation to Leave_Request model
+     *
+     * @since 1.6.0
+     *
+     * @return object
+     */
+    public function leave_request() {
+        return $this->belongsTo( 'WeDevs\ERP\HRM\Models\Leave_Request' );
+    }
+
+    /**
+     * Relation to Leave_Request_Detail model
+     *
+     * @since 1.6.0
+     *
+     * @return object
+     */
+    public function details() {
+        return $this->hasMany( 'WeDevs\ERP\HRM\Models\Leave_Request_Detail', 'leave_request_id', 'leave_request_id' );
+    }
 }
