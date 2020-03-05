3ea0114696e6341a78ca80bb4b7baa922bb5339e
Jahidur Rahman Nadim <nadim0988@gmail.com>
2020-03-05 10:49:45 +0600

    Test scenario for HR, CRM & Accounting (#998)
    * Modify all tests case
    
    * Modified and Add some scenarion for Expenses
    
    * Add exploratory testing
    
    * Modified exploratory testing and create purchases
    
    * Create bill, Pay bill and Create Purchase
    
    * Write basic test scenario for HR & CRM. Also write for Purchase & Tax
    
    * Moditied tax scenario

--- tests/_support/Step/Acceptance/AllSteps.php
+++ tests/_support/Step/Acceptance/AllSteps.php
@@ -21,6 +21,29 @@ class AllSteps extends \AcceptanceTester
         $I->wait(5);
         $I->moveMouseOver('//div[2]/div/div[2]/div/div/ul/li[3]/a');
     }
+    public function previewUsers()
+    {
+        $I = $this;
+        $I->click('WP ERP');
+        $I->click('Accounting');
+        $I->wait(5);
+        $I->moveMouseOver('//div[2]/div/div[2]/div/div/ul/li[2]/a');
+    }
+    public function previewProducts()
+    {
+        $I = $this;
+        $I->click('WP ERP');
+        $I->click('Accounting');
+        $I->wait(5);
+        $I->moveMouseOver('//div[2]/div/div[2]/div/div/ul/li[6]/a');
+    }
+    public function checkError()
+    {
+        $I = $this;
+        $I->dontSee('Warning');
+        $I->dontSee('Fatal error');
+        $I->dontSee('Notice');
+    }
 
 }
 
