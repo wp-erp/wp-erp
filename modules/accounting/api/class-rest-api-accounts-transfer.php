f4139a75980a4959be3543fe7fbc4b63a36184d7
Towhidul Islam <itowhid06@gmail.com>
2020-03-25 15:41:17 +0600

    Remove unused endpoint from accounting dashboard

--- modules/accounting/api/class-rest-api-accounts-transfer.php
+++ modules/accounting/api/class-rest-api-accounts-transfer.php
@@ -137,14 +137,6 @@ class Bank_Accounts_Controller extends \WeDevs\ERP\API\REST_Controller {
 						return current_user_can( 'erp_ac_view_bank_accounts' );
 					},
 				],
-				[
-					'methods'             => WP_REST_Server::CREATABLE,
-					'callback'            => [ $this, 'update_dashboard_accounts' ],
-					'args'                => [],
-					'permission_callback' => function( $request ) {
-						return current_user_can( 'erp_ac_create_bank_transfer' );
-					},
-				],
 			]
         );
 
@@ -360,50 +352,6 @@ class Bank_Accounts_Controller extends \WeDevs\ERP\API\REST_Controller {
 
         $response = rest_ensure_response( $formatted_items );
         $response = $this->format_collection_response( $response, $request, 0 );
-
-        return $response;
-    }
-
-    /**
-     * Update dashboard bank accounts
-     *
-     * @param WP_REST_Request $request
-     *
-     * @return WP_Error|WP_REST_Response
-     */
-    public function update_dashboard_accounts( $request ) {
-        global $wpdb;
-
-        $wpdb->query( 'TRUNCATE TABLE ' . $wpdb->prefix . 'erp_acct_cash_at_banks' );
-
-        $items           = $request['accounts'];
-        $formatted_items = [];
-
-        if ( empty( $items ) ) {
-            return new WP_Error( 'rest_empty_accounts', __( 'Bank accounts are empty.' ), [ 'status' => 204 ] );
-        }
-
-        foreach ( $items as $item ) {
-            $additional_fields = [];
-
-            $wpdb->insert(
-                $wpdb->prefix . 'erp_acct_cash_at_banks',
-                array(
-					'ledger_id' => $item['ledger_id'],
-					'name'      => $item['name'],
-					'balance'   => $item['balance'],
-                )
-            );
-
-            $data              = $this->prepare_dashboard_item_for_response( $item, $request, $additional_fields );
-            $formatted_items[] = $this->prepare_response_for_collection( $data );
-        }
-
-        $additional_fields['namespace'] = $this->namespace;
-        $additional_fields['rest_base'] = $this->rest_base;
-
-        $response = rest_ensure_response( $formatted_items );
-        $response->set_status( 201 );
 
         return $response;
     }
