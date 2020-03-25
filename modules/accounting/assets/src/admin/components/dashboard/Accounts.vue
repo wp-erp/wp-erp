4eb8ebb9d170afe76b1f840d91748462d3ab3690
Towhidul Islam <itowhid06@gmail.com>
2020-03-25 15:39:07 +0600

    Remove unused code from accounting dashboard

--- modules/accounting/assets/src/admin/components/dashboard/Accounts.vue
+++ modules/accounting/assets/src/admin/components/dashboard/Accounts.vue
@@ -2,10 +2,6 @@
     <div :class="['bank-accounts-section', 'wperp-panel', 'wperp-panel-default', ( isEditSettingsEnabled ? 'open-edit':'' )]">
         <div class="wperp-panel-heading wperp-bg-white">
             <h4>{{ __('Accounts', 'erp') }}</h4>
-            <!--<a href="#" @click.prevent="editSettings" id="bank-account-edit" class="panel-badge">-->
-                <!--<span v-if="isEditSettingsEnabled" @click.prevent="saveDashboardBanks">Save</span>-->
-                <!--<i v-else class="flaticon-quick-edit"></i>-->
-            <!--</a>-->
         </div>
         <div class="wperp-panel-body pb-0">
             <ul v-if="accounts.length" class="wperp-list-unstyled list-table-content list-table-content--border">
@@ -86,28 +82,6 @@ export default {
                 this.accounts = response.data;
             });
         }
-
-        // saveDashboardBanks() {
-        //     HTTP.post('/accounts/cash-at-bank', {
-        //         accounts: this.edit_accounts
-        //     }).then(() => {
-        //         this.fetchAccounts();
-        //         this.isEditSettingsEnabled = false;
-        //     });
-        // },
-        //
-        // removeAccount(key) {
-        //     this.$delete(this.accounts, key);
-        // },
-        //
-        // removeEditAccount(key) {
-        //     this.$delete(this.edit_accounts, key);
-        // },
-        //
-        // editSettings() {
-        //     this.fetchAccounts();
-        //     this.isEditSettingsEnabled = !this.isEditSettingsEnabled;
-        // }
     }
 };
 </script>
