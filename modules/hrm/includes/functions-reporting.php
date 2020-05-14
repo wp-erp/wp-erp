449d8e727666caca52d5177105bd2941eecee76f
opurockey <opu.rockey@gmail.com>
2020-05-14 09:57:30 +0600

    String update of designation page list page

--- modules/hrm/includes/functions-reporting.php
+++ modules/hrm/includes/functions-reporting.php
@@ -28,7 +28,7 @@ function erp_hr_get_reports() {
             'description' => __( 'Shows longevity and experience report of the employees of your company.', 'erp' )
         ],
         'leaves' => [
-            'title' => __( 'Leaves Reports', 'erp' ),
+            'title' => __( 'Leaves', 'erp' ),
             'description' => __( 'Shows leaves report.', 'erp' )
         ]
     ];
