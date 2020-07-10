bbb306f760877aa9f72dae9d17bb160efc822995
opurockey <opu.rockey@gmail.com>
2020-07-10 11:41:45 +0600

    Configured pay purchase email template for accounting

--- modules/accounting/includes/classes/class-emails.php
+++ modules/accounting/includes/classes/class-emails.php
@@ -21,6 +21,7 @@ class Emailer {
         $emails['Transactional_Email_Purchase']         = new Includes\Emails\Transactional_Email_Purchase();
         $emails['Transactional_Email_Estimate']         = new Includes\Emails\Transactional_Email_Estimate();
         $emails['Transactional_Email_Purchase_Order']   = new Includes\Emails\Transactional_Email_Purchase_Order();
+        $emails['Transactional_Email_Pay_Purchase']     = new Includes\Emails\Transactional_Email_Pay_Purchase();
 
         return $emails;
     }
