bd8842d4d6383d69b72f0439ecc5d2704d311eda
Nurul Umbhiya <zikubd@gmail.com>
2020-09-24 08:05:36 +0600

    Release version 1.6.5

--- wp-erp.php
+++ wp-erp.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://wperp.com
  * Author: weDevs
  * Author URI: https://wedevs.com
- * Version: 1.6.4
+ * Version: 1.6.5
  * License: GPL2
  * Text Domain: erp
  * Domain Path: /i18n/languages/
@@ -52,7 +52,7 @@ final class WeDevs_ERP {
      *
      * @var string
      */
-    public $version = '1.6.4';
+    public $version = '1.6.5';
 
     /**
      * Minimum PHP version required
