d229622a12a379c1786853a99aa38035689b4aa2
tanmayjay <jktanmay@gmail.com>
2020-10-19 01:10:25 +0600

    Added employee type edit template

---
+++ modules/hrm/views/js-templates/employment-type.php
@@ -0,0 +1,36 @@
+<div class="type-form-wrap">
+    <div class="row">
+        <?php erp_html_form_input( [
+            'label'    => __( 'Date', 'erp' ),
+            'name'     => 'date',
+            'value'    => date( 'Y-m-d', current_time( 'timestamp' ) ),
+            'required' => true,
+            'class'    => 'erp-date-field',
+        ] ); ?>
+    </div>
+
+    <div class="row">
+        <?php erp_html_form_input( [
+            'label'   => __( 'Employment Type', 'erp' ),
+            'name'    => 'type',
+            'value'   => '',
+            'type'    => 'select',
+            'options' => [ 0 => __( '- Select -', 'erp' ) ] + erp_hr_get_employee_types(),
+        ] ); ?>
+    </div>
+
+    <div class="row">
+        <?php erp_html_form_input( [
+            'label'       => __( 'Comment', 'erp' ),
+            'name'        => 'comment',
+            'value'       => '',
+            'placeholder' => __( 'Optional comment', 'erp' ),
+            'type'        => 'textarea',
+            'custom_attr' => [ 'rows' => 4, 'cols' => 25 ],
+        ] ); ?>
+    </div>
+
+    <?php wp_nonce_field( 'employee_update_employment' ); ?>
+    <input type="hidden" name="action" id="type-action" value="erp-hr-emp-update-type">
+    <input type="hidden" name="user_id" id="emp-id" value="{{ data.user_id }}">
+</div>
