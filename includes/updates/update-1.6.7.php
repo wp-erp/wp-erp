6049cb067653ad175766d498642275ea17181f6f
emran-wedevs <69339029+emran-wedevs@users.noreply.github.com>
2020-10-23 11:29:55 +0600

    update phpcs issue

--- includes/updates/update-1.6.7.php
+++ includes/updates/update-1.6.7.php
@@ -8,10 +8,9 @@ function erp_acct_alter_acct_expenses_1_6_7() {
     global $wpdb;
 
     $table = $wpdb->prefix . 'erp_acct_expenses';
-    $prepared_statement = $wpdb->prepare( "DESC { $table}" );
-    $cols  = $wpdb->get_col($prepared_statement);
+    $cols  = $wpdb->get_col($wpdb->prepare("DESC $table"));
 
-    if ( ! in_array( 'transaction_charge', $cols, true ) ) {
+    if (! in_array( 'transaction_charge', $cols, true )) {
         $wpdb->query(
             $wpdb->prepare(
                 "ALTER TABLE {$table} ADD `transaction_charge` decimal(20,2) DEFAULT 0 AFTER `trn_by`;"
@@ -27,10 +26,9 @@ function erp_acct_alter_pay_purchase_1_6_7() {
     global $wpdb;
 
     $table = $wpdb->prefix . 'erp_acct_pay_purchase';
-    $prepared_statement = $wpdb->prepare( "DESC { $table}" );
-    $cols  = $wpdb->get_col($prepared_statement);
+    $cols  = $wpdb->get_col($wpdb->prepare("DESC $table"));
 
-    if ( ! in_array( 'transaction_charge', $cols, true ) ) {
+    if (! in_array( 'transaction_charge', $cols, true )) {
         $wpdb->query(
             $wpdb->prepare(
                 "ALTER TABLE {$table} ADD `transaction_charge` decimal(20,2) DEFAULT 0 AFTER `trn_by`;"
@@ -54,10 +52,9 @@ function erp_acct_alter_pay_bill_1_6_7() {
     global $wpdb;
 
     $table = $wpdb->prefix . 'erp_acct_pay_bill';
-    $prepared_statement = $wpdb->prepare( "DESC { $table}" );
-    $cols  = $wpdb->get_col( $prepared_statement );
+    $cols  = $wpdb->get_col( $wpdb->prepare("DESC $table")      );
 
-    if ( ! in_array( 'ref', $cols, true) ) {
+    if (! in_array( 'ref', $cols, true)) {
         $wpdb->query(
             $wpdb->prepare(
                 "ALTER TABLE {$table} ADD `ref` varchar(255) NULL DEFAULT NULL  AFTER `particulars`;"
