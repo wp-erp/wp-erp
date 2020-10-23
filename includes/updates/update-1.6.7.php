47d3899f399df3ea5f7aa24a68af28fbeeeec50b
emran-wedevs <69339029+emran-wedevs@users.noreply.github.com>
2020-10-23 11:37:45 +0600

    update phpcs issue

--- includes/updates/update-1.6.7.php
+++ includes/updates/update-1.6.7.php
@@ -10,7 +10,7 @@ function erp_acct_alter_acct_expenses_1_6_7() {
     $table = $wpdb->prefix . 'erp_acct_expenses';
     $cols  = $wpdb->get_col($wpdb->prepare("DESC $table"));
 
-    if (! in_array( 'transaction_charge', $cols, true )) {
+    if ( ! in_array('transaction_charge', $cols, true) ) {
         $wpdb->query(
             $wpdb->prepare(
                 "ALTER TABLE {$table} ADD `transaction_charge` decimal(20,2) DEFAULT 0 AFTER `trn_by`;"
@@ -28,7 +28,7 @@ function erp_acct_alter_pay_purchase_1_6_7() {
     $table = $wpdb->prefix . 'erp_acct_pay_purchase';
     $cols  = $wpdb->get_col($wpdb->prepare("DESC $table"));
 
-    if (! in_array( 'transaction_charge', $cols, true )) {
+    if ( ! in_array('transaction_charge', $cols, true) ) {
         $wpdb->query(
             $wpdb->prepare(
                 "ALTER TABLE {$table} ADD `transaction_charge` decimal(20,2) DEFAULT 0 AFTER `trn_by`;"
@@ -52,9 +52,9 @@ function erp_acct_alter_pay_bill_1_6_7() {
     global $wpdb;
 
     $table = $wpdb->prefix . 'erp_acct_pay_bill';
-    $cols  = $wpdb->get_col( $wpdb->prepare("DESC $table")      );
+    $cols  = $wpdb->get_col($wpdb->prepare("DESC $table"));
 
-    if (! in_array( 'ref', $cols, true)) {
+    if ( ! in_array('ref', $cols, true) ) {
         $wpdb->query(
             $wpdb->prepare(
                 "ALTER TABLE {$table} ADD `ref` varchar(255) NULL DEFAULT NULL  AFTER `particulars`;"
