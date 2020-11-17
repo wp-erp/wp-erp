8560263fb60c367616fab9e98bc1531e8898cb39
Al Emran <emran@wedevs.com>
2020-11-17 11:17:26 +0600

    update migration

--- includes/updates/update-1.6.9.php
+++ includes/updates/update-1.6.9.php
@@ -6,13 +6,12 @@ namespace WeDevs\ERP\HRM\Update;
 function erp_acct_alter_purchase_1_6_9() {
     global $wpdb;
 
-    $table = $wpdb->prefix . 'erp_acct_purchase';
-    $cols  = $wpdb->get_col( "DESC $table" );
+    $cols  = $wpdb->get_col( "DESC {$wpdb->prefix}erp_acct_purchase" );
 
     if ( !in_array( 'tax', $cols ) ) {
         $wpdb->query(
             $wpdb->prepare(
-                "ALTER TABLE $table ADD `tax` decimal(20,2)  NULL DEFAULT NULL  AFTER `amount`;"
+                "ALTER TABLE {$wpdb->prefix}erp_acct_purchase ADD `tax` decimal(20,2)  DEFAULT NULL  AFTER `amount`;"
             )
         );
     }
@@ -20,7 +19,7 @@ function erp_acct_alter_purchase_1_6_9() {
     if ( !in_array( 'tax_zone_id', $cols ) ) {
         $wpdb->query(
             $wpdb->prepare(
-                "ALTER TABLE $table ADD `tax_zone_id` integer  NULL DEFAULT NULL  AFTER `tax`;"
+                "ALTER TABLE {$wpdb->prefix}erp_acct_purchase  ADD `tax_zone_id` integer  DEFAULT NULL  AFTER `tax`;"
             )
         );
     }
@@ -52,6 +51,7 @@ function erp_acct_alter_purchase_details_1_6_9() {
 function crate_erp_acct_purchase_details_tax_table_1_6_9() {
 
     global $wpdb;
+    $charset_collate  = $wpdb->get_charset_collate() ;
     $sql = "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}erp_acct_purchase_details_tax (
               `id` int(20) UNSIGNED NOT NULL AUTO_INCREMENT,
               `invoice_details_id` int(20) NOT NULL,
@@ -62,7 +62,7 @@ function crate_erp_acct_purchase_details_tax_table_1_6_9() {
               `updated_at` timestamp DEFAULT NULL,
               `updated_by` int(20) DEFAULT NULL,
               PRIMARY KEY  (`id`)
-            ) DEFAULT CHARSET=utf8";
+            ) DEFAULT $charset_collate";
 
     dbDelta($sql);
 }
