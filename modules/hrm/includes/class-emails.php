ede18e80db6b31da9369c623de5e198db9a2f6d1
opurockey <opu.rockey@gmail.com>
2020-12-03 16:06:07 +0600

    Added government holiday email template

--- modules/hrm/includes/class-emails.php
+++ modules/hrm/includes/class-emails.php
@@ -21,6 +21,7 @@ class Emailer {
         $emails['Rejected_Leave_Request']   = new Emails\Rejected_Leave_Request();
         $emails['Birthday_Wish']            = new Emails\Birthday_Wish();
         $emails['Hiring_Anniversary_Wish']  = new Emails\Hiring_Anniversary_Wish();
+        $emails['Govt_Holiday_Reminder']    = new Emails\Govt_Holiday_Reminder();
 
         return apply_filters( 'erp_hr_email_classes', $emails );
     }
