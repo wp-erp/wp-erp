410f6479cfbef5e15fcddece55ce3dc6ebb3b763
opurockey <opu.rockey@gmail.com>
2020-12-07 12:11:58 +0600

    Fixed hr calender leave request delay issue

--- modules/hrm/includes/functions-leave.php
+++ modules/hrm/includes/functions-leave.php
@@ -1257,10 +1257,11 @@ function erp_hr_get_leave_request( $request_id ) {
  * @since 1.6.0
  *
  * @param array $args
+ * @param boolean $cached
  *
  * @return array
  */
-function erp_hr_get_leave_requests( $args = [] ) {
+function erp_hr_get_leave_requests( $args = [], $cached = true ) {
     global $wpdb;
 
     $defaults = [
@@ -1322,7 +1323,7 @@ function erp_hr_get_leave_requests( $args = [] ) {
     $cache_key = 'erp_hr_leave_requests_' . md5( serialize( $args ) );
     $requests  = wp_cache_get( $cache_key, 'erp' );
 
-    if ( false !== $requests ) {
+    if ( false !== $requests && true === $cached ) {
         return $requests;
     }
 
