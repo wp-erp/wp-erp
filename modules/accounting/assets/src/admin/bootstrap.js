3267ff922f15ebad3062f7537552a29258ae93be
Tanmay Kirtania <jktanmay@gmail.com>
2021-01-26 13:40:54 +0600

    Updated bootstrap and store script

--- modules/accounting/assets/src/admin/bootstrap.js
+++ modules/accounting/assets/src/admin/bootstrap.js
@@ -20,9 +20,12 @@ import SelectPeople from 'admin/components/people/SelectPeople.vue';
 import Vuelidate from 'vuelidate';
 import PieChart from 'admin/components/chart/PieChart.vue';
 import DynamicTrnLoader from 'admin/components/transactions/DynamicTrnLoader.vue';
+import TransParticulars from 'admin/components/transactions/TransParticulars.vue';
+import TransactionsFilter from 'admin/components/transactions/TransactionsFilter.vue';
 import VueClipboards from 'vue-clipboards';
 import { createHooks } from '@wordpress/hooks';
 import { clickOutside } from './directive/directives';
+import Swal from 'sweetalert2' ;
 
 // global acct var
 window.acct = {
@@ -53,6 +56,9 @@ window.acct.libs['Vuelidate']        = Vuelidate;
 window.acct.libs['PieChart']         = PieChart;
 window.acct.libs['VueClipboards']    = VueClipboards;
 window.acct.libs['clickOutside']     = clickOutside;
+window.acct.libs['TransParticulars'] = TransParticulars;
+window.acct.libs['TransactionsFilter'] = TransactionsFilter;
+window.acct.libs['Swal']               = Swal;
 
 // get lib reference from window
 window.acct_get_lib = function(lib) {
