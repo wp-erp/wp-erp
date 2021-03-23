d980a9db4d57a7317e8479e0b88e404a320ba586
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-03-23 08:52:32 +0600

    Fixed code formatting

--- modules/crm/views/schedules.php
+++ modules/crm/views/schedules.php
@@ -1,6 +1,6 @@
 <?php
-$tab            = ( isset( $_GET['tab'] ) && !empty( $_GET['tab'] ) ) ? sanitize_text_field( wp_unslash( $_GET['tab'] ) ) : 'own';
-$sub_section    = ( isset( $_GET['sub-section'] ) && !empty( $_GET['sub-section'] ) ) ? sanitize_text_field( wp_unslash( $_GET['sub-section'] ) ) : '';
+$tab            = ( isset( $_GET['tab'] ) && ! empty( $_GET['tab'] ) ) ? sanitize_text_field( wp_unslash( $_GET['tab'] ) ) : 'own';
+$sub_section    = ( isset( $_GET['sub-section'] ) && ! empty( $_GET['sub-section'] ) ) ? sanitize_text_field( wp_unslash( $_GET['sub-section'] ) ) : '';
 $schedules_data = erp_crm_get_schedule_data( $tab );
 ?>
 <div class="wrap erp erp-crm-schedules" id="wp-erp">
