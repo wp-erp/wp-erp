9119305dbb57838e260202a499c9b60df9d8f898
tanmayjay <jktanmay@gmail.com>
2021-04-07 19:30:00 +0600

    Added update attachment in notes

--- modules/crm/views/js-templates/customer-newnote.php
+++ modules/crm/views/js-templates/customer-newnote.php
@@ -8,6 +8,26 @@ $customer_id = isset( $_GET['id'] ) ? intval( $_GET['id'] ) : 0;
     <trix-editor v-if="feed" id="note-text-editor" input="note_activity_message_{{ feed.id }}" placeholder="<?php esc_attr_e( 'Type your note .....', 'erp' ); ?>"></trix-editor>
     <input v-if="feed" id="note_activity_message_{{ feed.id }}" v-model="feedData.message" type="hidden" name="note_activity_message_{{ feed.id }}" value="{{ feed.message }}">
 
+    <div class="crm-attachments" id="{{feed ? 'crm-attachments-' + feed.id : 'crm-attachments'}}">
+       <div id="progress-wrp"><div class="progress-bar"></div ><div class="status">0%</div></div>
+        <div id="output">
+            <p v-for="file in files">
+                {{ file }}
+            </p>
+        </div>
+    </div>
+
+    <div class="timeline-body" v-if="feed">
+        <div class="timeline-activity-attachments">
+            <?php esc_attr_e( 'Attachments : ', 'erp' ); ?>
+            <ul v-if="(feed.extra.attachments && feed.extra.attachments.length > 0)">
+                <li v-for="file in feed.extra.attachments"><a id="activity-atch-name-{{{feed.id}}}-{{{$index}}}" target="_blank" href="{{{file.url}}}">{{{file.name}}}{{{updateAttachments(feed.id, file, $index, false)}}}</a><span id="btn-activity-atch-{{{feed.id}}}-{{{$index}}}" v-on:click.prevent="updateAttachments(feed.id, file, $index, removeAtchFlag)" class="btn-activity-atch remove-atch dashicons dashicons-dismiss"></span></li>
+            </ul>
+            <input type="file" name="attachments[]" class="crm-activity-attachment" id="activity-attachment-{{feed.id}}" v-on:change="addAttachments(feed)" multiple>
+            <label for="activity-attachment-{{feed.id}}" class="attachments-label" title="Add File"><?php _e( '+ Add File', 'erp' ); ?></label>
+        </div>
+    </div>
+
     <div class="submit-action">
         <input type="hidden" name="user_id" v-model="feedData.user_id" value="<?php echo esc_attr( $customer_id ); ?>" >
         <input type="hidden" name="created_by" v-model="feedData.created_by" value="<?php echo esc_attr( get_current_user_id() ); ?>" >
