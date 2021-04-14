657860eeb0699e256ecd9bf6e261fc1870c46d52
tanmayjay <jktanmay@gmail.com>
2021-04-14 20:30:21 +0600

    Fixed employee history was not parsing th elatest correctly

--- modules/hrm/includes/class-employee.php
+++ modules/hrm/includes/class-employee.php
@@ -1523,7 +1523,7 @@ class Employee {
             $histories = $histories->where( 'module', $module );
         }
 
-        $histories = $histories->orderBy( 'id', 'desc' )
+        $histories = $histories->orderBy( 'date', 'desc' )
                             ->skip( $offset )
                             ->take( $limit )
                             ->get();
