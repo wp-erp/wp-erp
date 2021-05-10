b1326ccff4717338e42b812e7239183dc5be59de
tanmayjay <jktanmay@gmail.com>
2021-05-11 01:31:34 +0600

    Added bulk actions and table header map

--- modules/hrm/assets/js/requests.js
+++ modules/hrm/assets/js/requests.js
@@ -36,6 +36,110 @@
                     data    : erpHrReq.request_types,
                     default : '',
                     field   : 'type',
+                }, 
+                bulkActionMap    : [
+                    {
+                        id   : 'approved',
+                        text : __( 'Approve', 'erp' ),
+                    },
+                    {
+                        id   : 'rejected',
+                        text : __( 'Reject', 'erp' ),
+                    },
+                ],
+                tableHeaderMap    : {
+                    resigned    : [
+                        {
+                            title : __( 'Reason', 'erp' ),
+                            class : ''
+                        },
+                        {
+                            title : __( 'Resign Date', 'erp' ),
+                            class : ''
+                        },
+                        {
+                            title : __( 'Request Date', 'erp' ),
+                            class : ''
+                        },
+                    ],
+                    leave       : [
+                        {
+                            title : __( 'Reason', 'erp' ),
+                            class : ''
+                        },
+                        {
+                            title : __( 'Start Date', 'erp' ),
+                            class : ''
+                        },
+                        {
+                            title : __( 'End Date', 'erp' ),
+                            class : ''
+                        },
+                        {
+                            title : __( 'Duration', 'erp' ),
+                            class : 'text-center'
+                        },
+                    ],
+                    remote_work : [
+                        {
+                            title : __( 'Reason', 'erp' ),
+                            class : ''
+                        },
+                        {
+                            title : __( 'Start Date', 'erp' ),
+                            class : ''
+                        },
+                        {
+                            title : __( 'End Date', 'erp' ),
+                            class : ''
+                        },
+                        {
+                            title : __( 'Duration', 'erp' ),
+                            class : 'text-center'
+                        },
+                    ],
+                    asset       : [
+                        {
+                            title : __( 'Item', 'erp' ),
+                            class : ''
+                        },
+                        {
+                            title : __( 'Category', 'erp' ),
+                            class : ''
+                        },
+                        {
+                            title : __( 'Request Date', 'erp' ),
+                            class : ''
+                        },
+                    ],
+                    reimburse   : [
+                        {
+                            title : __( 'Amount', 'erp' ),
+                            class : ''
+                        },
+                        {
+                            title : __( 'Transaction Date', 'erp' ),
+                            class : ''
+                        },
+                        {
+                            title : __( 'Request Date', 'erp' ),
+                            class : ''
+                        },
+                    ],
+                    common      : [
+                        {
+                            title : __( 'Employee', 'erp' ),
+                            class : ''
+                        },
+                        {
+                            title : __( 'Status', 'erp' ),
+                            class : 'text-center'
+                        },
+                        {
+                            title : __( 'Actions', 'erp' ),
+                            class : 'text-center'
+                        },
+                    ]
                 },
             },
         });
