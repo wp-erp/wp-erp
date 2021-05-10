18195f5f73fa7fcf9d32217cea032c59df425f9b
tanmayjay <jktanmay@gmail.com>
2021-05-11 01:59:57 +0600

    Added method to keep notification up to date

--- modules/hrm/assets/js/requests.js
+++ modules/hrm/assets/js/requests.js
@@ -564,6 +564,33 @@
                         });
                     });
                 },
+
+                updateNotification: function(val) {
+                    var selector = '#erp-hr-requests .erp-nav .requests a',
+                        child    = 'span.erp-notification',
+                        pending  = 0;
+
+                    wp.ajax.send({
+                        data: {
+                            action: 'erp_hr_get_total_pending_requests'
+                        },
+                        success: function(response) {
+                            pending = response;
+                        },
+                        error: function(error) {
+                            pending = 0;
+                        }
+                    })
+                    .then( function() {
+                        if ( pending > 0 ) {
+                            if ( ! $( selector ).find( child ).length ) {
+                                $( selector ).append( ` <span class="erp-notification">${pending}</span>` );
+                            } else {
+                                $( `${selector} ${child}` ).html( pending );
+                            }
+                        }
+                    });
+                },
             },
 
             watch: {
