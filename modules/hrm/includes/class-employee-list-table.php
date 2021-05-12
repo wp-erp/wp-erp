fd987e1e713bc3000c68f38a9c949a1d22733fbb
tanmayjay <jktanmay@gmail.com>
2021-05-13 01:56:28 +0600

    Updated employee search box

--- modules/hrm/includes/class-employee-list-table.php
+++ modules/hrm/includes/class-employee-list-table.php
@@ -323,11 +323,38 @@ class Employee_List_Table extends \WP_List_Table {
      * @return void
      */
     public function search_box( $text, $input_id ) {
-        parent::search_box( $text, $input_id );
+        if ( empty( $_REQUEST['s'] ) && ! $this->has_items() ) {
+			return;
+		}
+
+		$input_id = $input_id . '-search-input';
+
+		if ( ! empty( $_REQUEST['orderby'] ) ) {
+			echo '<input type="hidden" name="orderby" value="' . esc_attr( $_REQUEST['orderby'] ) . '" />';
+		}
+
+		if ( ! empty( $_REQUEST['order'] ) ) {
+			echo '<input type="hidden" name="order" value="' . esc_attr( $_REQUEST['order'] ) . '" />';
+		}
+
+		if ( ! empty( $_REQUEST['post_mime_type'] ) ) {
+			echo '<input type="hidden" name="post_mime_type" value="' . esc_attr( $_REQUEST['post_mime_type'] ) . '" />';
+		}
+
+		if ( ! empty( $_REQUEST['detached'] ) ) {
+			echo '<input type="hidden" name="detached" value="' . esc_attr( $_REQUEST['detached'] ) . '" />';
+		}
 
         if ( ! empty( $_REQUEST['status'] ) ) {
             echo '<input type="hidden" name="status" value="' . esc_attr( sanitize_text_field( wp_unslash( $_REQUEST['status'] ) ) ) . '" />';
         }
+
+		?>
+        <p class="search-box erp-list-table-search">
+            <label class="screen-reader-text" for="<?php echo esc_attr( $input_id ); ?>"><?php echo $text; ?>:</label>
+            <input type="search" id="<?php echo esc_attr( $input_id ); ?>" name="s" value="<?php _admin_search_query(); ?>" placeholder="<?php esc_attr_e( 'Search Employee', 'erp' ); ?>"/>
+        </p>
+		<?php
     }
 
     /**
