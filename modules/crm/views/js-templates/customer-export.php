d818f2fbc51795aa504c71bf5a1175923c00aa60
tanmayjay <jktanmay@gmail.com>
2021-05-24 14:15:37 +0600

    Added template for exporter in crm

---
+++ modules/crm/views/js-templates/customer-export.php
@@ -0,0 +1,28 @@
+<div class="erp-grid-container">
+    <div class="row">
+        <div class="col-3">
+            <label for="fields">
+                <h3>{{ data.label }}<span class="required"> *</span></h3>
+            </label>
+        </div>
+
+        <div class="col-3">
+            <h3>
+                <input type="checkbox" id="selecctall"/> 
+                <strong><?php esc_html_e( 'Select all', 'erp' ); ?></strong>
+            </h3>
+        </div>
+    </div>
+
+    <div class="row" id="fields"></div>
+    
+    <div class="row"></div>
+
+    <div class="row">
+        <p class="description" style="color: grey; float: right;"><?php esc_html_e( '**Only selected fields will be on the csv file.', 'erp' ); ?></p>
+    </div>
+</div>
+
+<input type="hidden" name="type" value="{{ data.type }}">
+<input type="hidden" name="erp_export_csv" value="1">
+<?php wp_nonce_field( 'erp-import-export-nonce' ); ?>
