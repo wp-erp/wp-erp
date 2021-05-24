91b5d9f0924bd0680b83253d5cd51d54507519c9
tanmayjay <jktanmay@gmail.com>
2021-05-24 12:35:05 +0600

    Added initial employee import template

---
+++ modules/hrm/views/js-templates/employee-import.php
@@ -0,0 +1,27 @@
+<table class="form-table">
+    <tbody>
+        <tr>
+            <th>
+                <label for="csv_file"><?php esc_html_e( 'CSV File', 'erp' ); ?> <span class="required">*</span></label>
+            </th>
+            <td>
+                <input type="file" name="csv_file" id="csv_file" required />
+                <p class="description">
+                    <?php
+                    esc_html_e( 'Upload a csv file.', 'erp' );
+                    echo erp_help_tip( esc_html__( 'Make sure CSV meets the sample CSV format exactly.', 'erp' ) );
+                    ?>
+                </p>
+                <p id="download_sample_wrap">                    
+                    <button class="button button-primary"
+                        id="erp-employee-sample-csv"
+                        data-url="<?php echo esc_url( $csv_sample_url ); ?>">
+                        <?php esc_html_e( 'Download Sample CSV', 'erp' ); ?>
+                    </button>
+                </p>
+            </td>
+        </tr>
+    </tbody>
+
+    <tbody id="erp-csv-fields-container" style="display: none;"></tbody>
+</table>
