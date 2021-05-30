0a84722db21aeda1ab6bd51de0db16b3909a2eb1
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-05-31 05:08:50 +0600

    Updated design, sidebar and routes for HR

--- assets/src/settings/components/menu/SettingsSubMenu.vue
+++ assets/src/settings/components/menu/SettingsSubMenu.vue
@@ -2,7 +2,7 @@
   <div class="settings-submenu-navbar">
     <ul class="settings-sub-menu">
       <li>
-        <router-link tag="li" :to="'/hr/workdays'" class="active">
+        <router-link tag="li" :to="'/hr/workdays'">
           <a href="#">
             <span class="menu-name">{{ __("Workdays", "erp") }}</span>
           </a>
@@ -44,6 +44,29 @@
         <router-link tag="li" :to="'/hr/recruitment'" >
           <a href="#">
             <span class="menu-name">{{ __("Recruitment", "erp") }}</span>
+          </a>
+        </router-link>
+      </li>
+
+      <!-- <li class="dropdown-nav">
+        <a href="#" class="">{{ __('More', 'erp') }}</a>
+        <ul class="erp-nav-dropdown">
+            <li><a href="#/hr/payroll" class="">Payroll</a></li>
+        </ul>
+     </li> -->
+
+      <!-- <li>
+        <router-link tag="li" :to="'/hr/payroll'" >
+          <a href="#">
+            <span class="menu-name">{{ __("Payroll", "erp") }}</span>
+          </a>
+        </router-link>
+      </li> -->
+
+      <li>
+        <router-link tag="li" :to="'/hr/attendance'" >
+          <a href="#">
+            <span class="menu-name">{{ __("Attendance", "erp") }}</span>
           </a>
         </router-link>
       </li>
--- assets/src/settings/router/index.js
+++ assets/src/settings/router/index.js
@@ -1,7 +1,14 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import GeneralSettings from 'settings/components/general/GeneralSettings.vue';
-import HRWorkDays from 'settings/components/hr-workdays/HRWorkDays.vue';
+import HRWorkDays from 'settings/components/hr/workdays/HRWorkDays.vue';
+import HRLeave from 'settings/components/hr/leave/HRLeave.vue';
+import HRLeaveYears from 'settings/components/hr/leave-years/HRLeaveYears.vue';
+import HRMiscellaneous from 'settings/components/hr/miscellaneous/HRMiscellaneous.vue';
+import HRFrontend from 'settings/components/hr/hr-frontend/HRFrontend.vue';
+import HRRecruitment from 'settings/components/hr/recruitment/HRRecruitment.vue';
+import HRPayroll from 'settings/components/hr/payroll/HRPayroll.vue';
+import HRAttendance from 'settings/components/hr/attendance/HRAttendance.vue';
 
 Vue.use(Router);
 
@@ -15,28 +22,60 @@ export default new Router({
                 {
                     path: 'general',
                     name: 'GeneralSettings',
-                    component: GeneralSettings
+                    component: GeneralSettings,
+                    alias: '/general'
                 }
             ]
         },
         {
             path: '/hr',
-            component: HRWorkDays,
+            name: 'HR',
+            component: {
+                render(c) {
+                    return c('router-view');
+                }
+            },
             children: [
                 {
                     path: 'workdays',
                     name: 'HRWorkDays',
-                    component: HRWorkDays
+                    component: HRWorkDays,
+                    alias: '/hr'
                 },
                 {
                     path: 'leave',
-                    name: 'HRWorkDays',
-                    component: HRWorkDays
+                    name: 'HRLeave',
+                    component: HRLeave
                 },
                 {
                     path: 'leave-years',
-                    name: 'HRWorkDays',
-                    component: HRWorkDays
+                    name: 'HRLeaveYears',
+                    component: HRLeaveYears
+                },
+                {
+                    path: 'miscellaneous',
+                    name: 'HRMiscellaneous',
+                    component: HRMiscellaneous
+                },
+                {
+                    path: 'hr-frontend',
+                    name: 'HRFrontend',
+                    component: HRFrontend
+                },
+                {
+                    path: 'recruitment',
+                    name: 'HRRecruitment',
+                    component: HRRecruitment
+                },
+                {
+                    path: 'payroll',
+                    name: 'HRPayroll',
+                    component: HRPayroll
+                },
+                {
+                    path: 'attendance',
+                    name: 'HRAttendance',
+                    component: HRAttendance
                 },
             ]
         }
