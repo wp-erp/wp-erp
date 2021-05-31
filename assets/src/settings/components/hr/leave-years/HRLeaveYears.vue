e9febba9660000b0e35cf4e0b7299c0247903abb
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-05-31 17:55:18 +0600

    Fix design leave years page

--- assets/src/settings/components/hr/leave-years/HRLeaveYears.vue
+++ assets/src/settings/components/hr/leave-years/HRLeaveYears.vue
@@ -8,25 +8,33 @@
         <h3 class="sub-section-title">{{ __("Leave Years", "erp") }}</h3>
 
         <form action="" class="wperp-form" method="post" @submit.prevent="submitHRLeaveYearsForm">
-
             <div class="wperp-row">
                 <div class="wperp-form-group wperp-col-sm-4 wperp-col-xs-12">
                     <label> {{ __("Name", 'erp') }}</label>
-                    <input v-model="fields.name" class="wperp-form-field" />
                 </div>
                 <div class="wperp-form-group wperp-col-sm-4 wperp-col-xs-12">
                     <label> {{ __("Start Date", 'erp') }}</label>
-                    <date-picker class="wperp-form-field" :placeholder="__( 'Start date', 'erp' )"
-                                v-model="fields.start_date" />
                 </div>
                 <div class="wperp-form-group wperp-col-sm-4 wperp-col-xs-12">
                     <label> {{ __("End Date", 'erp') }}</label>
-                    <date-picker class="wperp-form-field" :placeholder="__( 'End date', 'erp' )"
-                                v-model="fields.end_date" />
                 </div>
-                <div class="wperp-form-group wperp-col-sm-12 wperp-col-xs-12">
-                    <button class="wperp-btn btn-outline-default" type="button">+ Add New</button>
+            </div>
+
+            <div class="wperp-row" v-for="(year, index) in years_data" :key="index">
+                <div class="wperp-form-group wperp-col-sm-4 wperp-col-xs-12">
+                    <input v-model="year.fyear_name" class="wperp-form-field" />
                 </div>
+                <div class="wperp-form-group wperp-col-sm-4 wperp-col-xs-12">
+                    <date-picker class="wperp-form-field" :placeholder="__( 'Start date', 'erp' )" v-model="year.fyear_start" />
+                </div>
+                <div class="wperp-form-group wperp-col-sm-4 wperp-col-xs-12">
+                    <date-picker class="wperp-form-field" :placeholder="__( 'End date', 'erp' )" v-model="fields.fyear_end" />
+                    <span v-if="index > 0" class="settings-btn-cancel">x</span>
+                </div>
+            </div>
+
+            <div class="wperp-form-group">
+                <button class="wperp-btn wperp-btn-default" type="button" @click="addNewYear">+ Add New</button>
             </div>
 
             <div class="wperp-form-group">
@@ -51,9 +59,15 @@ export default {
             fields: {
                 name: '',
                 start_date: '',
-                end_date: '',
-                years: []
-            }
+                end_date: ''
+            },
+            years_data: [
+                {
+                    fyear_name: '',
+                    fyear_start: '',
+                    fyear_end: ''
+                }
+            ]
         }
   },
 
@@ -70,6 +84,17 @@ export default {
         this.showAlert('success', 'Leave years saved successfully !');
 
         this.$store.dispatch('spinner/setSpinner', false);
+      },
+
+      addNewYear() {
+        //   const years_data = this.years_data;
+
+          this.years_data.push({
+            fyear_name: '',
+            fyear_start: '',
+            fyear_end: ''
+          });
+
       }
   },
 
