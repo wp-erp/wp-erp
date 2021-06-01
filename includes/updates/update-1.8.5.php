7184256c471882c01b23ec58b3f7687b0b5a17f4
Tanmay Kirtania <jktanmay@gmail.com>
2021-06-01 13:12:37 +0600

    Removed prepare statement as there's no placeholder

--- includes/updates/update-1.8.5.php
+++ includes/updates/update-1.8.5.php
@@ -9,11 +9,7 @@ function erp_acct_alter_table_erp_hr_education_1_8_5() {
     $cols = $wpdb->get_col( "DESC {$wpdb->prefix}erp_hr_education" );
 
     if ( in_array( 'result', $cols ) ) {
-        $wpdb->query(
-            $wpdb->prepare(
-                "ALTER TABLE `{$wpdb->prefix}erp_hr_education` MODIFY `result` VARCHAR(50);"
-            )
-        );
+        $wpdb->query( "ALTER TABLE `{$wpdb->prefix}erp_hr_education` MODIFY `result` VARCHAR(50);" );
     }
 }
 
