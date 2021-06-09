ceef59c170a7643b46d7b934e1c7b8433194d305
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-06-09 11:23:20 +0600

    Add HR Remote Work Settings

---
+++ assets/src/settings/components/hr/remote-work/HRRemoteWork.vue
@@ -0,0 +1,19 @@
+<template>
+    <base-layout
+        section_id="erp-hr"
+        sub_section_id="remote_work"
+        :enable_content="true"
+    />
+</template>
+
+<script>
+import BaseLayout from "settings/components/layouts/BaseLayout.vue";
+
+export default {
+    name: "HRRemoteWork",
+
+    components: {
+        BaseLayout,
+    },
+};
+</script>
