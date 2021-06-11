9591d33c5213573759b79137a42c6462ee6e887d
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-06-11 17:16:02 +0600

    Added Accounting Stripe Payment Settings

---
+++ assets/src/settings/components/act/payment/AcPaymentStripe.vue
@@ -0,0 +1,18 @@
+<template>
+    <ac-payment
+        :single_option="true"
+        sub_sub_section="stripe"
+    />
+</template>
+
+<script>
+import AcPayment from "settings/components/act/payment/AcPayment.vue";
+
+export default {
+    name: "AcPaymentStripe",
+
+    components: {
+        AcPayment
+    }
+};
+</script>
