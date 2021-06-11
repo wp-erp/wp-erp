981c6c233b17423b89c7949289313fbe982a17d1
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-06-11 17:15:02 +0600

    Payment General Settings Added

---
+++ assets/src/settings/components/act/payment/AcPaymentGeneral.vue
@@ -0,0 +1,17 @@
+<template>
+    <ac-payment
+        sub_sub_section="general"
+    />
+</template>
+
+<script>
+import AcPayment from "settings/components/act/payment/AcPayment.vue";
+
+export default {
+    name: "AcPaymentGeneral",
+
+    components: {
+        AcPayment
+    }
+};
+</script>
