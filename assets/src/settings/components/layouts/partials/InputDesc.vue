9e7ab7d432ba667439b6b7446ba73c20db005c63
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-06-14 15:46:14 +0600

    Component created for input hint description to optimize codes

---
+++ assets/src/settings/components/layouts/partials/InputDesc.vue
@@ -0,0 +1,25 @@
+<template>
+    <p class="erp-form-input-hint" v-if="isEnableDescription(input)">{{ input.desc }}</p>
+</template>
+
+<script>
+export default {
+    name: "InputDesc",
+
+    props: {
+        input: {
+            type    : Object,
+            required: true
+        }
+    },
+
+    methods: {
+        /**
+         * Check if description will be enable for input
+         */
+        isEnableDescription ( input ) {
+            return input.desc && input.desc.length > 0 && ! input.tooltip
+        },
+    },
+}
+</script>
