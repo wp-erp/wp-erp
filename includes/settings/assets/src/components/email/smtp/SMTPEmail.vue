dc41610f3f1852f93f2e52512baef755dfd20925
tanmayjay <jktanmay@gmail.com>
2021-07-01 15:34:13 +0600

    Added template for Email SMTP section

---
+++ includes/settings/assets/src/components/email/smtp/SMTPEmail.vue
@@ -0,0 +1,43 @@
+<template>
+    <base-layout
+        section_id="erp-email"
+        sub_section_id="smtp"
+        :enable_content="true"
+        :extra_content="true"
+        :options="options"
+    >
+        <template>
+            <div class="wperp-form-group">
+                <label for="smtp-test-connection"></label>
+                
+                <button id="smtp-test-connection" class="wperp-btn btn--secondary" @click="testConnection">{{ __('Send Test Email', 'erp') }}</button>
+            </div>
+        </template>
+    </base-layout>
+</template>
+
+<script>
+import BaseLayout from "../../layouts/BaseLayout.vue";
+
+export default {
+    name: 'SMTPEmail',
+
+    components: {
+        BaseLayout,
+    },
+
+    data() {
+        return {
+            smtpTestEmail: '',
+            options: {},
+        }
+    },
+
+    methods: {
+        testConnection() {
+            this.options.action    = 'erp_smtp_test_connection';
+            this.options.recurrent = false;
+        }
+    }
+}
+</script>
