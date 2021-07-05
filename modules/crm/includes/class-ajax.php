dd70a9618483f213e3488ec7504edfe7556ce417
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-07-05 15:43:27 +0600

    Undefined index handle on CRM create contact

--- modules/crm/includes/class-ajax.php
+++ modules/crm/includes/class-ajax.php
@@ -297,7 +297,7 @@ class Ajax_Handler {
 
         $current_user_id                      = get_current_user_id();
         $posted                               = array_map( 'strip_tags_deep', $_POST );
-        $posted['contact']['main']['company'] = stripslashes( $posted['contact']['main']['company'] ); // To remove Apostrophe slash
+        $posted['contact']['main']['company'] = stripslashes( ! empty( $posted['contact']['main']['company'] ) ? $posted['contact']['main']['company'] : '' ); // To remove Apostrophe slash
 
         $data = array_merge( $posted['contact']['main'], $posted['contact']['meta'], $posted['contact']['social'] );
 
