ec799b962d6e6b62a9b6f6ebb0173f30c9c1861e
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-07-05 11:22:37 +0600

    Updated some CSS and component design

--- includes/settings/assets/src/components/layouts/partials/InputDesc.vue
+++ includes/settings/assets/src/components/layouts/partials/InputDesc.vue
@@ -18,8 +18,8 @@ export default {
          * Check if description will be enable for input
          */
         isEnableDescription ( input ) {
-            return input.desc && input.desc.length > 0 && ! input.tooltip
-        },
+            return input.desc && input.desc.length > 0 && ! input.tooltip && input.type !== 'checkbox'
+        }
     },
 }
 </script>
