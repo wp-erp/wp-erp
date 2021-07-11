4b2ae499eab7f317f4c881b674add056c1e6779a
tanmayjay <jktanmay@gmail.com>
2021-07-11 18:13:46 +0600

    Updated data saving of integration settings

--- includes/settings/includes/Integration.php
+++ includes/settings/includes/Integration.php
@@ -5,7 +5,7 @@ namespace WeDevs\ERP\Settings;
 /**
  * Integration class
  */
-class Integration extends Page_View {
+class Integration extends Template {
 
     /**
      * Class constructor
@@ -14,6 +14,10 @@ class Integration extends Page_View {
         $this->id    = 'erp-integration';
         $this->label = __( 'Integrations', 'erp' );
         $this->icon  = WPERP_ASSETS . '/images/wperp-settings/integration.png';
+
+        $this->extra = [
+            'integrations' => wperp()->integration->get_integrations()
+        ];
 
         add_action( 'erp_admin_field_integrations', [ $this, 'integrations' ] );
     }
@@ -37,78 +41,6 @@ class Integration extends Page_View {
         ]; // End general settings
 
         return apply_filters( 'erp_integration_settings', $fields );
-    }
-
-    /**
-     * Display integrations settings.
-     *
-     * @return void
-     */
-    public function integrations() {
-        $integrations = wperp()->integration->get_integrations(); ?>
-        <tr valign="top">
-            <td class="erp-settings-table-wrapper" colspan="2">
-                <table class="erp-settings-table widefat" cellspacing="0">
-                    <thead>
-                        <tr>
-                            <?php
-                                $columns = apply_filters( 'erp_integration_setting_columns', [
-                                    'name'        => __( 'Integration', 'erp' ),
-                                    'description' => __( 'Description', 'erp' ),
-                                    'actions'     => '',
-                                ] );
-
-                                foreach ( $columns as $key => $column ) {
-                                    echo '<th class="erp-settings-table-' . esc_attr( $key ) . '">' . esc_html( $column ) . '</th>';
-                                } ?>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        foreach ( $integrations as $integration_key => $integration ) {
-                            echo '<tr>';
-
-                            foreach ( $columns as $key => $column ) {
-                                switch ( $key ) {
-                                    case 'name':
-                                        echo '<td class="erp-settings-table-' . esc_attr( $key ) . '">
-                                            <a href="' . esc_url( admin_url( 'admin.php?page=erp-settings&tab=erp-integration&section=' . esc_attr( strtolower( $integration_key ) ) ) ) . '">' . esc_html( $integration->get_title() ) . '</a>
-                                        </td>';
-                                        break;
-
-                                    case 'status':
-                                    case 'module':
-                                    case 'recipient':
-                                        echo '<td class="erp-settings-table-' . esc_attr( $key ) . '">
-
-                                        </td>';
-                                        break;
-
-                                    case 'description':
-                                        echo '<td class="erp-settings-table-' . esc_attr( $key ) . '">
-                                            <span class="help">' . esc_html( $integration->get_description() ) . '</span>
-                                        </td>';
-                                        break;
-
-                                    case 'actions':
-                                        echo '<td class="erp-settings-table-' . esc_attr( $key ) . '">
-                                            <a class="button alignright" href="' . esc_url( admin_url( 'admin.php?page=erp-settings&tab=erp-integration&section=' . strtolower( $integration_key ) ) ) . '">' . esc_html__( 'Configure', 'erp' ) . '</a>
-                                        </td>';
-                                        break;
-
-                                    default:
-                                        do_action( 'erp_integration_setting_column_' . $key, $integration );
-                                    break;
-                                }
-                            }
-                        } ?>
-                    </tbody>
-                </table>
-            </td>
-        </tr>
-
-        <style>p.submit { display: none; }</style>
-        <?php
     }
 
     /**
@@ -145,10 +77,10 @@ class Integration extends Page_View {
      */
     public function save( $section = false ) {
         if ( isset( $_POST['_wpnonce'] ) && wp_verify_nonce( sanitize_key( $_POST['_wpnonce'] ), 'erp-settings-nonce' ) ) {
-            $current_section = isset( $_GET['section'] ) ? sanitize_text_field( wp_unslash( $_GET['section'] ) ) : false;
+            $current_section = ! empty( $_REQUEST['section'] ) ? sanitize_text_field( wp_unslash( $_REQUEST['section'] ) ) : false;
 
             // saving individual integration settings
-            if ( $current_section ) {
+            if ( ! empty( $current_section ) ) {
                 $integrations = wperp()->integration->get_integrations();
 
                 foreach ( $integrations as $integration_key => $integration ) {
@@ -156,23 +88,38 @@ class Integration extends Page_View {
                         $settings       = $integration->get_form_fields();
                         $update_options = [];
 
-                        if ( $settings ) {
-                            foreach ( $settings as $field ) {
-                                if ( ! isset( $field['id'] ) || ! isset( $_POST[ $field['id'] ] ) ) {
-                                    continue;
-                                }
+                        if ( 'sms' === $current_section ) {
+                            $processed_fields = [];
 
-                                $option_value = $this->parse_option_value( $field );
-
-                                if ( ! is_null( $option_value ) ) {
-                                    $update_options[ $field['id'] ] = $option_value;
+                            foreach ( $settings as $key => $setting ) {
+                                foreach ( $setting as $field ) {
+                                    $processed_fields[] = $field;
                                 }
+                            }
+
+                            $settings = $processed_fields;
+
+                            $update_options = get_option( $integration->get_option_id() );
+                        }
+
+                        foreach ( $settings as $field ) {
+
+                            if ( ! isset( $field['id'] ) || ! isset( $_POST[ $field['id'] ] ) ) {
+                                continue;
+                            }
+
+                            $option_value = $this->parse_option_value( $field );
+
+                            if ( ! is_null( $option_value ) ) {
+                                $update_options[ $field['id'] ] = $option_value;
                             }
                         }
 
-                        do_action( $integration->get_option_id() . '_action', $update_options );
-
                         $update_options = apply_filters( $integration->get_option_id() . '_filter', $update_options );
+
+                        if ( is_wp_error( $update_options ) ) {
+                            return $update_options;
+                        }
 
                         update_option( $integration->get_option_id(), $update_options );
 
@@ -185,5 +132,3 @@ class Integration extends Page_View {
         }
     }
 }
-
-return new Integration();
