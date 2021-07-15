8f39e65f1a0cb611c00a97c3b408c054d1ed1bbd
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-07-15 08:22:39 +0600

    Updated some text of Danger Zone

--- includes/admin/views/tools/danger-zone.php
+++ includes/admin/views/tools/danger-zone.php
@@ -6,18 +6,18 @@
             <i class="fa fa-warning"></i>
             <?php esc_html_e( 'Warning', 'erp' ); ?>
         </h4>
-        <p><?php esc_html_e( 'The reset makes a fresh installation of your database. Therefore any data related to Wp ERP in your database will be lost. Please do not use this option if you do not want to delete all of your WP ERP data.', 'erp' ); ?></p>
+        <p><?php esc_html_e( 'Performing The Reset will delete all the data and get the WP ERP back to its fresh state. This means all data stored related to your ERP will be lost. So please be aware to use this option.', 'erp' ); ?></p>
     </div>
 
     <div class="tools-alert tools-alert-info display-flex tools-alert-no-title">
         <p class="tools-alert-description-icon"><i class="fa fa-info-circle"></i></p>
         <p class="tools-alert-description-text">
-            <?php esc_html_e( 'The reset does not delete of modified any of your other files/data of the site. It will only remove data related to WP ERP', 'erp' ); ?>
+            <?php esc_html_e( 'The reset will not delete or change any of your other files/data of the site. It will only delete data related to your WP ERP.', 'erp' ); ?>
         </p>
     </div>
 
     <div class="tools-card">
-        <h4 class="tools-content-header"><?php esc_html_e( 'Reset if you are well aware about that!', 'erp' ); ?></h4>
+        <h4 class="tools-content-header"><?php esc_html_e( 'Reset if you are well aware about it!', 'erp' ); ?></h4>
         <p class="tools-content-info-text"><?php _e( 'Type <b>“Reset”</b> in the confirmation field below to confirm the reset and then click the reset button.', 'erp' ); ?></p>
 
         <form method="POST" action="" id="danger-zone-form">
@@ -39,7 +39,7 @@
             <input type="hidden" name="action" value="erp_reset_data" />
 
             <button type="button" class="tools-btn tools-btn-loading tools-btn-submit tools-submit-hidden" disabled>
-                <?php _e( 'Resetting', 'erp' ); ?>... <i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>      
+                <?php _e( 'Resetting', 'erp' ); ?>... <i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
             </button>
 
             <button type="submit" class="tools-btn tools-btn-danger tools-btn-submit"> <?php esc_html_e( 'Reset Now', 'erp' ); ?> </button>
@@ -50,7 +50,7 @@
     <div class="tools-alert tools-alert-info display-flex tools-alert-no-title">
         <p class="tools-alert-description-icon"><i class="fa fa-info-circle"></i></p>
         <p class="tools-alert-description-text">
-            <?php _e( '<b>Caution:</b> You’ll need to insert data on WP ERP again after resetting it. Resetting makes WP ERP fresh as it was at the first-time installation.', 'erp' ); ?>
+            <?php _e( '<b>Caution:</b> You\'ll need to insert data again on WP ERP after resetting it. So, Reset WP ERP if you want to start fresh.', 'erp' ); ?>
         </p>
     </div>
 
