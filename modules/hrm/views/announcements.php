f68580599c3b272b37f4c46face110a75fa0903e
Md. Abdulla Al Mamun <nayonabdulla@gmail.com>
2021-08-03 18:44:52 +0600

    add announcements view

---
+++ modules/hrm/views/announcements.php
@@ -0,0 +1,28 @@
+<div class="wrap erp erp-hr-announcement">
+    <h2>
+        <?php esc_html_e( 'People', 'erp' ); ?>
+        <a href="<?php echo admin_url( 'post-new.php?post_type=erp_hr_announcement' ); ?>" id="erp-new-announcement" data-single="1" class="add-new-h2"><?php esc_html_e( 'Add New', 'erp' ); ?></a>
+    </h2>
+
+    <?php do_action( 'erp_hr_people_menu', 'announcement' ); ?>
+
+    <div id="erp-announcement-table-wrap">
+
+        <div class="list-table-inner">
+
+            <form method="get">
+                <input type="hidden" name="page" value="erp-hr">
+                <input type="hidden" name="section" value="people">
+                <input type="hidden" name="sub-section" value="announcement">
+                <?php
+                $announcement = new \WeDevs\ERP\HRM\Announcement_List_Table();
+                $announcement->prepare_items();
+                $announcement->views();
+                $announcement->display();
+                ?>
+            </form>
+
+        </div><!-- .list-table-inner -->
+    </div><!-- .list-table-wrap -->
+
+</div>
