7c64b037c5238f46eb07eea4ad2b31f3c26a3a0e
tanmayjay <jktanmay@gmail.com>
2021-08-03 11:49:01 +0600

    Added updater for 191

--- includes/updates/update-1.9.1.php
+++ includes/updates/update-1.9.1.php
@@ -34,5 +34,32 @@ function erp_acct_alter_purchase_details_table_1_9_1() {
     }
 }
 
+/**
+ * Create table erp_acct_synced_taxes
+ */
+function erp_acct_create_synced_taxes_table_1_9_1() {
+    global $wpdb;
+
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $schema = "CREATE TABLE {$wpdb->prefix}erp_acct_synced_taxes (
+                id int NOT NULL AUTO_INCREMENT,
+                system_id bigint NOT NULL,
+                sync_id bigint NOT NULL,
+                sync_type varchar(100),
+                sync_source varchar(100),
+                PRIMARY KEY  (id),
+                KEY system_id (system_id),
+                KEY sync_id (sync_id),
+                KEY sync_type (sync_type),
+                KEY sync_source (sync_source)
+            ) $charset_collate;";
+
+    require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+
+    dbDelta( $schema );
+}
+
 erp_acct_alter_invoice_details_table_1_9_1();
 erp_acct_alter_purchase_details_table_1_9_1();
+erp_acct_create_synced_taxes_table_1_9_1();
