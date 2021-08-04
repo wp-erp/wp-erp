95ebf7cba1cd4ba7f9e3ff05410ef2d3c36690b5
Md. Abdulla Al Mamun <nayonabdulla@gmail.com>
2021-08-04 09:37:48 +0600

    added row actions and fixed pagination

--- modules/hrm/includes/class-announcement-list-table.php
+++ modules/hrm/includes/class-announcement-list-table.php
@@ -55,35 +55,7 @@ class Announcement_List_Table extends WP_List_Table {
     public function column_default( $item, $column_name ) {
         switch ( $column_name ) {
             case 'title':
-                return "<a href='" . admin_url( "post.php?post={$item->ID}&action=edit" ) . "'>{$item->post_title}</a>";
-
-            case 'sent_to':
-                $type = get_post_meta( $item->ID, '_announcement_type', true );
-                $sent_to = [];
-                if ( $type === 'by_department' ) {
-                    $sent_to = get_post_meta( $item->ID, '_announcement_department', true );
-                    $sent_to = array_map( function( $id ) {
-                        if ( $d = \WeDevs\ERP\HRM\Models\Department::find( (int) $id ) ) {
-                            return $d->title;
-                        }
-                    }, $sent_to );
-                } elseif ( $type === 'by_designation' ) {
-                    $sent_to = get_post_meta( $item->ID, '_announcement_designation', true );
-                    $sent_to = array_map( function( $id ) {
-                        if ( $d = \WeDevs\ERP\HRM\Models\Designation::find( (int) $id ) ) {
-                            return $d->title;
-                        }
-                    }, $sent_to );
-                } else {
-                    $sent_to = get_post_meta( $item->ID, '_announcement_selected_user', true );
-                    $sent_to = array_map( function( $id ) {
-                        if ( $u = get_user_by( 'id', $id ) ) {
-                            return $u->display_name;
-                        }
-                    }, $sent_to );
-                }
-
-                return count( $sent_to ) === 0 ? 'None' : implode( ', ', $sent_to );
+                return esc_html( $item->post_title );
 
             case 'type':
                 $type = ucfirst( get_post_meta( $item->ID, '_announcement_type', true ) );
@@ -95,6 +67,80 @@ class Announcement_List_Table extends WP_List_Table {
             default:
                 return isset( $item->$column_name ) ? $item->$column_name : '';
         }
+    }
+
+    /**
+     * Render checkbox column
+     *
+     * @param Object $item
+     *
+     * @return void
+     */
+    public function column_cb( $item ) {
+        return sprintf( '<input type="checkbox" name="announce[]" value="%d" />', $item->ID );
+    }
+
+    /**
+     * Render the name column
+     *
+     * @param Object $item
+     *
+     * @return void
+     */
+    public function column_title( $item ) {
+        $actions = [];
+
+        $params = [
+            'post' => $item->ID,
+        ];
+
+        $params['action'] = 'edit';
+        $edit_url         = add_query_arg( $params, admin_url( 'post.php' ) );
+
+        $params['action']   = 'trash';
+        $params['_wpnonce'] = wp_create_nonce( 'trash-post_' . $item->ID );
+        $delete_url         = add_query_arg( $params, admin_url( 'post.php' ) );
+
+        $actions['edit']   = sprintf( '<a href="%s" data-id="%d" title="%s">%s</a>', $edit_url, $item->id, esc_html__( 'Edit this item', 'erp' ), esc_html__( 'Edit', 'erp' ) );
+        $actions['delete'] = sprintf( '<a href="%s" class="submitcopy" data-id="%d" title="%s">%s</a>', $delete_url, $item->id, esc_html__( 'Delete this item', 'erp' ), esc_html__( 'Delete', 'erp' ) );
+
+        return sprintf( '<strong>%s</strong> %s', esc_html( $item->post_title ), $this->row_actions( $actions ) );
+    }
+
+    /**
+     * Render Sent To Column
+     *
+     * @param Object $item
+     *
+     * @return void
+     */
+    public function column_sent_to( $item ) {
+        $type = get_post_meta( $item->ID, '_announcement_type', true );
+        $sent_to = [];
+        if ( $type === 'by_department' ) {
+            $sent_to = get_post_meta( $item->ID, '_announcement_department', true );
+            $sent_to = array_map( function( $id ) {
+                if ( $d = \WeDevs\ERP\HRM\Models\Department::find( (int) $id ) ) {
+                    return $d->title;
+                }
+            }, $sent_to );
+        } elseif ( $type === 'by_designation' ) {
+            $sent_to = get_post_meta( $item->ID, '_announcement_designation', true );
+            $sent_to = array_map( function( $id ) {
+                if ( $d = \WeDevs\ERP\HRM\Models\Designation::find( (int) $id ) ) {
+                    return $d->title;
+                }
+            }, $sent_to );
+        } else {
+            $sent_to = get_post_meta( $item->ID, '_announcement_selected_user', true );
+            $sent_to = array_map( function( $id ) {
+                if ( $u = get_user_by( 'id', $id ) ) {
+                    return $u->display_name;
+                }
+            }, $sent_to );
+        }
+
+        return count( $sent_to ) === 0 ? 'None' : implode( ', ', $sent_to );
     }
 
     /* public function get_bulk_actions() {
@@ -122,11 +168,11 @@ class Announcement_List_Table extends WP_List_Table {
 
         // only ncessary because we have sample data
         $args = [
-            'offset' => $offset,
-            'number' => $per_page,
+            'offset'      => $offset,
+            'numberposts' => $per_page,
         ];
 
-        $this->items = erp_hr_get_announcements( /* $args */ );
+        $this->items = erp_hr_get_announcements( $args );
 
         $this->set_pagination_args( [
             'total_items' => erp_hr_get_announcements_count( $args ),
