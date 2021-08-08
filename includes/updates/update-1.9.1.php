fc1f0c1df36aa469d4c10722f64067f5c7b77c12
tanmayjay <jktanmay@gmail.com>
2021-08-08 18:14:29 +0600

    Updated synced tax schema updater

--- includes/updates/update-1.9.1.php
+++ includes/updates/update-1.9.1.php
@@ -43,12 +43,14 @@ function erp_acct_create_synced_taxes_table_1_9_1() {
     $schema = "CREATE TABLE {$wpdb->prefix}erp_acct_synced_taxes (
                 id int NOT NULL AUTO_INCREMENT,
                 system_id bigint NOT NULL,
-                sync_id bigint NOT NULL,
-                sync_type varchar(100),
-                sync_source varchar(100),
+                sync_id bigint DEFAULT NULL,
+                sync_slug varchar(100) DEFAULT NULL,
+                sync_type varchar(100) DEFAULT NULL,
+                sync_source varchar(100) DEFAULT NULL,
                 PRIMARY KEY  (id),
                 KEY system_id (system_id),
                 KEY sync_id (sync_id),
+                KEY sync_slug (sync_slug),
                 KEY sync_type (sync_type),
                 KEY sync_source (sync_source)
             ) $charset_collate;";
