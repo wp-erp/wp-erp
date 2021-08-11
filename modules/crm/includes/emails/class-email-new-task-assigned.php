d6461d95a254b949d173c47d8fb8dda8a729c1d1
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-08-11 12:40:56 +0600

    Formatted some codes

--- modules/crm/includes/emails/class-email-new-task-assigned.php
+++ modules/crm/includes/emails/class-email-new-task-assigned.php
@@ -55,22 +55,23 @@ class New_Task_Assigned extends Email {
         $this->heading = $this->get_option( 'heading', $this->heading );
         $this->subject = $this->get_option( 'subject', $this->subject );
 
+        $task_view_url      = admin_url( 'admin.php?page=erp-crm&section=task&sub-section=tasks' );
+        $task_description   = $activity->message . "<br><a style='background: #1A9ED4;color: #fff;padding: 6px 10px;margin: 0;border-radius: 3px;display: inline-block;text-decoration: none;' href='$task_view_url' target='_blank'> ". __( 'View Tasks', 'erp' ) ." </a><br><br>";
+
         foreach ( $data['user_ids'] as $id ) {
             $employee = new \WeDevs\ERP\HRM\Employee( intval( $id ) );
 
-            $this->recipient    = $employee->user_email;
-            $task_view_url      = admin_url( 'admin.php?page=erp-crm&section=task&sub-section=tasks' );
-            $task_description   = $activity->message . "<br><a style='background: #1A9ED4;color: #fff;padding: 6px 10px;margin: 0;border-radius: 3px;display: inline-block;text-decoration: none;' href='$task_view_url' target='_blank'> ". __( 'View Tasks', 'erp' ) ." </a><br><br>";
-
-            $this->replace      = [
-                'employee_name'    => $employee->get_full_name(),
-                'task_title'       => $extra->task_title,
-                'task_description' => $task_description,
-                'due_date'         => erp_format_date( $activity->start_date ),
-                'created_by'       => $current_user->display_name,
-            ];
-
             if ( $employee ) {
+                $this->recipient    = $employee->user_email;
+                
+                $this->replace      = [
+                    'employee_name'    => $employee->get_full_name(),
+                    'task_title'       => $extra->task_title,
+                    'task_description' => $task_description,
+                    'due_date'         => erp_format_date( $activity->start_date ),
+                    'created_by'       => $current_user->display_name,
+                ];
+
                 $this->send( $this->get_recipient(), $this->get_subject(), $this->get_content(), $this->get_headers(), $this->get_attachments() );
             }
         }
