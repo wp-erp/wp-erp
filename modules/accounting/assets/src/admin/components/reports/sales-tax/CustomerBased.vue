aa68dfd07d0db850e6051f5cbce67a37f3ece4a1
tanmayjay <jktanmay@gmail.com>
2021-08-13 02:10:11 +0600

    Added on select trigger for customers

--- modules/accounting/assets/src/admin/components/reports/sales-tax/CustomerBased.vue
+++ modules/accounting/assets/src/admin/components/reports/sales-tax/CustomerBased.vue
@@ -2,7 +2,7 @@
     <div class="sales-tax-report">
         <h2 class="title-container">
             <span>{{ __( 'Sales Tax Report (Customer Based)', 'erp' ) }}</span>
-            
+
             <router-link
                 class="wperp-btn btn--primary"
                 :to="{ name: 'SalesTaxReportOverview' }">
@@ -13,13 +13,13 @@
         <form @submit.prevent="getReport" class="query-options no-print">
             <div class="wperp-date-group">
                 <div class="with-multiselect">
-                    <multi-select v-model="customer" :options="customers"/>
+                    <multi-select v-model="customer" :options="customers" @input="getReport" />
                 </div>
 
                 <datepicker v-model="startDate" />
-                
+
                 <datepicker v-model="endDate" />
-                
+
                 <button class="wperp-btn btn--primary add-line-trigger" type="submit">
                     {{ __( 'Filter', 'erp' ) }}
                 </button>
@@ -36,12 +36,12 @@
                 <strong>{{ __( 'Customer Name', 'erp' ) }}:</strong>
                 <em> {{ customer.name }}</em>
             </li>
-            
+
             <li>
                 <strong>{{ __( 'Currency', 'erp' ) }}:</strong>
                 <em> {{ symbol }}</em>
             </li>
-            
+
             <li v-if="startDate && endDate">
                 <strong>{{ __( 'For the period of (Transaction date)', 'erp' ) }}:</strong>
                 <em> {{ formatDate( startDate ) }}</em> to <em>{{ formatDate( endDate ) }}</em>
@@ -53,7 +53,7 @@
             :columns="columns"
             :rows="taxes"
             :showCb="false">
-            
+
             <template slot="voucher_no" slot-scope="data">
                 <strong>
                     <router-link
@@ -67,11 +67,11 @@
                     </router-link>
                 </strong>
             </template>
-            
+
             <template slot="tax_amount" slot-scope="data">
                 {{ moneyFormat( data.row.tax_amount ) }}
             </template>
-            
+
             <template slot="tfoot">
                 <tr class="tfoot">
                     <td></td>
@@ -92,13 +92,13 @@
 
     export default {
         name: 'SalesTaxReportCustomerBased',
-        
+
         components: {
             ListTable,
             Datepicker,
             MultiSelect
         },
-        
+
         data() {
             return {
                 startDate : null,
@@ -127,11 +127,11 @@
 
             totalTax() {
                 let total = 0;
-                
+
                 this.taxes.forEach(item => {
                     total += parseFloat( item.tax_amount )
                 });
-                
+
                 return total;
             }
         },
@@ -147,7 +147,7 @@
                 const dateObj  = new Date();
                 const month    = ( '0' + ( dateObj.getMonth() + 1 ) ).slice( -2 );
                 const year     = dateObj.getFullYear();
-                
+
                 this.startDate = `${year}-${month}-01`;
                 this.endDate   = erp_acct_var.current_date;
 
@@ -168,9 +168,9 @@
                 if ( ! this.customer ) {
                     return;
                 }
-                
+
                 this.$store.dispatch('spinner/setSpinner', true);
-                
+
                 HTTP.get('/reports/sales-tax', {
                     params: {
                         customer_id : this.customer.id,
