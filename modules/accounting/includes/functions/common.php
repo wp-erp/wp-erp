390478943631317fd1c28c62d95e2c8b2d349812
tanmayjay <jktanmay@gmail.com>
2021-08-14 03:16:05 +0600

    Added method to parse agency wise tax rate

--- modules/accounting/includes/functions/common.php
+++ modules/accounting/includes/functions/common.php
@@ -395,12 +395,12 @@ function erp_acct_get_check_trn_type_by_id( $trn_type_id ) {
 
 /**
  * Retrieves tax category with agency
- * 
+ *
  * @since 1.9.1
- * 
+ *
  * @param int $tax_id
  * @param int $tax_cat_id
- * 
+ *
  * @return array
  */
 function erp_acct_get_tax_rate_with_agency( $tax_id, $tax_cat_id ) {
@@ -415,6 +415,31 @@ function erp_acct_get_tax_rate_with_agency( $tax_id, $tax_cat_id ) {
         ),
         ARRAY_A
     );
+}
+
+/**
+ * Retrieves agency wise tax rate for invoice items
+ *
+ * @since 1.9.1
+ *
+ * @param int|string $invoice_details_id
+ *
+ * @return array
+ */
+function erp_acct_get_invoice_items_agency_wise_tax_rate( $invoice_details_id ) {
+    global $wpdb;
+
+    $result = $wpdb->get_results(
+        $wpdb->prepare(
+            "SELECT agency_id, tax_rate
+            FROM {$wpdb->prefix}erp_acct_invoice_details_tax
+            WHERE invoice_details_id = %d",
+            $invoice_details_id
+        ),
+        ARRAY_A
+    );
+
+    return ! empty( $result ) && ! is_wp_error( $result ) ? $result : [];
 }
 
 /**
@@ -521,7 +546,7 @@ function erp_acct_check_voucher_edit_state( $id ) {
 
 /**
  * Check if people exists in given types
- * 
+ *
  * @since 1.8.4
  *
  * @param string $email
