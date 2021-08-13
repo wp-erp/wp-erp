21dfd8d61758a821f7dd5a816ad5a5f6927f9ef3
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-08-13 17:58:27 +0600

    Email Settings Section Redesigned & Structured - Integration Mailgun and Email Template Moved (#1276)
    * Remove template data and component from CRM Settings
    
    * Added template data and component in Email Settings
    
    * Updated template load ajax call supported globally
    
    * Added images for email settings new design
    
    * Added Email Connect menu in Email Settings Section
    
    * Moved SMTP Email connection to Email Connect Section
    
    * Added some props to support menu and some section hiding option
    
    * Added Styles for Email Connect Settings
    
    * Added Email Connect settings and routes
    
    * Managed email provider list dynamically from backend
    
    * Added mailgun enable or not
    
    * Added iMap and Gmail Fields to Email section
    
    * Modify css for incoming and outgoing email test
    
    * Added IMap Email Component in Email section
    
    * Added Google connect Component in Email section
    
    * Fixed some design for SMTP Email component
    
    * Added all email connection component
    
    * Added mailgun component
    
    * Added Mailgun Backend Dataset for Settings
    
    * Removed test email input from SMTP input fields
    
    * Changed Google Auth Email Settings Link
    
    * Fixed Multiple external input fields and change detection of inputs
    
    * Added Mailgun PHP Library to core with HTTP client support
    
    * Fixed SMTP Email Test Connection Issue
    
    * Fixed some style and Email Main connection section issue
    
    * Added Mailgun Test Connection System
    
    * Added Class to manage all functionality of Mailgun Email
    
    * Updated ajax functionality by using New Mailgun Class
    
    * Fix toggle email providers
    
    * Added responsive design
    
    * Fixed iMap test connection
    
    * Added some additional headers data support for Mailgun
    
    * Added vuex store to handle settings form data outside of the component
    
    * Added AJAX to get all email provider lists
    
    * Removed Email from CRM settings and updated some links
    
    * Optimized some query to getting active emails data
    
    * Added outgoing email sending supported by Mailgun
    
    * Fixed Email provider lists data
    
    * Fixed Email settings option for Google auth
    
    * Added Updater for Email Settings
    
    * Removed new email settings options on reset WP ERP
    
    * Fixed Integration Dropbox connection test issue
    
    * Fixed some exception message display to frontend
    
    * Fixed Mailgun email to validate some undefined index and make message supported as HTML
    
    * Added Some Styles for Selected Email
    
    * Formatting some codes
    
    * Updated Some Message for Email Settings in CRM
    
    * Fixed HTML Email new lines with replaced by break tag
    
    * Fixed Imap Enable/Disable Issue with Status
    
    * Fixed some CSS to eliminate long selector
    
    * Fixed some PHPcs for Mailgun class
    
    * Formatted some codes for vue component
    
    * Fixed some phpcs coding standard
    
    * Removed some unused key
    
    * Removed old settings files
    
    * Updated structure for settings
    
    Co-authored-by: tanmayjay <jktanmay@gmail.com>

--- includes/Settings/assets/src/store/store.js
+++ includes/Settings/assets/src/store/store.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import spinner from './modules/spinner';
+import formdata from './modules/formdata';
 
 Vue.use(Vuex);
 
@@ -8,7 +9,8 @@ const debug = process.env.NODE_ENV !== 'production';
 
 const store = new Vuex.Store({
     modules: {
-        spinner
+        spinner,
+        formdata
     },
     strict: debug,
     plugins: []
