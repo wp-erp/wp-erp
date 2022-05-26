e6e21619fa9b25c7f4655dcd854397733441d4ed
tanmayjay <jktanmay@gmail.com>
2022-05-26 16:52:09 +0600

    Fixed some escaping usage

--- modules/hrm/views/leave/requests.php
+++ modules/hrm/views/leave/requests.php
@@ -10,7 +10,7 @@
     <?php
     if ( ! empty( $_GET['error'] ) ) {
         $errors = new \WeDevs\ERP\ERP_Errors( sanitize_text_field( wp_unslash( $_GET['error'] ) ) );
-        echo esc_html( $errors->display() );
+        echo wp_kses_post( $errors->display() );
     }
     ?>
     <div class="erp-hr-leave-requests-inner">
