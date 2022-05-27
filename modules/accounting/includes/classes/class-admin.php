052b00c3fcea6cefee52f12cfb2bff0cd59ee418
tanmayjay <jktanmay@gmail.com>
2022-05-27 11:42:30 +0600

    Fixed some issues

--- modules/accounting/includes/classes/class-admin.php
+++ modules/accounting/includes/classes/class-admin.php
@@ -328,11 +328,9 @@ class Admin {
      * @return void
      */
     public function init_hooks() {
-        if ( ! isset( $_GET['page'] ) || 'erp-accounting' !== sanitize_text_field( wp_unslash( $_GET['page'] ) ) ) {
-            return;
+        if ( isset( $_GET['page'] ) && 'erp-accounting' === sanitize_text_field( wp_unslash( $_GET['page'] ) ) ) {
+            add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_scripts' ] );
         }
-
-        add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_scripts' ] );
 
         // Ajax hooks
         new Ajax_Handler();
