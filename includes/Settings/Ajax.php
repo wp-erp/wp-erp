414f5b4c67fa0229304d0aead5c92ae6d86da59f
tanmayjay <jktanmay@gmail.com>
2022-06-09 16:31:41 +0600

    Fixed mail server issue

--- includes/Settings/Ajax.php
+++ includes/Settings/Ajax.php
@@ -103,7 +103,7 @@ class Ajax {
                 'message' => erp_get_message(
                     [
                         'type'       => 'save_success',
-                        'additional' => 'Settings',
+                        'additional' => 'Settings'
                     ]
                 )
             ]
@@ -345,7 +345,7 @@ class Ajax {
             $password       = sanitize_text_field( wp_unslash( $_REQUEST['password'] ) );
         }
 
-        $mail_server = esc_url_raw( wp_unslash( $_REQUEST['mail_server'] ) );
+        $mail_server = str_replace( [ 'https://', 'http://' ], '', esc_url_raw( wp_unslash( $_REQUEST['mail_server'] ) ) );
         $port        = sanitize_text_field( wp_unslash( $_REQUEST['port'] ) );
         $to          = empty( $_REQUEST['test_email'] ) ? get_option( 'admin_email' ) : sanitize_email( wp_unslash( $_REQUEST['test_email'] ) );
 
