0d18d67e6699b4a9108dedb7087a2fed426f9361
ratulhasan <ratuljh@gmail.com>
2022-12-19 14:44:31 +0600

    fix "Upgrade to Pro" text alignment is postion is faulty

--- includes/admin/views/upgrade-notice.php
+++ includes/admin/views/upgrade-notice.php
@@ -15,7 +15,7 @@
             <h3 class='title'><?php echo __( 'Please update <b>WP ERP PRO</b>', 'erp' ); ?></h3>
             <div><?php echo __( 'We’ve pushed a major update on both <b>WP ERP Free</b> and <b>WP ERP Pro</b> that requires you to use latest version of both. Please update your <b>ERP Pro</b> to the latest version', 'erp' ); ?></div>
             <div class='notice-button'>
-                <a href="<?php echo esc_url(  self_admin_url( 'plugins.php' ) ); ?>" class='erp-btn erp-btn-primary install-erp-core'><?php esc_html_e( 'Upgrade Now', 'erp' ); ?></a>
+                <a href="<?php echo esc_url( self_admin_url( 'plugins.php' ) ); ?>" class='erp-btn erp-btn-primary install-erp-core'><?php esc_html_e( 'Upgrade Now', 'erp' ); ?></a>
             </div>
         </div>
     </div>
