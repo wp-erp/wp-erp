f96e1262d2bb64aa83babda60658d080e2e04dbe
ratulhasan <ratuljh@gmail.com>
2023-05-22 16:21:15 +0600

    Change callback method

--- includes/Admin/AdminMenu.php
+++ includes/Admin/AdminMenu.php
@@ -130,7 +130,7 @@ class AdminMenu {
         add_submenu_page( 'erp', __( 'Modules', 'erp' ), __( 'Modules', 'erp' ), 'manage_options', 'erp-modules', [ $this, 'module' ] );
         add_submenu_page( 'erp', __( 'WP ERP Pro', 'erp' ), __( 'WP ERP Pro', 'erp' ), 'manage_options', 'erp-addons', [ $this, 'addon_page' ] );
         add_submenu_page( 'erp', __( 'Settings', 'erp' ), __( 'Settings', 'erp' ), 'manage_options', 'erp-settings', [ $this, 'router' ] );
-        add_submenu_page( 'erp', __( 'Create Invoice', 'erp' ), __( 'Create Invoice', 'erp' ), 'manage_options', 'erp-accounting#/invoices/new', [ $this, 'router' ], 4 );
+        add_submenu_page( 'erp', __( 'Create Invoice', 'erp' ), __( 'Create Invoice', 'erp' ), 'manage_options', 'erp-accounting#/invoices/new', '', 4 );
         erp_add_menu( 'settings', [
             'title'         => __( 'Settings', 'erp' ),
             'slug'          => 'settings',
