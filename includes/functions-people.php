e874af2955200762d5ff8cbf847b17005efe79f8
ratulhasan <ratuljh@gmail.com>
2023-05-24 14:45:39 +0600

    🛠️ [FIX] vulnerability issues

--- includes/functions-people.php
+++ includes/functions-people.php
@@ -172,19 +172,20 @@ function erp_get_peoples( $args = [] ) {
                                   . ' )';
         }
 
-        $final_query = $wrapper_select . ' '
-                       . implode( ' ', $sql['select'] ) . ' '
-                       . $sql_from_tb . ' '
-                       . implode( ' ', $sql['join'] ) . ' '
-                       . $sql_people_type . ' '
-                       . 'AND ( 1=1 '
-                       . implode( ' ', $sql['where'] ) . ' '
-                       . ' )'
-                       . $post_where_queries
-                       . $sql_group_by . ' '
-                       . $sql_order_by . ' '
-                       . $sql_limit;
-
+        $final_query = $wpdb->prepare(
+            $wrapper_select . ' '
+            . implode( ' ', $sql['select'] ) . ' '
+            . $sql_from_tb . ' '
+            . implode( ' ', $sql['join'] ) . ' '
+            . $sql_people_type . ' '
+            . 'AND ( 1=1 '
+            . implode( ' ', $sql['where'] ) . ' '
+            . ' )'
+            . $post_where_queries
+            . $sql_group_by . ' '
+            . $sql_order_by . ' '
+            . $sql_limit
+        );
         if ( $count ) {
             // Only filtered total count of people
             $items = $wpdb->get_var( apply_filters( 'erp_get_people_total_count_query', $final_query, $args ) );
