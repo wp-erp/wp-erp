f87e298e7fe4caa75d9fbea5cf8bbed34388503c
Iftakharul Islam Ifat <88052038+iftakharul-islam@users.noreply.github.com>
2025-04-25 15:54:36 +0600

    Remove unwanted iflese

--- modules/hrm/views/reporting.php
+++ modules/hrm/views/reporting.php
@@ -10,9 +10,6 @@
             $sections = count( $reports );
 
             if ( $sections ) {
-                // if ( 0 === $sections ) {
-                //     return;
-                // }
 
                 $left_column  = array_slice( $reports, 0, (int) ( $sections / 2 ) );
                 $right_column = array_slice( $reports, (int) ( $sections / 2 ) );
