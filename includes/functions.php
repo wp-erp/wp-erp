e8e9af5bacaecf5c50188152377a9869ec11a37b
Iftakharul Islam Ifat <88052038+iftakharul-islam@users.noreply.github.com>
2025-10-28 16:07:59 +0600

    fixed: Import export date format issue

--- includes/functions.php
+++ includes/functions.php
@@ -1558,7 +1558,12 @@ function erp_hr_get_human_readable_name( $id, $field, $value, $item ) {
         case 'nationality':
             return erp_get_country_name(  $value );
             break;
-
+        case 'hiring_date':
+        case 'date_of_birth':
+        case 'hiring_date':
+        case 'termination_date':
+            return  ( $value );
+            break;
         // case 'blood_group':
         //     return erp_hr_get_blood_group( $id, $field, $value );
         //     break;
